5ce5f5d84aadc63313b4c22387ae17cabdd8c3d9
Fred <frzyc@users.noreply.github.com>
2020-12-31 21:43:41 -0500

    Updates

--- static/js/13.1c7e8245.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgenshin-optimizer"]=this["webpackJsonpgenshin-optimizer"]||[]).push([[13],{206:function(t,e,r){"use strict";r.r(e),r.d(e,"default",(function(){return h}));var a=r(2),c=r(55),i=r(85),n=r(129),s=r(104);function h(t){Object(s.a)();var e=i.a.getCharacter("character_1");return Object(a.jsx)(c.a,{children:Object(a.jsx)(n.a,{editable:!0,characterToEdit:e})})}}}]);
-//# sourceMappingURL=13.1c7e8245.chunk.js.map
