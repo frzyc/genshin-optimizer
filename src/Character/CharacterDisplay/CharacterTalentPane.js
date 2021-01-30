38ece09df78d1c47d38780cc8be67f4fa0b7844d
Fred <frzyc@users.noreply.github.com>
2021-01-30 15:04:25 -0500

    3.10.9 - Add Elemental reactions to the calculations of the Talents page.

--- src/Character/CharacterDisplay/CharacterTalentPane.js
+++ src/Character/CharacterDisplay/CharacterTalentPane.js
@@ -55,6 +55,11 @@ export default function CharacterTalentPane(props) {
       keys.push(Character.getTalentStatKey("char_atk", character, true))
     //add talents/skills
     talKeys.forEach(key => keys.push(Character.getTalentStatKey(key, character)))
+    //show elemental interactions
+    keys.push(...ElementToReactionKeys[Character.getElementalKey(characterKey)])
+    let weaponTypeKey = Character.getWeaponTypeKey(characterKey)
+    if (!keys.includes("shatter_dmg") && weaponTypeKey === "claymore") keys.push("shatter_dmg")
+
     //search for dependency
     return Stat.getPrintableFormulaStatKeyList(GetDependencies(build.finalStats, keys))
   }
