e5df3db1538b55fe262f1a2e4eafbfdb1d6e0b05
lantua <lantua@users.noreply.github.com>
2021-04-02 22:41:40 -0400

    Compress character & weapon format

--- src/Util/EximUtil.js
+++ src/Util/EximUtil.js
@@ -1,11 +1,18 @@
+import { deepClone } from "./Util"
 
 // DO NOT REMOVE ITEM FROM THESE LISTS
 //      ONLY APPEND NEW ENTRIES
 // The index of items in this list is used to
 // compress the exported data. Removing an item
-// from this list can will shift all the data.
-const slotKeys = [
+// from this list will shift subsequent entries.
+const slots = [
   "flower", "plume", "sands", "goblet", "circlet",
+]
+const hitModes = [
+  "hit", "avgHit", "critHit",
+]
+const reactionModes = [
+  null, "hydro_vaporize", "hydro_vaporize", "pyro_melt", "cryo_melt",
 ]
 const stats = [
   "hp", "hp_", "atk", "atk_", "def", "def_", "eleMas", "enerRech_", "critRate_", "critDMG_", "heal_",
@@ -14,6 +21,9 @@ const stats = [
 ]
 const artifactSets = [
   "Adventurer", "ArchaicPetra", "Berserker", "BlizzardStrayer", "BloodstainedChivalry", "BraveHeart", "CrimsonWitchOfFlames", "DefendersWill", "Gambler", "GladiatorsFinale", "HeartOfDepth", "Instructor", "Lavawalker", "LuckyDog", "MaidenBeloved", "MartialArtist", "NoblesseOblige", "PrayersForDestiny", "PrayersForIllumination", "PrayersForWisdom", "PrayersToSpringtime", "ResolutionOfSojourner", "RetracingBolide", "Scholar", "TheExile", "ThunderingFury", "Thundersoother", "TinyMiracle", "TravelingDoctor", "ViridescentVenerer", "WanderersTroupe",
+]
+const characters = [
+  "albedo", "amber", "barbara", "beidou", "bennett", "chongyun", "diluc", "diona", "fischl", "ganyu", "hutao", "jean", "kaeya", "keqing", "klee", "lisa", "mona", "ningguang", "noelle", "qiqi", "razor", "sucrose", "tartaglia", "traveler_anemo", "traveler_geo", "venti", "xiao", "xiangling", "xingqiu", "xinyan", "zhongli",
 ]
 
 function exportUInt(number, length = 0) {
@@ -63,7 +73,14 @@ function importUInt(string) {
   return result
 }
 
-const exportFixedItem = (item, array, length) => exportUInt(array.indexOf(item), length)
+function exportFixedItem(item, array, length) {
+  let index = array.indexOf(item)
+  if (index === -1) {
+    throw new Error(`Cannot find item ${item}`)
+  }
+
+  return exportUInt(index, length)
+}
 const importFixedItem = (string, array) => array[importUInt(string)]
 
 function exportArtifact(artifact) {
@@ -72,7 +89,7 @@ function exportArtifact(artifact) {
     exportUInt(numStars, 1) +
     exportUInt(level, 1) +
     exportFixedItem(mainStatKey, stats, 1) +
-    exportFixedItem(slotKey, slotKeys, 1) +
+    exportFixedItem(slotKey, slots, 1) +
     substats.map(({key, value}) => {
       let factor = key.endsWith("_") ? 10 : 1 // one decimal place for percentage
       let index = stats.indexOf(key)
@@ -85,7 +102,7 @@ function importArtifact(string) {
   let numStars = importUInt(string.slice(1, 2))
   let level = importUInt(string.slice(2, 3))
   let mainStatKey = importFixedItem(string.slice(3, 4), stats)
-  let slotKey = importFixedItem(string.slice(4, 5), slotKeys)
+  let slotKey = importFixedItem(string.slice(4, 5), slots)
   let substats = string.slice(5).match(/.{3}/g).map(string => {
     let key = stats[importUInt(string.slice(0, 1))]
     let factor = key.endsWith("_") ? 10 : 1 // one decimal place for percentage
@@ -94,10 +111,87 @@ function importArtifact(string) {
 
   return { setKey, numStars, level, slotKey, mainStatKey, substats }
 }
+function exportWeapon(weapon) {
+  let {
+    key: k,
+    levelKey: l,
+    refineIndex: r,
+    overrideMainVal: m,
+    overrideSubVal: s,
+    conditionalNum: c,
+  } = weapon
+  const result = { k, l, r, c }
+  if (m) result.m = m
+  if (s) result.s = s
+  return result
+}
+function importWeapon(data) {
+  let {
+    k: key,
+    l: levelKey,
+    r: refineIndex,
+    m: overrideMainVal = 0,
+    s: overrideSubVal = 0,
+    c: conditionalNum,
+  } = data
+  return { key, levelKey, refineIndex, overrideMainVal, overrideSubVal, conditionalNum }
+}
+function exportCharacter(character) {
+  let result = deepClone(character)
+  let { characterKey, hitMode, reactionMode, constellation, talentLevelKeys, overrideLevel, autoInfused,
+    // Uncompressed
+    levelKey, artifactConditionals, baseStatOverrides, weapon, talentConditionals } = result
+
+  return {
+    c:
+      exportFixedItem(characterKey, characters, 1) +
+      exportFixedItem(hitMode, hitModes, 1) +
+      exportFixedItem(reactionMode, reactionModes, 1) +
+      exportUInt(constellation, 1) +
+      exportUInt(overrideLevel, 2) +
+      exportUInt(autoInfused ? 1 : 0, 1) +
+      exportUInt(talentLevelKeys.auto, 1) +
+      exportUInt(talentLevelKeys.skill, 1) +
+      exportUInt(talentLevelKeys.burst, 1) +
+      'z',
+    ac: artifactConditionals,
+    bo: baseStatOverrides,
+    l: levelKey,
+    tc: talentConditionals,
+    w: exportWeapon(weapon),
+  }
+}
+function importCharacter(data) {
+  const {
+    c: string,
+    ac: artifactConditionals,
+    bo: baseStatOverrides,
+    l: levelKey,
+    tc: talentConditionals,
+    w: weapon,
+  } = data
+
+  const talentLevelKeys = {
+    auto: importUInt(string.slice(7, 8)),
+    skill: importUInt(string.slice(8, 9)),
+    burst: importUInt(string.slice(9, 10)),
+  }
+  return {
+    characterKey: importFixedItem(string.slice(0, 1), characters),
+    hitMode: importFixedItem(string.slice(1,2), hitModes),
+    reactionMode: importFixedItem(string.slice(2, 3), reactionModes),
+    constellation: importUInt(string.slice(3, 4)),
+    overrideLevel: importUInt(string.slice(4, 6)),
+    autoInfused: importUInt(string.slice(6, 7)) === 1,
+    levelKey, artifactConditionals, baseStatOverrides, talentLevelKeys, weapon: importWeapon(weapon), talentConditionals,
+  }
+}
 
 export {
   exportUInt,
   importUInt,
   exportArtifact,
   importArtifact,
+  exportCharacter,
+  importCharacter,
 }
--- src/Util/FlexUtil.js
+++ src/Util/FlexUtil.js
@@ -2,47 +2,40 @@ import ArtifactDatabase from "../Database/ArtifactDatabase";
 import CharacterDatabase from "../Database/CharacterDatabase";
 import { CurrentDatabaseVersion } from "../Database/DatabaseUtil";
 import { deepClone } from "./Util";
-import { exportArtifact, importArtifact } from "./EximUtil"
+import { exportArtifact, importArtifact, importCharacter, exportCharacter } from "./EximUtil"
 
 export function createFlexObj(characterKey) {
   const character = deepClone(CharacterDatabase.get(characterKey))
   if (!character) return null
-  const { levelKey, hitMode, reactionMode, artifactConditionals, baseStatOverrides, weapon, talentLevelKeys, autoInfused, talentConditionals, constellation, overrideLevel } = character
-  const artifacts = []
-  Object.values(character.equippedArtifacts).forEach(id => {
-    const art = deepClone(ArtifactDatabase.get(id))
-    if (!art) return
-    artifacts.push(exportArtifact(art))
-  });
-  const { auto, skill, burst } = talentLevelKeys
 
-  return { dbv: 3, characterKey, levelKey, hitMode, reactionMode, artifactConditionals, baseStatOverrides, weapon, autoInfused, talentConditionals, constellation, overrideLevel, artifacts, tlvl: [auto, skill, burst] }
+  return {
+    v: 1,
+    ...exportCharacter(character),
+
+    // Artifacts
+    a: Object.values(character.equippedArtifacts)
+      .filter(art => art)
+      .map(id => exportArtifact(ArtifactDatabase.get(id)))
+  }
 }
 
 export function parseFlexObj(character) {
-  if (character.dbv === 3) {
-    return parseFlexObj3(character)
-  } else if (character.dbv === 2) {
-    return parseFlexObj2(character)
-  }
+  if (character.v === 1) return parseFlexObj3(character)
+  return parseFlexObjOld(character)
 }
 
 function parseFlexObj3(character) {
-  const { dbv, characterKey, levelKey, hitMode, reactionMode, artifactConditionals, baseStatOverrides, weapon, autoInfused, talentConditionals, constellation, overrideLevel, tlvl, artifacts: artifactStrings } = character
-
-  const artifacts = artifactStrings.map(string => importArtifact(string))
-  const characterkey = character.characterKey
+  const imported = importCharacter(character)
+  const artifacts = character.a.map(string => importArtifact(string))
   artifacts.forEach(art => {
-    art.location = characterkey
-  });
-  const [auto, skill, burst] = tlvl
+    art.location = imported.characterKey
+  })
   return {
     databaseVersion: CurrentDatabaseVersion,
-    characterKey, levelKey, hitMode, reactionMode, artifactConditionals, baseStatOverrides, weapon, autoInfused, talentConditionals, constellation, overrideLevel,
-    talentLevelKeys: { auto, skill, burst }, artifacts
+    artifacts, ...imported
   }
 }
-function parseFlexObj2(character) {
+function parseFlexObjOld(character) {
   const { dbv, characterKey, levelKey, hitMode, reactionMode, artifactConditionals, baseStatOverrides, weapon, autoInfused, talentConditionals, constellation, overrideLevel, tlvl, artifacts } = character
   const characterkey = character.characterKey
   character.artifacts.forEach(art => {
