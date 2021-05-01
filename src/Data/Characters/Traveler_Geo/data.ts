22f1469068597a76950626c40dc6bcc78002683f
aznn8ter <chenhawhsu@gmail.com>
2021-05-01 13:51:59 -0700

    Initial Geo Trav Stuff

---
+++ src/Data/Characters/Traveler_Geo/data.ts
@@ -0,0 +1,57 @@
+import { IFormulaSheet } from "../../../Types/character"
+import { basicDMGFormula } from "../../../Util/FormulaUtil"
+
+export const data = {
+  baseStat: {
+    characterHP: [912, 2342, 3024, 4529, 5013, 5766, 6411, 7164, 7648, 8401, 8885, 9638, 10122, 10875],
+    characterATK: [18, 46, 60, 88, 98, 112, 126, 140, 149, 164, 174, 188, 198, 213],
+    characterDEF: [57, 147, 190, 284, 315, 362, 405, 450, 480, 527, 558, 605, 635, 682]
+  },
+  specializeStat: {
+    key: "atk_",
+    value: [0, 0, 0, 0, 6, 6, 12, 12, 12, 12, 18, 18, 24, 24]
+  },
+  normal: {
+    hitArr: [
+      [44.46, 48.08, 51.7, 56.87, 60.49, 64.63, 70.31, 76, 81.69, 87.89, 94.09, 100.3, 106.5, 112.71, 118.91],
+      [43.43, 46.97, 50.5, 55.55, 59.09, 63.13, 68.68, 74.23, 79.79, 85.85, 91.91, 97.97, 104.03, 110.09, 116.15],
+      [52.98, 57.29, 61.6, 67.76, 72.07, 77, 83.78, 90.55, 97.33, 104.72, 112.11, 119.5, 126.9, 134.29, 141.68],
+      [58.31, 63.05, 67.8, 74.58, 79.33, 84.75, 92.21, 99.67, 107.12, 115.26, 123.4, 131.53, 139.67, 147.8, 155.94],
+      [70.78, 76.54, 82.3, 90.53, 96.29, 102.88, 111.93, 120.98, 130.03, 139.91, 149.79, 159.66, 169.54, 179.41, 189.29],
+    ],
+  },
+  charged: {
+    hitArr: [
+      [55.9, 60.45, 65, 71.5, 76.05, 81.25, 88.4, 95.55, 102.7, 110.5, 118.3, 126.1, 133.9, 141.7, 149.5],
+      [60.72, 65.66, 70.6, 77.66, 82.6, 88.25, 96.02, 103.78, 111.55, 120.02, 128.49, 136.96, 145.44, 153.91, 162.38],
+      [72.24, 78.12, 84, 92.4, 98.28, 105, 114.24, 123.48, 132.72, 142.8, 152.88, 162.96, 173.04, 183.12, 193.2],
+    ],
+  },
+  plunging: {
+    dmg: [63.93, 69.14, 74.34, 81.77, 86.98, 92.93, 101.1, 109.28, 117.46, 126.38, 135.3, 144.22, 153.14, 162.06, 170.98],
+    low: [127.84, 138.24, 148.65, 163.51, 173.92, 185.81, 202.16, 218.51, 234.86, 252.7, 270.54, 288.38, 306.22, 324.05, 341.89],
+    high: [159.68, 172.67, 185.67, 204.24, 217.23, 232.09, 252.51, 272.93, 293.36, 315.64, 337.92, 360.2, 382.48, 404.76, 427.04]
+  },
+  skill: {
+    dmg: [248, 266.6, 285.2, 310, 328.6, 347.2, 372, 396.8, 421.6, 446.4, 471.2, 496, 527, 558, 589],
+  },
+  burst: {
+    dmg: [148, 159.1, 170.2, 185,196.1, 207.2, 222, 236.8, 251.6, 266.4, 281.2, 296, 314.5, 333, 351.5],
+  }
+}
+
+const formula: IFormulaSheet = {
+  normal: Object.fromEntries(data.normal.hitArr.map((percentArr, i) => [i, stats =>
+    basicDMGFormula(percentArr[stats.tlvl.auto], stats, "normal")])),
+  charged: Object.fromEntries(data.charged.hitArr.map((percentArr, i) => [i, stats =>
+    basicDMGFormula(percentArr[stats.tlvl.auto], stats, "charged")])),
+  plunging: Object.fromEntries(Object.entries(data.plunging).map(([key, arr]) => [key, stats => basicDMGFormula(arr[stats.tlvl.auto], stats, "plunging")])),
+  skill: Object.fromEntries(Object.entries(data.skill).map(([name, arr]) =>
+    [name, stats => basicDMGFormula(arr[stats.tlvl.skill], stats, "skill")])),
+  burst: Object.fromEntries(Object.entries(data.burst).map(([name, arr]) =>
+    [name, stats => basicDMGFormula(arr[stats.tlvl.burst], stats, "burst")])),//not optimizationTarget, dont need to precompute
+  passive2: {
+    geoAuto: stats => basicDMGFormula(60, stats, "normal", true),
+  }
+}
+export default formula
--- src/Data/Characters/Traveler_Geo/index.tsx
+++ src/Data/Characters/Traveler_Geo/index.tsx
@@ -1,18 +1,31 @@
 import card from './Traveler_Male_Card.jpg'
 import thumb from './Character_Traveler_Thumb.png'
+import c1 from './Constellation_Invincible_Stonewall.png'
+import c2 from './Constellation_Rockcore_Meltdown.png'
+import c3 from './Constellation_Will_of_the_Rock.png'
+import c4 from './Constellation_Reaction_Force.png'
+import c5 from './Constellation_Meteorite_Impact.png'
+import c6 from './Constellation_Everlasting_Boulder.png'
+import normal from './Talent_Foreign_Rockblade.png'
+import skill from './Talent_Starfell_Sword.png'
+import burst from './Talent_Wake_of_Earth.png'
+import passive1 from './Talent_Shattered_Darkrock.png'
+import passive2 from './Talent_Frenzied_Rockslide.png'
+import ElementalData from '../../ElementalData'
+import Stat from '../../../Stat'
+import formula, { data } from './data'
+import { getTalentStatKey, getTalentStatKeyVariant } from "../../../Build/Build"
 import { ICharacterSheet } from '../../../Types/character';
-// import c1 from './Constellation_Invincible_Stonewall.png'
-// import c2 from './Constellation_Rockcore_Meltdown.png'
-// import c3 from './Constellation_Will_of_the_Rock.png'
-// import c4 from './Constellation_Reaction_Force.png'
-// import c5 from './Constellation_Meteorite_Impact.png'
-// import c6 from './Constellation_Everlasting_Boulder.png'
-// import normal from './Talent_Foreign_Rockblade.png'
-// import skill from './Talent_Starfell_Sword.png'
-// import burst from './Talent_Wake_of_Earth.png'
-// import passive1 from './Talent_Shattered_Darkrock.png'
-// import passive2 from './Talent_Frenzied_Rockslide.png'
+import { IConditionals, IConditionalValue } from '../../../Types/IConditional'
+import { stat } from 'fs'
 
+const conditionals: IConditionals = {
+  c1: { // InvincibleStonewall
+    canShow: stats => stats.constellation >= 1,
+    name: <span>Party members within the radius of <b>Wake of Earth</b>.</span>,
+    stats: { critRate_: 10 }
+  }
+}
 const char: ICharacterSheet = {
   name: "Traveler (Geo)",
   cardImg: card,
@@ -23,17 +36,159 @@ const char: ICharacterSheet = {
   gender: "F/M",
   constellationName: "Viator",//male const
   titles: ["Outlander", "Honorary Knight"],
-  baseStat: {
-    characterHP: [912, 2342, 3024, 4529, 5013, 5766, 6411, 7164, 7648, 8401, 8885, 9638, 10122, 10875],
-    characterATK: [18, 46, 60, 88, 98, 112, 126, 140, 149, 164, 174, 188, 198, 213],
-    characterDEF: [57, 147, 190, 284, 315, 362, 405, 450, 480, 527, 558, 605, 635, 682]
-  },
-  specializeStat: {
-    key: "atk_",
-    value: [0, 0, 0, 0, 6, 6, 12, 12, 12, 12, 18, 18, 24, 24]
-  },
-  formula: {},
-  conditionals: {},
-  talent: {}
+  baseStat: data.baseStat,
+  specializeStat: data.specializeStat,
+  formula,
+  conditionals,
+  talent: {
+    auto: {
+      name: "Foreign Rockblade",
+      img: normal,
+      document: [{
+        text: <span><strong>Normal Attack</strong> Perform up to 5 rapid strikes.</span>,
+        fields: data.normal.hitArr.map((percentArr, i) =>
+        ({
+          text: `${i + 1}-Hit DMG`,
+          formulaText: stats => <span>{percentArr[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("normal", stats), stats)}</span>,
+          formula: formula.normal[i],
+          variant: stats => getTalentStatKeyVariant("normal", stats),
+        }))
+      }, {
+        text: <span><strong>Charged Attack</strong> Consumes a certain amount of Stamina to unleash 2 rapid sword strikes.</span>,
+        fields: data.charged.hitArr.map((percentArr, i) =>
+        ({
+          text: i == 1 ? `Male Charged 2-Hit DMG`: (i == 2 ? `Female Charged 2-Hit DMG` : `Charged ${i + 1}-Hit DMG`),
+          formulaText: stats => <span>{percentArr[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("charged", stats), stats)}</span>,
+          formula: formula.charged[i],
+          variant: stats => getTalentStatKeyVariant("charged", stats),
+        }))
+      }, {
+        text: <span><strong>Plunging Attack</strong> Plugnes from mid-air to strike the ground below, damaing enemies along the path and ealing AoE DMG upon impact.</span>,
+        fields: [{
+          text: "Plunge DMG",
+          formulaText: stats => <span>{data.plunging.dmg[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("plunging", stats), stats)}</span>,
+          formula: formula.plunging.dmg,
+          variant: stats => getTalentStatKeyVariant("plunging", stats),
+        }, {
+          text: "Low Plunge DMG",
+          formulaText: stats => <span>{data.plunging.low[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("plunging", stats), stats)}</span>,
+          formula: formula.plunging.low,
+          variant: stats => getTalentStatKeyVariant("plunging", stats),
+        }, {
+          text: "High Plunge DMG",
+          formulaText: stats => <span>{data.plunging.high[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("plunging", stats), stats)}</span>,
+          formula: formula.plunging.high,
+          variant: stats => getTalentStatKeyVariant("plunging", stats),
+        }]
+      }]
+    },
+    skill: {
+      name: "Starfell Sword",
+      img: skill,
+      document: [{
+        text: <span>
+          <p className="mb-2">You disgorge a meteorite from the depths of the earth, dealing <span className="text-geo">AoE Geo DMG</span>.</p>
+          <p className="mb-2">
+            <strong>Hold:</strong> This skill's positioning may be adjusted.
+          </p>
+        </span>,
+        fields: [{
+          text: "Skill DMG",
+          formulaText: stats => <span>{data.skill.dmg[stats.tlvl.skill]}% {Stat.printStat(getTalentStatKey("skill", stats), stats)}</span>,
+          formula: formula.skill.dmg,
+          variant: stats => getTalentStatKeyVariant("skill", stats),
+        }, {
+          canShow: stats => stats.constellation >= 2,
+          text: "Meteorite Explosion DMG",
+          formulaText: stats => <span>{data.skill.dmg[stats.tlvl.skill]}% {Stat.printStat(getTalentStatKey("skill", stats), stats)}</span>,
+          formula: formula.skill.dmg,
+          variant: stats => getTalentStatKeyVariant("skill", stats),
+        }, {
+          text: "Base CD",
+          value: stats => stats.ascension >= 1 ? "6s" : "8s",
+        }, {
+          text: "Meteorite Duration",
+          value: stats => stats.ascension >= 6 ? "40s" : "30s",
+        }],
+      }],
+    },
+    burst: {
+      name: "Wake of Earth",
+      img: burst,
+      document: [{
+        text: <span>
+          <p className="mb-2">Energizing the Geo deep underground, you set off expanding shockwaves.</p>
+          <p>Launches surrounding opponents back and deals <span className="text-geo">AoE Geo DMG</span>.</p>
+          <p>A stone wall is erected at the edges of the shockwave.</p>
+          <p>The stone wall is considered a Geo Construct, and may be used to block attacks.</p>
+        </span>,
+        fields: [{
+          text: "DMG Per Shockwave",
+          formulaText: stats => <span>{data.burst.dmg[stats.tlvl.burst]}% {Stat.printStat(getTalentStatKey("burst", stats), stats)}</span>,
+          formula: formula.burst.dmg,
+          variant: stats => getTalentStatKeyVariant("burst", stats),
+        }, {
+          text: "Stonewall Duration",
+          value: stats => stats.constellation >= 6 ? "20s" : "15s",
+        }, {
+          text: "CD",
+          value: "15s",
+        }, {
+          text: "Energy Cost",
+          value: 60,
+        }],
+      }],
+    },
+    passive2: {
+      name: "Frenzied Rockslide",
+      img: passive2,
+      document: [{
+        text: <span>The last hit of a Normal Attack combo unleases a collapse blade, dealing 60% of ATK as <span className="text-geo">Aoe Geo DMG</span>.</span>,
+        fields: [{
+          text: "Geo Auto",
+          formulaText: stats => <span>60% * {Stat.printStat("finalATK", stats)}</span>,
+          formula: formula.passive2.geoAuto,
+          variant: stats => getTalentStatKeyVariant("normal", stats, true),
+        }]
+      }]
+    },
+    constellation1: {
+      name: "Invincible Stonewall",
+      img: c1,
+      document: [{ 
+        text: <span>Party members within the radius of <b>Wake of Earth</b> have their CRIT Rate increased by 10% and have increased resistance against interruption.</span>,
+        conditional: conditionals.c1
+      }]
+    },
+    constellation2: {
+      name: "Rockcore Meltdown",
+      img: c2,
+      document: [{
+        text: <span>When the meteorite created by <b>Starfell Sword</b> is destroyed, it will also explode, dealing additional <span className="text-geo">AoE Geo DMG</span> equal to the amount of damage dealt by <b>Starfell Sword</b>.</span>
+      }]
+    },
+    constellation3: {
+      name: "Will of the Rock",
+      img: c3,
+      document: [{ text: <span>Increases the Level of <b>Wake of Earth</b> by 3. Maximum upgrade level is 15.</span> }],
+      stats: { burstBoost: 3 }
+    },
+    constellation4: {
+      name: "Reaction Force",
+      img: c4,
+      document: [{ text: <span>The shockwave triggered by <b>Wake of Earth</b> regenerates 5 Energy for every opponent hit. A maximum of 25 Energy can be regenerated in this manner at any one time.</span> }]
+    },
+    constellation5: {
+      name: "Meteorite Impact",
+      img: c5,
+      document: [{ text: <span>Increases the Level of <b>Starfell Sword</b> by 3. Maximum upgrade level is 15.</span> }],
+      stats: { skillBoost: 3 }
+    },
+    constellation6: {
+      name: "Everlasting Boulder",
+      img: c6,
+      document: [{ text: <span>The barrier created by <b>Wake of Earth</b> lasts 5s longer. The meteorite created by <b>Starfell Sword</b> lasts 10s longer.</span> }]
+    },
+  }
 };
 export default char;
