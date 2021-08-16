23cfb6fe9c21a12a49c3e674ff04dd6b9e0913fe
frzyc <frzyc@users.noreply.github.com>
2021-08-16 13:46:10 -0400

    5.19.3 - Fix Song of Broken Pines Refinement 3 Bonus ATK.

--- src/Data/Weapons/Claymore/SongOfBrokenPines/index.tsx
+++ src/Data/Weapons/Claymore/SongOfBrokenPines/index.tsx
@@ -4,7 +4,7 @@ import { IConditionals } from '../../../../Types/IConditional'
 import { IWeaponSheet } from '../../../../Types/weapon'
 import data_gen from './data_gen.json'
 import img from './Weapon_Song_of_Broken_Pines.png'
-const atk_ = [16, 20, 34, 28, 32]
+const atk_ = [16, 20, 24, 28, 32]
 const atkSPD_ = [12, 15, 18, 21, 24]
 const condAtk_ = [20, 25, 30, 35, 40]
 const conditionals: IConditionals = {
