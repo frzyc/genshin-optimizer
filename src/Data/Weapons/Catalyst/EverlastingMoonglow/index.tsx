e9f01e41fbf425f2fb1ab01b3e56b54320086786
frzyc <frzyc@users.noreply.github.com>
2021-09-23 06:02:34 -0400

    6.1.16 - Disable incorrect Everlasting Moonglow passive calculations, pending system change - Update link in scanner page.

--- src/Data/Weapons/Catalyst/EverlastingMoonglow/index.tsx
+++ src/Data/Weapons/Catalyst/EverlastingMoonglow/index.tsx
@@ -1,14 +1,13 @@
 import { WeaponData } from 'pipeline'
-import Stat from '../../../../Stat'
+// import Stat from '../../../../Stat'
 import { IWeaponSheet } from '../../../../Types/weapon'
-import { KeyPath } from '../../../../Util/KeyPathUtil'
-import { st } from '../../../Characters/SheetUtil'
-import { FormulaPathBase } from '../../../formula'
-import formula, { data } from './data'
+// import { KeyPath } from '../../../../Util/KeyPathUtil'
+// import { FormulaPathBase } from '../../../formula'
+// import formula, { data } from './data'
 import data_gen from './data_gen.json'
 import img from './Weapon_Everlasting_Moonglow.png'
 
-const path = KeyPath<FormulaPathBase>().weapon.EverlastingMoonglow
+// const path = KeyPath<FormulaPathBase>().weapon.EverlastingMoonglow
 const heal_ = [10, 12.5, 15, 17.5, 20]
 
 const weapon: IWeaponSheet = {
@@ -16,14 +15,20 @@ const weapon: IWeaponSheet = {
   img,
   stats: stats => ({
     heal_: heal_[stats.weapon.refineIndex],
-    modifiers: { normal_dmg_: [path.norm()] }
+    // modifiers: { normal_dmg_: [path.norm()] }
   }),
   document: [{
-    fields: [{
-      text: st("increase.atk"),
-      formulaText: stats => <span>{data.hp_conv[stats.weapon.refineIndex]}% {Stat.printStat("finalHP", stats, true)}</span>,
-      formula: formula.norm,
-    }],
+    fields: [
+      // {
+      //   text: Stat.getStatName("normal_dmg_"),
+      //   formulaText: stats => <span>{data.hp_conv[stats.weapon.refineIndex]}% {Stat.printStat("finalHP", stats, true)}</span>,
+      //   formula: formula.norm,
+      //   unit: "%"
+      // }
+      {
+        text: <span className="text-warning">The normal damage increase is not being calculated right now, pending system change.</span>
+      }
+    ],
   }],
 }
 export default weapon
