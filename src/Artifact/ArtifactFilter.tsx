8b97b92d6086721c43237a1b92d27a67a56116aa
frzyc <frzyc@users.noreply.github.com>
2021-10-25 14:47:18 -0400

    7.2.5 - Add mass lock/unlock button to artifact filter.

--- src/Artifact/ArtifactFilter.tsx
+++ src/Artifact/ArtifactFilter.tsx
@@ -1,6 +1,6 @@
 import { faBan, faChartLine, faTrash, faUserShield, faUserSlash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { BusinessCenter, Replay } from "@mui/icons-material"
+import { BusinessCenter, Lock, LockOpen, Replay } from "@mui/icons-material"
 import { Button, CardContent, Divider, Grid, ListItemIcon, ListItemText, MenuItem, Skeleton, Slider, ToggleButton, Typography } from "@mui/material"
 import { Suspense, useCallback, useContext, useEffect, useMemo, useState } from "react"
 import { Trans, useTranslation } from "react-i18next"
@@ -27,12 +27,15 @@ import { initialFilter, sortKeys } from "./ArtifactFilterUtil"
 export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...props }) {
   const { t } = useTranslation(["artifact", "ui"]);
   const database = useContext(DatabaseContext)
-  const { numDelete, numUnequip, numExclude, numInclude } = useMemo(() => {
-    const numDelete = artifacts.reduce((a, art) => a + (art.lock ? 0 : 1), 0)
+  const { numDelete, numUnequip, numExclude, numInclude, numUnlock, numLock } = useMemo(() => {
+    const numUnlock = artifacts.reduce((a, art) => a + (art.lock ? 0 : 1), 0)
+    const numLock = artifacts.length - numUnlock
+    const numDelete = numUnlock
     const numUnequip = artifacts.reduce((a, art) => a + (art.location ? 1 : 0), 0)
     const numExclude = artifacts.reduce((a, art) => a + (art.exclude ? 1 : 0), 0)
     const numInclude = artifacts.length - numExclude
-    return { numDelete, numUnequip, numExclude, numInclude }
+
+    return { numDelete, numUnequip, numExclude, numInclude, numUnlock, numLock }
   }, [artifacts])
 
   const { filterArtSetKey, filterSlotKey, filterMainStatKey, filterStars, filterLevelLow, filterLevelHigh, filterSubstats = initialFilter().filterSubstats,
@@ -65,6 +68,14 @@ export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...
   const includeArtifacts = () =>
     window.confirm(`Are you sure you want to include ${numExclude} artifacts in build generations?`) &&
     artifacts.map(art => database.updateArt({ exclude: false }, art.id))
+
+  const lockArtifacts = () =>
+    window.confirm(`Are you sure you want to lock ${numLock} artifacts?`) &&
+    artifacts.map(art => database.updateArt({ lock: true }, art.id))
+
+  const unlockArtifacts = () =>
+    window.confirm(`Are you sure you want to unlock ${numUnlock} artifacts?`) &&
+    artifacts.map(art => database.updateArt({ lock: false }, art.id))
 
   const [sliderLow, setsliderLow] = useState(filterLevelLow)
   const [sliderHigh, setsliderHigh] = useState(filterLevelHigh)
@@ -105,7 +116,7 @@ export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...
             {/* Artiface level filter */}
             <CardLight sx={{ width: "100%", display: "flex", alignItems: "center" }}>
               <CustomNumberInput
-                value={filterLevelLow}
+                value={sliderLow || filterLevelLow}
                 onChange={val => filterDispatch({ filterLevelLow: clamp(val, 0, filterLevelHigh) })}
                 sx={{ pl: 2, width: 100, }}
                 inputProps={{ sx: { textAlign: "center" } }}
@@ -120,7 +131,7 @@ export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...
                 min={0} max={20} step={1} marks
               />
               <CustomNumberInput
-                value={filterLevelHigh}
+                value={sliderHigh || filterLevelHigh}
                 onChange={val => filterDispatch({ filterLevelHigh: clamp(val, filterLevelLow, 20) })}
                 sx={{ px: 1, width: 50, }}
                 inputProps={{ sx: { textAlign: "center" } }}
@@ -215,7 +226,7 @@ export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...
             </Grid>
           </Grid>
         </Grid>
-        <Grid container spacing={1}>
+        <Grid container spacing={1} alignItems="center">
           <Grid item xs={12} sm={6} md={3}>
             <Button fullWidth color="error" disabled={!numUnequip} onClick={unequipArtifacts} startIcon={<FontAwesomeIcon icon={faUserSlash} />}>
               <Trans t={t} i18nKey="button.unequipArtifacts" >Unequip Artifacts</Trans>
@@ -230,18 +241,32 @@ export default function ArtifactFilter({ artifacts, filters, filterDispatch, ...
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Button fullWidth color="error" disabled={!numInclude} onClick={excludeArtifacts} startIcon={<FontAwesomeIcon icon={faBan} />}>
-              <Trans t={t} i18nKey="button.excludeArtifacts" >Lock Artifacts</Trans>
+              <Trans t={t} i18nKey="button.excludeArtifacts" >Exclude Artifacts</Trans>
               <SqBadge sx={{ ml: 1 }} color={numInclude ? "success" : "secondary"}>{numInclude}</SqBadge>
             </Button>
           </Grid>
           <Grid item xs={12} sm={6} md={3}>
             <Button fullWidth color="error" disabled={!numExclude} onClick={includeArtifacts} startIcon={<FontAwesomeIcon icon={faChartLine} />}>
-              <Trans t={t} i18nKey="button.includeArtifacts" >Unlock Artifacts</Trans>
+              <Trans t={t} i18nKey="button.includeArtifacts" >Include Artifacts</Trans>
               <SqBadge sx={{ ml: 1 }} color={numExclude ? "success" : "secondary"}>{numExclude}</SqBadge>
             </Button>
           </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Button fullWidth color="error" disabled={!numLock} onClick={unlockArtifacts} startIcon={<LockOpen />}>
+              <Trans t={t} i18nKey="button.unlockrtifacts" >Unlock Artifacts</Trans>
+              <SqBadge sx={{ ml: 1 }} color={numLock ? "success" : "secondary"}>{numLock}</SqBadge>
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <Button fullWidth color="error" disabled={!numUnlock} onClick={lockArtifacts} startIcon={<Lock />}>
+              <Trans t={t} i18nKey="button.lockArtifacts" >Lock Artifacts</Trans>
+              <SqBadge sx={{ ml: 1 }} color={numUnlock ? "success" : "secondary"}>{numUnlock}</SqBadge>
+            </Button>
+          </Grid>
+          <Grid item xs={12} sm={12} md={6}>
+            <Typography variant="caption" color="text.secondary"><Trans t={t} i18nKey="buttonHint">Note: the red buttons above only applies to <b>filtered artifacts</b></Trans></Typography>
+          </Grid>
         </Grid>
-        <Typography variant="caption" color="text.secondary"><Trans t={t} i18nKey="buttonHint">Note: the above buttons only applies to <b>filtered artifacts</b></Trans></Typography>
       </CardContent>
     </CardDark>
   </Suspense>
