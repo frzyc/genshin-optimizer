1fdd22dae183993ff8dcc6194d6f80301b625d6b
lantua <16190491+lantua@users.noreply.github.com>
2021-12-29 06:34:51 -0500

    Add bare minimum formula `index`

---
+++ src/Formula/index.ts
@@ -0,0 +1,111 @@
+import { transformativeReactionLevelMultipliers } from "../StatConstants"
+import { allMainStatKeys, allSubstats } from "../Types/artifact"
+import { allArtifactSets, allElementsWithPhy } from "../Types/consts"
+import { objectFromKeyMap } from "../Util/Util"
+import type { ConstantNode, Data, ReadNode } from "./type"
+import { frac, prod, res, subscript, sum, min, max, read, input as i, setReadNodeKeys, stringRead } from "./utils"
+
+const allStats = [...allMainStatKeys, ...allSubstats] as const
+const unit: ConstantNode = { operation: "const", value: 1, info: { unit: "%" }, operands: [] }
+
+// All string read nodes
+const str = setReadNodeKeys({
+  character: stringRead(),
+  weaponType: stringRead(),
+  dmg: {
+    element: stringRead(),
+    reaction: stringRead(),
+    move: stringRead(),
+    critType: stringRead(),
+  }
+})
+
+// All read nodes
+const rd = setReadNodeKeys({
+  base: objectFromKeyMap(["atk", "def", "hp"] as const, _ => read("add")),
+  premod: objectFromKeyMap(allStats, _ => read("add")),
+  postmod: {
+    ...objectFromKeyMap(allStats, _ => read("add")),
+    cappedCritRate: read("unique"),
+  },
+
+  art: objectFromKeyMap(allArtifactSets, _ => read("add")),
+  char: {
+    auto: read("add"), skill: read("add"), burst: read("add"),
+    level: read("unique"), constellation: read("unique"), ascension: read("unique"),
+  },
+  weapon: { level: read("unique"), rank: read("unique"), },
+
+  hit: {
+    base: read("add"),
+    dmgBonus: read("add"),
+    amp: { reactionMulti: read("add"), multi: read("add"), base: read("add"), },
+    critValue: {
+      byType: read("unique", undefined, str.dmg.critType),
+    },
+  },
+
+  trans: {
+    dmg: read("add"),
+    reactionMulti: read("unique"),
+    base: read("add"),
+    lvlMulti: read("unique"),
+  },
+  enemy: {
+    res: { byElement: read("unique", undefined, str.dmg.element), },
+    level: read("unique"),
+    def: read("add"),
+    defRed: read("add"),
+  },
+})
+
+const { base, premod, postmod, char, hit, trans, enemy, } = rd
+
+const common = {
+  premod: objectFromKeyMap(["atk", "def", "hp"] as const,
+    key => prod(base[key], premod[`${key}_` as const])),
+  postmod: {
+    ...objectFromKeyMap(allStats, key => premod[key]),
+    cappedCritRate: max(min(postmod.critRate_, unit), 0),
+  },
+
+  trans: {
+    dmg: prod(trans.reactionMulti, trans.base, trans.lvlMulti, enemy.res.byElement),
+    base: sum(unit, prod(16, frac(postmod.eleMas, 2000))),
+    lvlMulti: subscript(char.level, transformativeReactionLevelMultipliers),
+  },
+
+  hit: {
+    dmg: prod(
+      hit.base,
+      sum(unit, hit.dmgBonus),
+      hit.critValue.byType,
+      enemy.def,
+      enemy.res.byElement,
+      hit.amp.multi),
+    critValue: {
+      base: unit,
+      dmg: sum(unit, postmod.critDMG_),
+      avg: sum(unit, prod(postmod.cappedCritRate, postmod.critDMG_)),
+    },
+    amp: {
+      multi: prod(hit.amp.reactionMulti, hit.amp.base),
+      base: sum(unit, prod(25 / 9, frac(postmod.eleMas, 1400))),
+    },
+  },
+
+  enemy: {
+    def: frac(sum(char.level, 100), prod(sum(enemy.level, 100), sum(1, prod(-1, enemy.defRed))))
+  }
+} as const
+
+const generationInput = {
+  generation: {
+    ...objectFromKeyMap(allStats, stat => i(stat)),
+    ...objectFromKeyMap(allArtifactSets, set => i(set))
+  }
+}
+
+export {
+  rd as input, common, generationInput
+}
