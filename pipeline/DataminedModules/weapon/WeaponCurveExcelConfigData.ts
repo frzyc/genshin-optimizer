018af19493346ac78eb4af988ee166f56ca370bc
lantua <16190491+lantua@users.noreply.github.com>
2022-01-10 10:07:16 -0500

    Revert gen file

--- pipeline/DataminedModules/weapon/WeaponCurveExcelConfigData.ts
+++ pipeline/DataminedModules/weapon/WeaponCurveExcelConfigData.ts
@@ -108,7 +108,7 @@ export type WeaponExpCurveData = Record<WeaponGrowCurveKey, { [level: number]: n
 const weaponCurveExcelConfigData = {} as WeaponExpCurveData
 weaponCurveExcelConfigDataSrc.forEach(({ Level, CurveInfos }) =>
   CurveInfos.forEach(({ Type, Value }) => {
-    if (!weaponCurveExcelConfigData[Type]) weaponCurveExcelConfigData[Type] = [0]
+    if (!weaponCurveExcelConfigData[Type]) weaponCurveExcelConfigData[Type] = {} // TODO: [0]
     weaponCurveExcelConfigData[Type][Level] = extrapolateFloat(Value)
   }))
 
