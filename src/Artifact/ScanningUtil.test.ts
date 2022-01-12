7f15e4d6ce917b8247a78ce446facd1c308000d8
Lucas Pollice <lp1935@nyu.edu>
2022-01-11 22:03:18 -0500

    Update ScanningUtil.test.ts

--- src/Artifact/ScanningUtil.test.ts
+++ src/Artifact/ScanningUtil.test.ts
@@ -5,9 +5,14 @@ import {
   parseMainStatKeys,
   parseMainStatValues,
 } from "./ScanningUtil"
-import { createCanvas, loadImage, createImageData, ImageData } from 'canvas'
+import { createCanvas, loadImage, createImageData, ImageData, registerFont } from 'canvas'
 import { ArtifactSheet } from "./ArtifactSheet"
-import { ArtifactRarity, ArtifactSetKey, CharacterKey, ElementKey, SetNum, SlotKey } from "../Types/consts";
+import { IArtifact } from "../Types/artifact";
+import { writeFile } from 'fs/promises'
+import { tmpdir } from "os";
+import { resolve } from "path";
+import { StatData } from "../StatData";
+import Artifact from "./Artifact";
 
 
 class MockImageData extends ImageData {
@@ -17,17 +22,48 @@ class MockImageData extends ImageData {
   }
 }
 
-window.ImageData = MockImageData as typeof window.ImageData 
+window.ImageData = MockImageData as typeof window.ImageData
 
-async function generateTestArtifact() {
+registerFont('./ja-jp.ttf', { family: 'serif' })
+
+async function generateTestArtifact(artifact: IArtifact) {
   const width = 1009, height = 1144
-  // registerFont('./ja-jp.ttf', { family: 'sans-serif' })
   const canvas = createCanvas(1009, 1144)
   const ctx = canvas.getContext('2d')
 
-  const background = await loadImage('./baseline.png')
+  const background = await loadImage('./background.png')
 
   ctx.drawImage(background, 0, 0, width, height)
+
+  const l = 69
+
+  ctx.font = "65px SDK_JP_Web, serif"
+  ctx.fillStyle = "#fff"
+  ctx.fillText("Fake Artifact", l, 95)
+  ctx.font = "41px SDK_JP_Web, serif"
+  ctx.fillText(`${Artifact.slotName(artifact.slotKey)}`, l, 195)
+  ctx.font = "86px SDK_JP_Web, serif"
+  ctx.fillText(`${Artifact.mainStatValue(artifact.mainStatKey, artifact.rarity, artifact.level)}`, l, 450)
+  ctx.font = "48px SDK_JP_Web, serif"
+  ctx.textAlign = "center"
+  ctx.fillText(`+${artifact.level}`, l+57, 678)
+  ctx.textAlign = "left"
+  ctx.fillStyle = "#bfafa8"
+  ctx.font = "45px SDK_JP_Web, serif"
+  ctx.fillText(StatData[artifact.mainStatKey].name, l, 360)
+  ctx.fillStyle = "#495366"
+  ctx.font = "47px SDK_JP_Web, serif"
+  artifact.substats.forEach((substat, i) => {
+    const {name, unit} = StatData[substat.key]
+    ctx.fillText(`·${name}+${substat.value}${unit ?? ''}`, l, 781 + 77 * i)
+  })
+  ctx.fillStyle = "#5cb256"
+  ctx.font = "46px SDK_JP_Web, serif"
+  ctx.fillText(`${artifact.setKey}:(0)`, l, 1089)
+
+  const previewPath = resolve(tmpdir(), `${Date.now()}.png`)
+  console.log(previewPath)
+  writeFile(previewPath, canvas.toBuffer())
 
   return ctx.getImageData(0, 0, width, height)
 }
@@ -35,19 +71,7 @@ async function generateTestArtifact() {
 describe('processEntry', () => {
   test('default', async () => {
     try {
-      const sheets = await ArtifactSheet.getAll()
-      const ocrResult = await ocr(await generateTestArtifact())
-
-      const [artifact, texts] = findBestArtifact(
-        sheets, ocrResult.rarities,
-        parseSetKeys(ocrResult.artifactSetTexts, sheets),
-        parseSlotKeys(ocrResult.whiteTexts),
-        parseSubstats(ocrResult.substatTexts),
-        parseMainStatKeys(ocrResult.whiteTexts),
-        parseMainStatValues(ocrResult.whiteTexts)
-      )
-
-      expect(artifact).toStrictEqual({
+      const subject: IArtifact = {
         "exclude": false,
         "level": 8,
         "location": "",
@@ -74,7 +98,20 @@ describe('processEntry', () => {
             "value": 269,
           },
         ],
-      })
+      }
+      const sheets = await ArtifactSheet.getAll()
+      const ocrResult = await ocr(await generateTestArtifact(subject))
+
+      const [artifact, texts] = findBestArtifact(
+        sheets, ocrResult.rarities,
+        parseSetKeys(ocrResult.artifactSetTexts, sheets),
+        parseSlotKeys(ocrResult.whiteTexts),
+        parseSubstats(ocrResult.substatTexts),
+        parseMainStatKeys(ocrResult.whiteTexts),
+        parseMainStatValues(ocrResult.whiteTexts)
+      )
+
+      expect(artifact).toStrictEqual(subject)
     } catch (e) {
       console.error(e)
       await new Promise(resolve => setTimeout(resolve, 10000))
