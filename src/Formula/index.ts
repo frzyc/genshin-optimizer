9097e907dc0c5f2eebe186fac2bdf590e1226056
lantua <16190491+lantua@users.noreply.github.com>
2022-02-11 13:45:22 -0500

    Add `elemental` dmg calculation

--- src/Formula/index.ts
+++ src/Formula/index.ts
@@ -9,7 +9,7 @@ const asConst = true as const, pivot = true as const
 
 const allElements = allElementsWithPhy
 const allTalents = ["auto", "skill", "burst"] as const
-const allMoves = ["normal", "charged", "plunging", "skill", "burst"] as const
+const allMoves = ["normal", "charged", "plunging", "skill", "burst", "elemental"] as const
 const allMainSubStats = [...new Set([...allMainStatKeys, ...allSubstats] as const)]
 const allTransformative = ["overloaded", "shattered", "electrocharged", "superconduct", "swirl"] as const
 const allAmplifying = ["vaporize", "melt"] as const
