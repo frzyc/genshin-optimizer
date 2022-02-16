350fe5e316afd2c8a7669ee7888ebb085cbb9429
Ryan <mineminemine@users.noreply.github.com>
2022-02-17 02:33:39 +0800

    Adds a bunch of polearms (#100)
    * Add Blackcliff Pole
    
    * Add Black Tassel
    
    * remove unnecessary tings in black tassel sheet
    
    * added Crescent Pike
    
    * remove file

--- src/Data/Weapons/Polearm/BlackcliffPole/index.tsx
+++ src/Data/Weapons/Polearm/BlackcliffPole/index.tsx
@@ -1,22 +1,59 @@
 import { WeaponData } from 'pipeline'
-import { IWeaponSheet } from '../../../../Types/weapon'
-import data_gen from './data_gen.json'
-import icon from './Icon.png'
+import ImgIcon from '../../../../Components/Image/ImgIcon'
+import { Translate } from '../../../../Components/Translate'
+import { input } from '../../../../Formula'
+import { constant, lookup, prod, subscript } from '../../../../Formula/utils'
+import { WeaponKey } from '../../../../Types/consts'
+import { objectKeyMap } from '../../../../Util/Util'
+import { cond, trans } from '../../../SheetUtil'
+import { dataObjForWeaponSheet } from '../../util'
+import WeaponSheet, { IWeaponSheet } from '../../WeaponSheet'
 import iconAwaken from './AwakenIcon.png'
-const refinementVals = [12, 15, 18, 21, 24]
-const weapon: IWeaponSheet = {
-  ...data_gen as WeaponData,
+import data_gen_json from './data_gen.json'
+import icon from './Icon.png'
+const key: WeaponKey = "BlackcliffPole"
+const data_gen = data_gen_json as WeaponData
+const [tr] = trans("weapon", key)
+export const st = (strKey: string) => <Translate ns="sheet" key18={strKey} />
+export const sgt = (strKey: string) => <Translate ns="sheet_gen" key18={strKey} />
+
+const [condPassivePath, condPassive] = cond(key, "PressTheAdvantage")
+const opponentsDefeated = [1, 2, 3]
+const atkInc = [0.12, 0.15, 0.18, 0.21, 0.24]
+const atk_ = prod(lookup(condPassive, objectKeyMap(opponentsDefeated, i => constant(i)), 0),
+  subscript(input.weapon.refineIndex, atkInc))
+
+const data = dataObjForWeaponSheet(key, data_gen, {
+  premod: {
+    atk_: atk_
+  }
+})
+
+const sheet: IWeaponSheet = {
   icon,
   iconAwaken,
   document: [{
     conditional: {
-      key: "pa",
-      name: "Opponents Defeated",
-      maxStack: 3,
-      stats: stats => ({
-        atk_: refinementVals[stats.weapon.refineIndex]
-      })
+      value: condPassive,
+      path: condPassivePath,
+      header: {
+        title: tr(`passiveName`),
+        icon: data => <ImgIcon size={2} sx={{ m: -1 }} src={data.get(input.weapon.asc).value < 2 ? icon : iconAwaken} />,
+      },
+      description: data => tr(`passiveDescription.${data.get(input.weapon.refineIndex).value}`),
+      name: st("afterDefeatEnemy"),
+      states:
+        Object.fromEntries(opponentsDefeated.map(c => [c, {
+          name: `${c}`,
+          fields: [{
+            node: atk_,
+          }, {
+            text: sgt("duration"),
+            value: 30,
+            unit: "s"
+          }]
+        }]))
     }
   }],
 }
-export default weapon
+export default new WeaponSheet(key, sheet, data_gen, data)
