71e5e91fcd0e0a91ee265c413cac5cd4a6b533d2
frzyc <frzyc@users.noreply.github.com>
2022-02-18 06:33:24 -0500

    prevent irrevelant artifacts from showing in optimization target

--- src/PageBuild/Components/OptimizationTargetSelector.tsx
+++ src/PageBuild/Components/OptimizationTargetSelector.tsx
@@ -56,7 +56,7 @@ export default function OptimizationTargetSelector({ optimizationTarget, setTarg
 function SelectorSection({ displayNs, sectionKey, setTarget }: { displayNs: DisplaySub<NodeDisplay>, sectionKey: string, setTarget: (target: string[]) => void }) {
   const { data } = useContext(DataContext)
   const header = usePromise(getDisplayHeader(data, sectionKey), [data, sectionKey])
-  if (!header) return null
+  if (!header || Object.values(displayNs).every(n => n.isEmpty)) return null
   return <CardLight key={sectionKey as string}>
     <CardHeader avatar={header.icon && <ImgIcon size={2} sx={{ m: -1 }} src={header.icon} />} title={header.title} action={header.action} titleTypographyProps={{ variant: "subtitle1" }} />
     <Divider />
