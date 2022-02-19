0802fcf86dc1a070a0df2b268dda8ecdc2f91746
Ryan <mineminemine@users.noreply.github.com>
2022-02-20 05:32:55 +0800

    Add artifacts and fix Yae incorect charged attack name (#108)
    * fix yae wrong attack name
    
    * add artifacts
    
    * fix for pale flame set

--- src/Data/Artifacts/TravelingDoctor/data.ts
+++
@@ -1,7 +0,0 @@
-import { ISubFormula } from "../../../Types/character"
-import { basicHealingFormula } from "../../../Util/FormulaUtil"
-
-const formula: ISubFormula = {
-  regen: stats => basicHealingFormula(30, "finalHP")
-}
-export default formula
