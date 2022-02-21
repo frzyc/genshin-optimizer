8bf56275c42d49ae8d504debb9607c983f237548
Lucas Pollice <lp1935@nyu.edu>
2022-02-20 22:11:57 -0500

    remove data.ts

--- src/Data/Characters/Yoimiya/index.tsx
+++ src/Data/Characters/Yoimiya/index.tsx
@@ -8,13 +8,76 @@ import { cond, sgt, st, trans } from '../../SheetUtil'
 import CharacterSheet, { ICharacterSheet, normalSrc, talentTemplate } from '../CharacterSheet'
 import { dataObjForCharacterSheet, dmgNode } from '../dataUtil'
 import { banner, burst, c1, c2, c3, c4, c5, c6, card, passive1, passive2, passive3, skill, thumb, thumbSide } from './assets'
-import { data as datamine } from './data'
 import data_gen_src from './data_gen.json'
+import skillParam_gen from './skillParam_gen.json'
+
 
 const characterKey: CharacterKey = "Yoimiya"
 const elementKey: ElementKey = "pyro"
 const data_gen = data_gen_src as CharacterData
 const [tr, charTr] = trans("char", characterKey)
+
+const datamine = {
+  normal: {
+    hitArr: [
+      skillParam_gen.auto[0],//x2
+      skillParam_gen.auto[1],
+      skillParam_gen.auto[2],
+      skillParam_gen.auto[3],//x2
+      skillParam_gen.auto[4],
+    ]
+  },
+  charged: {
+    hit: skillParam_gen.auto[5],
+    full: skillParam_gen.auto[6],
+    kindling: skillParam_gen.auto[7],
+  },
+  plunging: {
+    dmg: skillParam_gen.auto[8],
+    low: skillParam_gen.auto[9],
+    high: skillParam_gen.auto[10],
+  },
+  skill: {
+    dmg_: skillParam_gen.skill[3],
+    duration: 10,
+    cd: 18
+  },
+  burst: {
+    dmg: skillParam_gen.burst[0],
+    exp: skillParam_gen.burst[1],
+    duration: 10,
+    cd: 15,
+    cost: 60
+  },
+  passive1: {
+    pyro_dmg_: skillParam_gen.passive1[0][0],
+    duration: skillParam_gen.passive1[1][0],
+    maxStacks: 10,
+  },
+  passive2: {
+    fixed_atk_: skillParam_gen.passive2[0][0],
+    var_atk_: skillParam_gen.passive2[1][0],
+    duration: skillParam_gen.passive2[2][0],
+  },
+  constellation1: {
+    burst_durationInc: skillParam_gen.constellation1[0],
+    atk_: skillParam_gen.constellation1[1],
+    duration: skillParam_gen.constellation1[2]
+  },
+  constellation2: {
+    pyro_dmg_: skillParam_gen.constellation2[0],
+    duration: skillParam_gen.constellation2[1],
+  },
+  constellation3: {},
+  constellation4: {
+    cdRed: skillParam_gen.constellation4[0]
+  },
+  constellation5: {},
+  constellation6: {
+    chance: skillParam_gen.constellation6[0],
+    dmg_: skillParam_gen.constellation6[1],
+  },
+} 
 
 const [condSkillPath, condSkill] = cond(characterKey, "skill")
 const [condA1Path, condA1] = cond(characterKey, "a1")
@@ -30,15 +93,14 @@ const c1atk_ = equal(condC1, 'c1', percent(datamine.constellation1.atk_))
 const c2pyro_dmg_ = equal(condC2, 'c2', percent(datamine.constellation2.pyro_dmg_), { key: 'pyro_dmg_' })
 
 const canShowC6 = uiData => uiData.get(input.constellation).value >= 6 && uiData.get(condSkill).value === 'skill'
+const canShowA4 = uiData => uiData.get(input.asc).value >= 4
 
 const normalEntries = datamine.normal.hitArr.map((arr, i) =>
   [i, prod(normal_dmgMult, dmgNode("atk", arr, "normal", { hit: { ele: matchFull(condSkill, "skill", constant(elementKey), constant("physical")) } }))])
-
 const kindlingEntries = normalEntries.map(([_, node], i, arr) => [i + arr.length, (prod(prod(percent(datamine.constellation6.dmg_), percent(datamine.constellation6.chance)), node))])
 
 
 export const dmgFormulas = {
-  // TODO: Provide premod multiplicative damage bonuses e.g. normal_dmgMult
   normal: Object.fromEntries([...normalEntries, ...kindlingEntries]),
   charged: {
     hit: dmgNode("atk", datamine.charged.hit, "charged"),
@@ -190,10 +252,10 @@ const sheet: ICharacterSheet = {
         }
       ),
       passive2: talentTemplate("passive2", tr, passive2, [{
-        canShow: uiData => uiData.get(input.asc).value >= 4,
+        canShow: canShowA4,
         node: infoMut(atk_, { key: `char_${characterKey}_gen:passive2.name` })
       }, {
-        canShow: uiData => uiData.get(input.asc).value >= 4,
+        canShow: canShowA4,
         text: sgt("duration"),
         value: datamine.passive2.duration,
         unit: "s"
