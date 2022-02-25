a6829643ca46663333e110a656b929830221a4a1
Ryan <mineminemine@users.noreply.github.com>
2022-02-25 16:18:35 +0800

    add a bunch of claymores

--- src/Data/Weapons/Claymore/PrototypeArchaic/index.tsx
+++ src/Data/Weapons/Claymore/PrototypeArchaic/index.tsx
@@ -1,23 +1,30 @@
 import { WeaponData } from 'pipeline'
-import { getTalentStatKey, getTalentStatKeyVariant } from '../../../../PageBuild/Build'
-import Stat from '../../../../Stat'
-import { IWeaponSheet } from '../../../../Types/weapon'
-import formula, { data } from './data'
-import data_gen from './data_gen.json'
-import icon from './Icon.png'
+import { input } from '../../../../Formula'
+import { constant, prod, subscript } from '../../../../Formula/utils'
+import { WeaponKey } from '../../../../Types/consts'
+import { customDmgNode } from '../../../Characters/dataUtil'
+import { dataObjForWeaponSheet } from '../../util'
+import WeaponSheet, { IWeaponSheet } from '../../WeaponSheet'
 import iconAwaken from './AwakenIcon.png'
-import { st } from '../../../Characters/SheetUtil'
-const weapon: IWeaponSheet = {
-  ...data_gen as WeaponData,
+import data_gen_json from './data_gen.json'
+import icon from './Icon.png'
+
+const key: WeaponKey = "PrototypeArchaic"
+const data_gen = data_gen_json as WeaponData
+const dmg_Src = [1, 1.25, 1.5, 1.75, 2]
+
+// TODO: Is this the correct formula for the dmg dealt?
+const dmg_ = customDmgNode(prod(subscript(input.weapon.refineIndex, dmg_Src, { key: "_" }), input.premod.atk), "elemental", {
+  hit: { ele: constant("physical") }
+})
+
+const data = dataObjForWeaponSheet(key, data_gen)
+
+const sheet: IWeaponSheet = {
   icon,
   iconAwaken,
   document: [{
-    fields: [{
-      text: st("dmg"),
-      formulaText: stats => <span>{data.dmg[stats.weapon.refineIndex]}% {Stat.printStat(getTalentStatKey("physical", stats), stats)}</span>,
-      formula: formula.dmg,
-      variant: stats => getTalentStatKeyVariant("physical", stats),
-    }]
-  }]
+    fields: [{ node: dmg_ }],
+  }],
 }
-export default weapon
+export default new WeaponSheet(key, sheet, data_gen, data)
--- src/Data/Weapons/Claymore/RedhornStonethresher/index.tsx
+++ src/Data/Weapons/Claymore/RedhornStonethresher/index.tsx
@@ -1,39 +1,44 @@
 import { WeaponData } from 'pipeline'
-import { getTalentStatKey } from '../../../../PageBuild/Build'
-import ColorText from '../../../../Components/ColoredText'
-import { Translate } from '../../../../Components/Translate'
-import Stat from '../../../../Stat'
-import { IWeaponSheet } from '../../../../Types/weapon'
+import { input } from '../../../../Formula'
+import { prod, subscript } from '../../../../Formula/utils'
+import { WeaponKey } from '../../../../Types/consts'
+import { dataObjForWeaponSheet } from '../../util'
+import WeaponSheet, { IWeaponSheet } from '../../WeaponSheet'
 import iconAwaken from './AwakenIcon.png'
-import formula, { data } from './data'
-import data_gen from './data_gen.json'
+import data_gen_json from './data_gen.json'
 import icon from './Icon.png'
-const def_ = [28, 35, 42, 49, 56]
-const weapon: IWeaponSheet = {
-  ...data_gen as WeaponData,
+
+const key: WeaponKey = "SacrificialSword"
+const data_gen = data_gen_json as WeaponData
+
+const def_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
+const normal_dmg_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
+const charged_dmg_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
+const def_ = subscript(input.weapon.refineIndex, def_Src)
+// TODO: Should these be premod or total
+const normal_dmg_ = prod(subscript(input.weapon.refineIndex, normal_dmg_Src), input.premod.def)
+const charged_dmg_ = prod(subscript(input.weapon.refineIndex, charged_dmg_Src), input.premod.def)
+
+const data = dataObjForWeaponSheet(key, data_gen, {
+  premod: {
+    def_,
+    // TODO: Should these be premod or total
+    normal_dmg_,
+    charged_dmg_
+  },
+})
+
+const sheet: IWeaponSheet = {
   icon,
   iconAwaken,
-  stats: stats => ({
-    def_: def_[stats.weapon.refineIndex]
-  }),
   document: [{
-    conditional: {
-      key: "e",
-      name: <Translate ns="weapon_RedhornStonethresher_gen" key18="passiveName" />,
-      fields: [{
-        text: <Translate ns="weapon_RedhornStonethresher" key18="nameNormal" />,
-        formulaText: stats => <span>{data.defConv[stats.weapon.refineIndex]}% {Stat.printStat("finalDEF", stats, true)} * {Stat.printStat(getTalentStatKey("normal", stats) + "_multi", stats)}</span>,
-        formula: formula.normal,
-        variant: stats => stats.characterEle
-      }, {
-        text: <Translate ns="weapon_RedhornStonethresher" key18="nameCharged" />,
-        formulaText: stats => <span>{data.defConv[stats.weapon.refineIndex]}% {Stat.printStat("finalDEF", stats, true)} * {Stat.printStat(getTalentStatKey("charged", stats) + "_multi", stats)}</span>,
-        formula: formula.charged,
-        variant: stats => stats.characterEle
-      }, {
-        text: <ColorText color="warning">The normal and charged DMG increase is not currently being added to the character's skill damage as a singular damage number, pending future implemention.</ColorText>
-      }],
-    }
+    fields: [{
+      node: def_
+    }, {
+      node: normal_dmg_
+    }, {
+      node: charged_dmg_
+    }]
   }],
 }
-export default weapon
+export default new WeaponSheet(key, sheet, data_gen, data)
