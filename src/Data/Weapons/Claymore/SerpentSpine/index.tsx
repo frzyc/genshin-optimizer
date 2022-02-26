13ba15d012f17a787eee6eebfef113ef272ab301
Ryan <mineminemine@users.noreply.github.com>
2022-02-27 02:03:44 +0800

    use st for name

--- src/Data/Weapons/Claymore/SerpentSpine/index.tsx
+++ src/Data/Weapons/Claymore/SerpentSpine/index.tsx
@@ -3,7 +3,7 @@ import { input } from '../../../../Formula'
 import { lookup, naught, prod, subscript } from '../../../../Formula/utils'
 import { WeaponKey } from '../../../../Types/consts'
 import { objectKeyMap, range } from '../../../../Util/Util'
-import { cond, trans } from '../../../SheetUtil'
+import { cond, st, trans } from '../../../SheetUtil'
 import { dataObjForWeaponSheet } from '../../util'
 import WeaponSheet, { conditionaldesc, conditionalHeader, IWeaponSheet } from '../../WeaponSheet'
 import iconAwaken from './AwakenIcon.png'
@@ -41,14 +41,8 @@ const sheet: IWeaponSheet = {
       description: conditionaldesc(tr),
       name: trm("condName"),
       states: {
-        1: {
-          name: `1 Stack`,
-          fields: [{
-            node: all_dmg_stack
-          }]
-        },
         ...objectKeyMap(range(1, 5), i => ({
-          name: `${i} Stacks`,
+          name: st("stack", { count: i }),
           fields: [{
             node: all_dmg_stack
           }, {
