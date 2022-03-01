ca62e9d10b1b7ce07bf921dae52904219c077047
Ryan <mineminemine@users.noreply.github.com>
2022-03-01 15:11:26 +0800

    fix redhorn

--- src/Data/Weapons/Claymore/RedhornStonethresher/index.tsx
+++ src/Data/Weapons/Claymore/RedhornStonethresher/index.tsx
@@ -12,20 +12,21 @@ const key: WeaponKey = "RedhornStonethresher"
 const data_gen = data_gen_json as WeaponData
 
 const def_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
-const normal_dmg_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
-const charged_dmg_Src = [0.28, 0.35, 0.42, 0.49, 0.56]
+const normal_dmg_Src = [0.4, 0.5, 0.6, 0.7, 0.8]
+const charged_dmg_Src = [0.4, 0.5, 0.6, 0.7, 0.8]
 const def_ = subscript(input.weapon.refineIndex, def_Src)
 // TODO: Should these be premod or total
-const normal_dmg_ = prod(subscript(input.weapon.refineIndex, normal_dmg_Src, { key: "_" }), input.premod.def)
-const charged_dmg_ = prod(subscript(input.weapon.refineIndex, charged_dmg_Src, { key: "_" }), input.premod.def)
+// TODO: Is this the correct formula?
+const normal_dmgInc = prod(subscript(input.weapon.refineIndex, normal_dmg_Src, { key: "_" }), input.total.def)
+const charged_dmgInc = prod(subscript(input.weapon.refineIndex, charged_dmg_Src, { key: "_" }), input.total.def)
 
 const data = dataObjForWeaponSheet(key, data_gen, {
   premod: {
     def_,
-    // TODO: Should these be premod or total
-    normal_dmg_,
-    charged_dmg_
-  },
+    // TODO: Should these in premod or total
+    normal_dmgInc,
+    charged_dmgInc
+  }
 })
 
 const sheet: IWeaponSheet = {
@@ -35,9 +36,9 @@ const sheet: IWeaponSheet = {
     fields: [{
       node: def_
     }, {
-      node: normal_dmg_
+      node: normal_dmgInc
     }, {
-      node: charged_dmg_
+      node: charged_dmgInc
     }]
   }],
 }
