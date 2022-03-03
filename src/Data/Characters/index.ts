7dc3c9b42e5cf4a7faf5cd876b0f862f8a478f0b
Ryan <mineminemine@users.noreply.github.com>
2022-03-03 15:38:22 +0800

    add hu tao

--- src/Data/Characters/index.ts
+++ src/Data/Characters/index.ts
@@ -15,7 +15,7 @@ import Bennett from './Bennett'
 // import Fischl from './Fischl'
 // import Ganyu from './Ganyu'
 // import Gorou from './Gorou'
-// import HuTao from './HuTao'
+import HuTao from './HuTao'
 // import Jean from './Jean'
 import KaedeharaKazuha from './KaedeharaKazuha'
 // import Kaeya from './Kaeya'
@@ -64,7 +64,7 @@ const characters: Record<CharacterKey, CharacterSheet> = {
   // Fischl,
   // Ganyu,
   // Gorou,
-  // HuTao,
+  HuTao,
   // Jean,
   KaedeharaKazuha,
   // Kaeya,
