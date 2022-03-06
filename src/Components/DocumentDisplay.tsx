5fa35c58aa3acc72251405f7be756cd5ab651e7a
Van Nguyen <nguyentvan7@gmail.com>
2022-03-05 22:41:25 -0600

    Add optional header for sections

--- src/Components/DocumentDisplay.tsx
+++ src/Components/DocumentDisplay.tsx
@@ -1,9 +1,10 @@
-import { Box, ListItem } from "@mui/material"
+import { Box, CardHeader, ListItem, Divider } from "@mui/material"
 import { useContext } from "react"
 import { DataContext } from "../DataContext"
 import { DocumentSection } from "../Types/sheet"
 import { evalIfFunc } from "../Util/Util"
 import ConditionalDisplay from "./ConditionalDisplay"
+import CardDark from "./Card/CardDark"
 import FieldDisplay, { FieldDisplayList } from "./FieldDisplay"
 
 export default function DocumentDisplay({ sections }: { sections: DocumentSection[] }) {
@@ -12,11 +13,17 @@ export default function DocumentDisplay({ sections }: { sections: DocumentSectio
     if (section.canShow && !section.canShow(data)) return null
     const talentText = evalIfFunc(section.text, data)
     const fields = section.fields ?? []
+    let { icon, title, action } = section.header ?? {}
+    icon = evalIfFunc(icon, data)
     return <Box key={"section" + i} display="flex" flexDirection="column" gap={1}>
-      {talentText && <div>{talentText}</div>}
-      {fields.length > 0 && <FieldDisplayList>
-        {fields?.map?.((field, i) => <FieldDisplay key={i} field={field} component={ListItem} />)}
-      </FieldDisplayList>}
+        {talentText && <div>{talentText}</div>}
+      <CardDark>
+        {section.header && <CardHeader avatar={icon} title={title} action={action} titleTypographyProps={{ variant: "subtitle2" }} />}
+        {section.header && <Divider />}
+        {fields.length > 0 && <FieldDisplayList>
+          {fields?.map?.((field, i) => <FieldDisplay key={i} field={field} component={ListItem} />)}
+        </FieldDisplayList>}
+      </CardDark>
       {!!section.conditional && <ConditionalDisplay conditional={section.conditional} hideDesc />}
     </Box>
   }).filter(s => s)
