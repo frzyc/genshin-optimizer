63cf44ddb934badff0fd7de56d4cfd8c9416ca78
Van Nguyen <nguyentvan7@gmail.com>
2022-03-06 00:39:48 -0600

    Add header and change some wording for Freedom Sworn

--- src/Data/Weapons/Sword/FreedomSworn/index.tsx
+++ src/Data/Weapons/Sword/FreedomSworn/index.tsx
@@ -1,25 +1,25 @@
 import { WeaponData } from 'pipeline'
-import { Translate } from '../../../../Components/Translate'
 import { input } from '../../../../Formula'
 import { equal, subscript } from '../../../../Formula/utils'
 import { WeaponKey } from '../../../../Types/consts'
-import { cond, sgt, trans } from '../../../SheetUtil'
+import { cond, sgt, st, trans } from '../../../SheetUtil'
 import { dataObjForWeaponSheet } from '../../util'
 import WeaponSheet, { conditionaldesc, conditionalHeader, IWeaponSheet } from '../../WeaponSheet'
 import iconAwaken from './AwakenIcon.png'
 import data_gen_json from './data_gen.json'
 import icon from './Icon.png'
+
 const key: WeaponKey = "FreedomSworn"
 const data_gen = data_gen_json as WeaponData
-const [tr] = trans("weapon", key)
+const [tr, trm] = trans("weapon", key)
 const autoSrc = [0.16, 0.20, 0.24, 0.28, 0.32]
 const atk_Src = [0.2, 0.25, 0.3, 0.35, 0.40]
 
 const [condPassivePath, condPassive] = cond(key, "MillennialMovement")
 const atk_ = equal("on", condPassive, subscript(input.weapon.refineIndex, atk_Src))
 const normal_dmg_ = equal("on", condPassive, subscript(input.weapon.refineIndex, autoSrc))
-const charged_dmg_ = equal("on", condPassive, subscript(input.weapon.refineIndex, autoSrc))
-const plunging_dmg_ = equal("on", condPassive, subscript(input.weapon.refineIndex, autoSrc))
+const charged_dmg_ = { ...normal_dmg_ }
+const plunging_dmg_ = { ...normal_dmg_ }
 
 const dmg_ = subscript(input.weapon.refineIndex, data_gen.addProps.map(x => x.dmg_ ?? NaN))
 
@@ -41,14 +41,15 @@ const sheet: IWeaponSheet = {
   icon,
   iconAwaken,
   document: [{
+    fieldsHeader: conditionalHeader(tr, icon, iconAwaken, st("base")),
     fields: [{ node: dmg_ }],
     conditional: {
       value: condPassive,
       path: condPassivePath,
       teamBuff: true,
-      header: conditionalHeader(tr, icon, iconAwaken),
+      header: conditionalHeader(tr, icon, iconAwaken, trm("milMove")),
       description: conditionaldesc(tr),
-      name: <Translate ns="weapon_FreedomSworn" key18="name" />,
+      name: trm("sigilsConsumed"),
       states: {
         on: {
           fields: [{
