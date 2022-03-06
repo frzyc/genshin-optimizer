c33dd81b5cb0f5ce6a5e7874368bd83bdf2bb305
Van Nguyen <nguyentvan7@gmail.com>
2022-03-06 00:14:54 -0600

    Add headers for Skyward Blade

--- src/Data/Weapons/Sword/SkywardBlade/index.tsx
+++ src/Data/Weapons/Sword/SkywardBlade/index.tsx
@@ -12,7 +12,7 @@ import icon from './Icon.png'
 
 const key: WeaponKey = "SkywardBlade"
 const data_gen = data_gen_json as WeaponData
-const [tr] = trans("weapon", key)
+const [tr, trm] = trans("weapon", key)
 const atkSrc_ = [0.2, 0.25, 0.3, 0.35, 0.40]
 
 const [condPassivePath, condPassive] = cond(key, "SkyPiercingMight")
@@ -36,11 +36,12 @@ const sheet: IWeaponSheet = {
   icon,
   iconAwaken,
   document: [{
+    fieldsHeader: conditionalHeader(tr, icon, iconAwaken, st("base")),
     fields: [{ node: critRate_ }],
     conditional: {
       value: condPassive,
       path: condPassivePath,
-      header: conditionalHeader(tr, icon, iconAwaken),
+      header: conditionalHeader(tr, icon, iconAwaken, trm("might")),
       description: conditionaldesc(tr),
       name: st('afterUse.burst'),
       states: {
