c0fafe0e3d75a6f89ce253d8a5430f90f9c4b366
Ryan <mineminemine@users.noreply.github.com>
2022-03-11 14:42:20 +0800

    fix NO teambuff

--- src/Data/Artifacts/NoblesseOblige/index.tsx
+++ src/Data/Artifacts/NoblesseOblige/index.tsx
@@ -1,6 +1,6 @@
 import { input } from '../../../Formula'
 import { Data } from '../../../Formula/type'
-import { customRead, equal, greaterEq, percent } from '../../../Formula/utils'
+import { equal, greaterEq, percent } from '../../../Formula/utils'
 import { ArtifactSetKey } from '../../../Types/consts'
 import { cond, sgt, trans } from '../../SheetUtil'
 import { ArtifactSheet, conditionalHeader, IArtifactSheet } from '../ArtifactSheet'
@@ -14,18 +14,15 @@ const [tr, trm] = trans("artifact", key)
 const set2 = greaterEq(input.artSet.NoblesseOblige, 2, percent(0.2))
 
 const [condSet4Path, condSet4] = cond(key, "set4")
-const set4TallyWrite = greaterEq(input.artSet.NoblesseOblige, 4, equal(condSet4, "on", 1))
-const set4TallyRead = customRead(["tally", "NO4"])
-const set4 = greaterEq(set4TallyRead, 1, percent(0.2))
+const set4 = equal("on", condSet4, greaterEq(input.artSet.NoblesseOblige, 4, percent(0.2)))
 
 export const data: Data = dataObjForArtifactSheet(key, {
   premod: {
     burst_dmg_: set2,
-    atk_: set4,
   },
   teamBuff: {
-    tally: {
-      NO4: set4TallyWrite
+    premod: {
+      atk_: set4,
     }
   }
 })
