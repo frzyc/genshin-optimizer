b2115c4c7b3d10e4eac4375d9ae63ec615a6283f
Qbe <jose.jtp@gmail.com>
2022-03-17 23:06:40 +0900

    Ningguang, first draft

--- src/Data/Characters/Ningguang/index.tsx
+++ src/Data/Characters/Ningguang/index.tsx
@@ -1,175 +1,263 @@
-import card from './Character_Ningguang_Card.jpg'
-import thumb from './Icon.png'
-import thumbSide from './IconSide.png'
-import banner from './Banner.png'
-import c1 from './constellation1.png'
-import c2 from './constellation2.png'
-import c3 from './constellation3.png'
-import c4 from './constellation4.png'
-import c5 from './constellation5.png'
-import c6 from './constellation6.png'
-import skill from './skill.png'
-import burst from './burst.png'
-import passive1 from './passive1.png'
-import passive2 from './passive2.png'
-import passive3 from './passive3.png'
-import Stat from '../../../Stat'
-import formula, { data } from './data'
-import data_gen from './data_gen.json'
-import { getTalentStatKey, getTalentStatKeyVariant } from '../../../PageBuild/Build'
-import { ICharacterSheet } from '../../../Types/character'
-import { Translate } from '../../../Components/Translate'
-import { conditionalHeader, normalSrc, plungeDocSection, talentTemplate } from '../SheetUtil'
-import { WeaponTypeKey } from '../../../Types/consts'
-import ColorText from '../../../Components/ColoredText'
-const tr = (strKey: string) => <Translate ns="char_Ningguang_gen" key18={strKey} />
-const char: ICharacterSheet = {
+import { CharacterData } from 'pipeline'
+import { input } from '../../../Formula'
+import { constant, equal, equalStr, greaterEq, infoMut, percent, prod, subscript, sum, unequal } from '../../../Formula/utils'
+import { CharacterKey, ElementKey } from '../../../Types/consts'
+import { cond, st, trans } from '../../SheetUtil'
+import CharacterSheet, { conditionalHeader, ICharacterSheet, normalSrc, talentTemplate } from '../CharacterSheet'
+import { customDmgNode, dataObjForCharacterSheet, dmgNode, healNodeTalent, shieldNode, shieldNodeTalent } from '../dataUtil'
+import { banner, burst, c1, c2, c3, c4, c5, c6, card, passive1, passive2, passive3, skill, thumb, thumbSide } from './assets'
+import data_gen_src from './data_gen.json'
+import skillParam_gen from './skillParam_gen.json'
+
+const data_gen = data_gen_src as CharacterData
+
+const key: CharacterKey = "Ningguang"
+const elementKey: ElementKey = "geo"
+const [tr, trm] = trans("char", key)
+
+let a = 0, s = 0, b = 0, p1 = 0
+const datamine = {
+  normal: {
+    hitArr: [
+      skillParam_gen.auto[a++],
+    ]
+  },
+  charged: {
+    dmg: skillParam_gen.auto[a++],
+    jadeDmg: skillParam_gen.auto[a++],
+    stamina: skillParam_gen.auto[a++][0],
+  },
+  plunging: {
+    dmg: skillParam_gen.auto[a++],
+    low: skillParam_gen.auto[a++],
+    high: skillParam_gen.auto[a++],
+  },
+  skill: {
+    screenHpMod: skillParam_gen.skill[s++], // 100% + skillParam_gen.skill[s++] * 100
+    skillDmg: skillParam_gen.skill[s++],
+    screenHp: skillParam_gen.skill[s++], //screenHp * 100%
+    cd: skillParam_gen.skill[s++][0],
+  },
+  burst: {
+    dmgPerGem: skillParam_gen.burst[b++],
+    cd: skillParam_gen.burst[b++][0],
+    enerCost: skillParam_gen.burst[b++][0],
+  },
+  passive1: { 
+    //Nothing
+  },
+  passive2: {
+    geoDmgBonus_ : skillParam_gen.passive2[p1++][0],
+    duration: skillParam_gen.passive2[p1++][0],
+  },
+  passive3: {
+    //Nothing
+  },
+  constellation1: {
+    //Nothing
+  },
+  constellation2: {
+    //Nothing
+  },
+  constellation4: {
+    //Nothing
+  },
+  constellation6: {
+    //Nothing
+  },
+} as const
+
+const [condA4Path, condA4] = cond(key, "Ascension4") //12% Geo DMG bonus after passing through the Jade Screen
+const [condC4Path, condC4] = cond(key, "Constellation4")
+
+const nodeA4GeoDmgBonus_ = equal(condA4, "on", percent(datamine.passive2.geoDmgBonus_), { key: `char_${key}:a4bonus_` })
+
+const nodeC4CryoResBonus_ = equal(condC4, "on", percent(0.10))
+const nodeC4GeoResBonus_ = equal(condC4, "on", percent(0.10))
+const nodeC4PyroResBonus_ = equal(condC4, "on", percent(0.10))
+const nodeC4AnemoResBonus_ = equal(condC4, "on", percent(0.10))
+const nodeC4HydroResBonus_ = equal(condC4, "on", percent(0.10))
+const nodeC4ElecResBonus_ = equal(condC4, "on", percent(0.10))
+
+const dmgFormulas = {
+  normal: Object.fromEntries(datamine.normal.hitArr.map((arr, i) =>
+    [i, dmgNode("atk", arr, "normal")])),
+  charged: {
+    baseDmg: dmgNode("atk", datamine.charged.dmg, "charged"),
+    jadeDmg: dmgNode("atk", datamine.charged.jadeDmg, "charged"),
+  },
+  plunging: Object.fromEntries(Object.entries(datamine.plunging).map(([key, value]) =>
+    [key, dmgNode("atk", value, "plunging")])),
+  skill: {
+    screenHp: prod(subscript(input.total.skillIndex, datamine.skill.screenHp, { key: '_' }), input.total.hp),
+    dmg: dmgNode("atk", datamine.skill.skillDmg, "skill"),    
+  },
+  burst: {
+    gemDmg: dmgNode("atk", datamine.burst.dmgPerGem, "burst"),
+  },
+}
+
+const nodeC3 = greaterEq(input.constellation, 3, 3)
+const nodeC5 = greaterEq(input.constellation, 5, 3)
+
+export const data = dataObjForCharacterSheet(key, elementKey, "liyue", data_gen, dmgFormulas, {
+  bonus: {
+    skill: nodeC3,
+    burst: nodeC5,
+  }, teamBuff: {
+    premod: {
+      geo_dmg_  :nodeA4GeoDmgBonus_,
+      geo_res_  :nodeC4GeoResBonus_,
+      cryo_res_ :nodeC4CryoResBonus_,
+      pyro_res_ :nodeC4PyroResBonus_,
+      anemo_res_:nodeC4AnemoResBonus_,
+      hydro_res_:nodeC4HydroResBonus_,
+      electro_res_ :nodeC4ElecResBonus_,
+    }
+  }
+})
+
+const sheet: ICharacterSheet = {
   name: tr("name"),
   cardImg: card,
   thumbImg: thumb,
   thumbImgSide: thumbSide,
   bannerImg: banner,
   rarity: data_gen.star,
-  elementKey: "geo",
-  weaponTypeKey: data_gen.weaponTypeKey as WeaponTypeKey,
+  elementKey: elementKey,
+  weaponTypeKey: data_gen.weaponTypeKey,
   gender: "F",
   constellationName: tr("constellationName"),
   title: tr("title"),
-  baseStat: data_gen.base,
-  baseStatCurve: data_gen.curves,
-  ascensions: data_gen.ascensions,
   talent: {
-    formula,
     sheets: {
       auto: {
         name: tr("auto.name"),
-        img: normalSrc(data_gen.weaponTypeKey as WeaponTypeKey),
-        sections: [
-          {
-            text: stats => <span><strong>Normal Attack</strong> Shoots gems that deal <ColorText color="geo">{stats.constellation >= 1 ? "AoE " : ""}Geo DMG</ColorText>. Upon hit, this grants Ningguang 1 Star Jade.</span>,
-            fields: [{
-              text: `Normal Attack DMG`,
-              formulaText: stats => <span>{data.normal.hit[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("normal", stats), stats)}</span>,
-              formula: formula.normal.hit,
-              variant: stats => getTalentStatKeyVariant("normal", stats),
-            }, {
-              canShow: stats => stats.constellation >= 1,
-              text: <span>Gems do <ColorText color="geo">AoE Geo DMG</ColorText></span>,
+        img: normalSrc(data_gen.weaponTypeKey),
+        sections: [{
+          text: tr("auto.fields.normal"),
+          fields: [
+            { node: infoMut(dmgFormulas.normal[0], { key: `char_${key}_gen:auto.skillParams.0` }) } ,
+            {
+              canShow: data => data.get(input.constellation).value >= 1,
+              text: trm("aoeGems"),
             }]
+        },
+        {
+          text: tr("auto.fields.charged"),
+          fields: [{
+            node: infoMut(dmgFormulas.charged.baseDmg, { key: `char_${key}_gen:auto.skillParams.1` }),
           }, {
-            text: <span><strong>Charged Attack</strong> Consumes a certain amount of stamina to fire off a giant gem that deals <ColorText color="geo">Geo DMG</ColorText>. If Ningguang has any Star Jades, unleashing a Charged Attack will cause the Star Jades to be fired at the enemy as well, dealing additional DMG.</span>,
-            fields: [{
-              text: `Charged Attack DMG`,
-              formulaText: stats => <span>{data.charged.dmg[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("charged", stats), stats)}</span>,
-              formula: formula.charged.dmg,
-              variant: stats => getTalentStatKeyVariant("charged", stats),
-            }, {
-              text: `DMG per Star Jade`,
-              formulaText: stats => <span>{data.charged.jade[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("charged", stats), stats)}</span>,
-              formula: formula.charged.jade,
-              variant: stats => getTalentStatKeyVariant("charged", stats),
-            }, {
-              text: `Stamina Cost`,
-              value: stats => <span>50{(stats.ascension >= 1 ? <span>; With <b>Star Jade</b>: 0</span> : "")}</span>,
-            }]
-          },
-          plungeDocSection(tr, formula, data),
+            node: infoMut(dmgFormulas.charged.jadeDmg, { key: `char_${key}_gen:auto.skillParams.2` }),
+          }, {
+            canShow: data => data.get(input.asc).value < 1,
+            text: tr("auto.skillParams.3"),
+            value: datamine.charged.stamina,
+          }, {
+            canShow: data => data.get(input.asc).value >= 1,
+            text: tr("auto.skillParams.3"),
+            value: trm("starJadeStaminaCost"),
+          }]
+        }, {
+          text: tr(`auto.fields.plunging`),
+          fields: [{
+            node: infoMut(dmgFormulas.plunging.dmg, { key: "sheet_gen:plunging.dmg" }),
+          }, {
+            node: infoMut(dmgFormulas.plunging.low, { key: "sheet_gen:plunging.low" }),
+          }, {
+            node: infoMut(dmgFormulas.plunging.high, { key: "sheet_gen:plunging.high" }),
+          }]
+        }
         ],
       },
-      skill: {
+      skill: { // Cannot use talentTemplate because this has multiple conditionals.
         name: tr("skill.name"),
         img: skill,
         sections: [{
           text: tr("skill.description"),
-          fields: [{
-            text: "Inherited HP",
-            formulaText: stats => <span>{data.skill.inheri_hp[stats.tlvl.skill]}% {Stat.printStat("finalHP", stats)}</span>,
-            formula: formula.skill.inheri_hp,
-          }, {
-            text: "Skill DMG",
-            formulaText: stats => <span>{data.skill.dmg[stats.tlvl.skill]}% {Stat.printStat(getTalentStatKey("skill", stats), stats)}</span>,
-            formula: formula.skill.dmg,
-            variant: stats => getTalentStatKeyVariant("skill", stats),
-          }, {
-            text: "CD",
-            value: "12s",
-          }, {
-            canShow: stats => stats.constellation >= 2,
-            text: "Resets CD on shatter, every 6s",
-          },]
-        }],
+          fields: [
+            { node: infoMut(dmgFormulas.skill.screenHp, { key: `char_${key}_gen:skill.skillParams.0` }), }, 
+            { node: infoMut(dmgFormulas.skill.dmg, { key: `char_${key}_gen:skill.skillParams.1` }), }, 
+            {
+              text: tr("skill.skillParams.2"),
+              value: datamine.burst.cd,
+              unit: "s"
+            } ,
+            {
+              canShow: data => data.get(input.constellation).value >= 2,
+              text: trm("skillReset"),
+            }
+          ],
+        }, {
+          conditional: {
+            teamBuff: true,
+            value: condA4,
+            path: condA4Path,
+            name: trm("a4toggle"),
+            canShow: greaterEq(input.asc, 4, 1),
+            header: conditionalHeader("passive2", tr, passive2), description: tr(`passive2.description`),
+            states: {
+              on: {
+                fields: [{
+                  node: nodeA4GeoDmgBonus_
+                }, {
+                  text: trm("a4duration"),
+                  value: datamine.passive2.duration,
+                  unit: "s"
+                }]
+              }
+            }
+          } 
+        }, {
+          conditional: {
+            teamBuff: true,
+            value: condC4,
+            path: condC4Path,
+            name: trm("c4toggle"),
+            canShow: greaterEq(input.constellation, 4, 1),
+            header: conditionalHeader("constellation4", tr, c4), description: tr(`constellation4.description`),
+            states: {
+              on: {
+                fields: [
+                  { node:nodeC4CryoResBonus_ },
+                  { node:nodeC4GeoResBonus_ } ,
+                  { node:nodeC4PyroResBonus_ } ,
+                  { node:nodeC4AnemoResBonus_ } ,
+                  { node:nodeC4HydroResBonus_ } ,
+                  { node:nodeC4ElecResBonus_ } ,
+                ]
+              }
+            }
+          }
+        }]
       },
-      burst: {
-        name: tr("burst.name"),
-        img: burst,
-        sections: [{
-          text: tr("burst.description"),
-          fields: [{
-            text: "DMG Per Gem",
-            formulaText: stats => <span>{data.burst.dmg_per_gem[stats.tlvl.burst]}% {Stat.printStat(getTalentStatKey("burst", stats), stats)}</span>,
-            formula: formula.burst.dmg_per_gem,
-            variant: stats => getTalentStatKeyVariant("burst", stats),
-          }, {
-            text: "CD",
-            value: "12s",
-          }, {
-            text: "Energy Cost",
-            value: 40,
-          }, {
-            canShow: stats => stats.constellation >= 6,
-            text: "Star Jade gained",
-            value: "7",
-          }]
-        }],
-      },
+      burst: talentTemplate("burst", tr, burst, [
+        { node: infoMut(dmgFormulas.burst.gemDmg, { key: `char_${key}_gen:burst.skillParams.0` }), },
+        {
+          text: tr("burst.skillParams.1"),
+          value: datamine.burst.cd,
+          unit: "s",
+        }, {
+          text: tr("burst.skillParams.2"),
+          value: datamine.burst.enerCost,
+        }, {
+          canShow: data => data.get(input.constellation).value >= 6,
+          text: trm("c6bonus"),
+          value: 7,
+        }
+      ],
+      ),
       passive1: talentTemplate("passive1", tr, passive1),
-      passive2: {
-        name: tr("passive2.name"),
-        img: passive2,
-        sections: [{
-          text: tr("passive2.description"),
-          conditional: { // StrategicReserve
-            key: "a4",
-            canShow: stats => stats.ascension >= 4,
-            partyBuff: "partyAll",
-            header: conditionalHeader("passive2", tr, passive2),
-            description: tr("passive2.description"),
-            name: <span>Passing through <b>Jade Screen</b></span>,
-            stats: { geo_dmg_: 12 },
-            fields: [{
-              text: "Duration",
-              value: "10s",
-            }]
-          },
-        }],
-      },
+      passive2: talentTemplate("passive2", tr, passive2),
       passive3: talentTemplate("passive3", tr, passive3),
       constellation1: talentTemplate("constellation1", tr, c1),
       constellation2: talentTemplate("constellation2", tr, c2),
-      constellation3: talentTemplate("constellation3", tr, c3, "burstBoost"),
-      constellation4: {
-        name: tr("constellation4.name"),
-        img: c4,
-        sections: [{
-          text: tr("constellation4.description"),
-          conditional: { // ExquisiteBeTheJade
-            key: "c4",
-            canShow: stats => stats.constellation >= 4,
-            partyBuff: "partyAll",
-            header: conditionalHeader("constellation4", tr, c4),
-            description: tr("constellation4.description"),
-            name: <span>Allies within 10m of <b>Jade Screen</b></span>,
-            fields: [{
-              text: "Elemental DMG received",//TODO: elemental dmg reduction
-              value: -10,
-              unit: "%"
-            }]
-          }
-        }],
-      },
-      constellation5: talentTemplate("constellation5", tr, c5, "skillBoost"),
+      constellation3: talentTemplate("constellation3", tr, c3, [{ node: nodeC3 }]),
+      constellation4: talentTemplate("constellation4", tr, c4),
+      constellation5: talentTemplate("constellation5", tr, c5, [{ node: nodeC5 }]),
       constellation6: talentTemplate("constellation6", tr, c6),
-    },
-  },
-};
-export default char;
+    }
+  }
+}
+
+export default new CharacterSheet(sheet, data);
