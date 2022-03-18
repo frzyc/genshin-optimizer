809dc49110f21c6a10975cff1fedb856f4ebcee0
frzyc <frzyc@users.noreply.github.com>
2022-03-18 12:21:27 -0400

    add shieldElement util

--- src/Data/Characters/dataUtil.tsx
+++ src/Data/Characters/dataUtil.tsx
@@ -3,7 +3,7 @@ import { input } from "../../Formula";
 import { inferInfoMut, mergeData } from "../../Formula/api";
 import { reactions } from "../../Formula/reaction";
 import { Data, DisplaySub, NumNode } from "../../Formula/type";
-import { constant, data, equalStr, infoMut, prod, stringPrio, subscript, sum, unit } from "../../Formula/utils";
+import { constant, data, equalStr, infoMut, percent, prod, stringPrio, subscript, sum, unit } from "../../Formula/utils";
 import { allMainStatKeys, allSubstats, MainStatKey } from "../../Types/artifact";
 import { CharacterKey, ElementKey, Region } from "../../Types/consts";
 import { layeredAssignment, objectKeyMap, objectMap } from "../../Util/Util";
@@ -72,6 +72,9 @@ export function shieldNodeTalent(base: MainStatKey | SubstatKey, baseMultiplier:
     prod(subscript(talentIndex, baseMultiplier, { key: '_' }), input.total[base]),
     subscript(talentIndex, flat)
   ), additional)
+}
+export function shieldElement(element: "electro" | "cryo" | "hydro" | "pyro" | "geo", shieldNode: NumNode) {
+  return infoMut(prod(percent(element === "geo" ? 1.5 : 2.5), shieldNode), { variant: element })
 }
 /** Note: `additional` applies only to this formula */
 export function healNodeTalent(base: MainStatKey | SubstatKey, baseMultiplier: number[], flat: number[], move: "normal" | "charged" | "plunging" | "skill" | "burst", additional?: Data): NumNode {
