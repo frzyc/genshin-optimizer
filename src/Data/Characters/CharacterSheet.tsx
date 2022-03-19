5d9ee729b80336a6e8879c44a79203a39ab763cc
Van Nguyen <nguyentvan7@gmail.com>
2022-03-19 13:51:47 -0500

    Selectively override conditional properties

--- src/Data/Characters/CharacterSheet.tsx
+++ src/Data/Characters/CharacterSheet.tsx
@@ -88,9 +88,9 @@ export const sectionTemplate = (talentKey: TalentSheetElementKey, tr: (string) =
   canShow: fieldsCanShow,
   teamBuff,
   conditional: conditional
-    ? !conditional.header
-      ? { ...conditional, header: conditionalHeader(talentKey, tr, img), description: tr(`${talentKey}.description`) }
-      : conditional
+    ? { ...conditional,
+      header: conditional.header ? conditional.header : conditionalHeader(talentKey, tr, img),
+      description: conditional.description ? conditional.description : tr(`${talentKey}.description`) }
     : undefined,
 })
 
