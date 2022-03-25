974a68dffe16aa86ff0ae759f415c1caf1854ebc
frzyc <frzyc@users.noreply.github.com>
2022-03-25 19:41:51 -0400

    add aloy rarity

--- src/Data/Characters/Aloy/index.tsx
+++ src/Data/Characters/Aloy/index.tsx
@@ -121,7 +121,7 @@ const sheet: ICharacterSheet = {
   thumbImg: thumb,
   thumbImgSide: thumbSide,
   bannerImg: banner,
-  rarity: data_gen.star,
+  rarity: 5,//data_gen.star, TODO: not in datamine
   elementKey,
   weaponTypeKey: data_gen.weaponTypeKey,
   gender: "F",
