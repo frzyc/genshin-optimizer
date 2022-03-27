164f2dcbb19022a7673685da8c3bf601e8d32a19
SirDerpyHerp <40593628+SirDerpyHerp@users.noreply.github.com>
2022-03-28 00:38:29 +0700

    Add Qiqi (#190)

--- src/Data/Characters/Qiqi/index.tsx
+++ src/Data/Characters/Qiqi/index.tsx
@@ -1,223 +1,229 @@
-import card from "./Character_Qiqi_Card.jpg";
-import thumb from './Icon.png'
-import thumbSide from './IconSide.png'
-import banner from './Banner.png'
-import c1 from "./constellation1.png";
-import c2 from "./constellation2.png";
-import c3 from "./constellation3.png";
-import c4 from "./constellation4.png";
-import c5 from "./constellation5.png";
-import c6 from "./constellation6.png";
-import skill from "./skill.png";
-import burst from "./burst.png";
-import passive1 from './passive1.png'
-import passive2 from './passive2.png'
-import passive3 from './passive3.png'
-import Stat from "../../../Stat";
-import formula, { data } from "./data";
-import data_gen from './data_gen.json'
-import { getTalentStatKey, getTalentStatKeyVariant } from "../../../PageBuild/Build";
-import { ICharacterSheet } from "../../../Types/character";
-import { Translate } from '../../../Components/Translate'
-import { chargedDocSection, normalSrc, plungeDocSection, talentTemplate } from "../SheetUtil";
-import { WeaponTypeKey } from "../../../Types/consts";
-const tr = (strKey: string) => <Translate ns="char_Qiqi_gen" key18={strKey} />
-const char: ICharacterSheet = {
+import { CharacterData } from 'pipeline'
+import { input } from '../../../Formula'
+import { equal, greaterEq, infoMut, prod, min, constant } from '../../../Formula/utils'
+import { CharacterKey, ElementKey } from '../../../Types/consts'
+import { cond, sgt, st, trans } from '../../SheetUtil'
+import CharacterSheet, { conditionalHeader, ICharacterSheet, normalSrc, talentTemplate } from '../CharacterSheet'
+import { dataObjForCharacterSheet, dmgNode, healNode, healNodeTalent } from '../dataUtil'
+import { banner, burst, c1, c2, c3, c4, c5, c6, card, passive1, passive2, passive3, skill, thumb, thumbSide } from './assets'
+import data_gen_src from './data_gen.json'
+import skillParam_gen from './skillParam_gen.json'
+
+const key: CharacterKey = "Qiqi"
+const elementKey: ElementKey = "cryo"
+const data_gen = data_gen_src as CharacterData
+const [tr, trm] = trans("char", key)
+
+let a = 0, s = 0, b = 0
+const datamine = {
+  normal: {
+    hitArr: [
+      skillParam_gen.auto[a++], // 1
+      skillParam_gen.auto[a++], // 2
+      skillParam_gen.auto[a++], // 3
+      skillParam_gen.auto[a++], // 4
+      skillParam_gen.auto[a++], // 5
+    ]
+  },
+  charged: {
+    dmg: skillParam_gen.auto[a++],
+    stamina: skillParam_gen.auto[a++][0],
+  },
+  plunging: {
+    dmg: skillParam_gen.auto[a++],
+    low: skillParam_gen.auto[a++],
+    high: skillParam_gen.auto[a++],
+  },
+  skill: {
+    hitRegenPercent: skillParam_gen.skill[s++],
+    hitRegenFlat: skillParam_gen.skill[s++],
+    contRegenPercent: skillParam_gen.skill[s++],
+    contRegenFlat: skillParam_gen.skill[s++],
+    tickDmg: skillParam_gen.skill[s++],
+    duration: skillParam_gen.skill[s++][0],
+    cd: skillParam_gen.skill[s++][0],
+    castDmg: skillParam_gen.skill[s++],
+  },
+  burst: {
+    healPercent: skillParam_gen.burst[b++],
+    healFlat: skillParam_gen.burst[b++],
+    dmg: skillParam_gen.burst[b++],
+    duration: skillParam_gen.burst[b++][0],
+    cd: skillParam_gen.burst[b++][0],
+    cost: skillParam_gen.burst[b++][0]
+  }
+} as const
+
+const [condA1Path, condA1] = cond(key, "QiqiA1")
+const [condC2Path, condC2] = cond(key, "QiqiC2")
+
+// Values here doesn't exist in skillParam_gen
+const nodeA1HealingBonus = equal(condA1, "on", greaterEq(input.asc, 1, constant(0.2)))
+const nodeC2ChargedDmgInc = equal(condC2, "on", greaterEq(input.constellation, 2, constant(0.15)))
+const nodeC2NormalDmgInc = equal(condC2, "on", greaterEq(input.constellation, 2, constant(0.15)))
+
+const dmgFormulas = {
+  normal: Object.fromEntries(datamine.normal.hitArr.map((arr, i) =>
+    [i, dmgNode("atk", arr, "normal")])),
+  charged: {
+    dmg: dmgNode("atk", datamine.charged.dmg, "charged"),
+  },
+  plunging: Object.fromEntries(Object.entries(datamine.plunging).map(([name, arr]) =>
+    [name, dmgNode("atk", arr, "plunging")])),
+  skill: {
+    castDmg: dmgNode("atk", datamine.skill.castDmg, "skill"),
+    tickDmg: dmgNode("atk", datamine.skill.tickDmg, "skill"),
+    hitRegen: healNodeTalent("atk", datamine.skill.hitRegenPercent, datamine.skill.hitRegenFlat, "skill"),
+    contRegen: healNodeTalent("atk", datamine.skill.contRegenPercent, datamine.skill.contRegenFlat, "skill")
+  },
+  burst: {
+    dmg: dmgNode("atk", datamine.burst.dmg, "burst"),
+    heal: healNodeTalent("atk", datamine.burst.healPercent, datamine.burst.healFlat, "burst")
+  },
+}
+
+const nodeC3 = greaterEq(input.constellation, 3, 3)
+const nodeC5 = greaterEq(input.constellation, 5, 3)
+export const data = dataObjForCharacterSheet(key, elementKey, "liyue", data_gen, dmgFormulas, {
+  bonus: {
+    burst: nodeC3,
+    skill: nodeC5,
+  },
+  premod: {
+    normal_dmg_: nodeC2NormalDmgInc,
+    charged_dmg_: nodeC2ChargedDmgInc,
+    incHeal_: nodeA1HealingBonus
+  }
+})
+
+const sheet: ICharacterSheet = {
   name: tr("name"),
   cardImg: card,
   thumbImg: thumb,
   thumbImgSide: thumbSide,
   bannerImg: banner,
   rarity: data_gen.star,
-  elementKey: "cryo",
-  weaponTypeKey: data_gen.weaponTypeKey as WeaponTypeKey,
+  elementKey: elementKey,
+  weaponTypeKey: data_gen.weaponTypeKey,
   gender: "F",
   constellationName: tr("constellationName"),
   title: tr("title"),
-  baseStat: data_gen.base,
-  baseStatCurve: data_gen.curves,
-  ascensions: data_gen.ascensions,
   talent: {
-    formula,
     sheets: {
       auto: {
         name: tr("auto.name"),
-        img: normalSrc(data_gen.weaponTypeKey as WeaponTypeKey),
+        img: normalSrc(data_gen.weaponTypeKey),
         sections: [
           {
-            text: <span>{tr(`auto.fields.normal`)} <small><i>Note: the 3rd and 4th attacks hit twice.</i></small></span>,
-            fields: data.normal.hitArr.map((percentArr, i) => ({
-              text: `${i + 1}-Hit DMG`,
-              formulaText: (i === 2 || i === 3) ?
-                stats => <span>( {percentArr[stats.tlvl.auto]}% + {percentArr[stats.tlvl.auto]}% ) {Stat.printStat(getTalentStatKey("normal", stats), stats)}</span> :
-                stats => <span>{percentArr[stats.tlvl.auto]}% {Stat.printStat(getTalentStatKey("normal", stats), stats)}</span>,
-              formula: formula.normal[i],
-              variant: stats => getTalentStatKeyVariant("normal", stats),
-            })),
-          },
-          chargedDocSection(tr, formula, data, 20),
-          plungeDocSection(tr, formula, data),
+            text: tr("auto.fields.normal"),
+            fields: datamine.normal.hitArr.map((_, i) =>
+            ({
+              node: infoMut(dmgFormulas.normal[i], { key: `char_${key}_gen:auto.skillParams.${i}` }),
+            }))
+          }, {
+            text: tr("auto.fields.charged"),
+            fields: [{
+              node: infoMut(dmgFormulas.charged.dmg, { key: `char_${key}_gen:auto.skillParams.5` }),
+              textSuffix: st("brHits", { count: 2 })
+            }, {
+              text: tr("auto.skillParams.6"),
+              value: datamine.charged.stamina,
+            }]
+          }, {
+            text: tr("auto.fields.plunging"),
+            fields: [{
+              node: infoMut(dmgFormulas.plunging.dmg, { key: "sheet_gen:plunging.dmg" }),
+            }, {
+              node: infoMut(dmgFormulas.plunging.low, { key: "sheet_gen:plunging.low" }),
+            }, {
+              node: infoMut(dmgFormulas.plunging.high, { key: "sheet_gen:plunging.high" }),
+            }]
+          }
         ],
       },
-      skill: {
-        name: tr("skill.name"),
-        img: skill,
-        sections: [{
-          text: tr("skill.description"),
-          fields: [{
-            text: "Skill DMG",
-            formulaText: stats => <span>{data.skill.hit[stats.tlvl.skill]}% {Stat.printStat(getTalentStatKey("skill", stats), stats)}</span>,
-            formula: formula.skill.hit,
-            variant: stats => getTalentStatKeyVariant("skill", stats),
-          }, {
-            text: "Regeneration on Hit",
-            formulaText: stats => <span>( {data.skill.hitregen.atk[stats.tlvl.skill]}% {Stat.printStat("finalATK", stats)} + {data.skill.hitregen.base[stats.tlvl.skill]} ) * {Stat.printStat("heal_multi", stats)}</span>,
-            formula: formula.skill.hitregen,
-            variant: "success",
-          }, {
-            text: "Continuous Regeneration",
-            formulaText: stats => <span>( {data.skill.continuousregen.atk[stats.tlvl.skill]}% {Stat.printStat("finalATK", stats)} + {data.skill.continuousregen.base[stats.tlvl.skill]} ) * {Stat.printStat("heal_multi", stats)}            </span>,
-            formula: formula.skill.continuousregen,
-            variant: "success",
-          }, {
-            text: "Herald of Frost DMG",
-            formulaText: stats => <span>{data.skill.herald[stats.tlvl.skill]}% {Stat.printStat(getTalentStatKey("skill", stats), stats)}</span>,
-            formula: formula.skill.herald,
-            variant: stats => getTalentStatKeyVariant("skill", stats),
-          }, {
-            text: "Duration",
-            value: "15s",
-          }, {
-            text: "CD",
-            value: "30s",
-          }],
-        }],
-      },
+      skill: talentTemplate("skill", tr, skill, [{
+        node: infoMut(dmgFormulas.skill.castDmg, { key: `char_${key}_gen:skill.skillParams.0` })
+      }, {
+        node: infoMut(dmgFormulas.skill.hitRegen, { key: `char_${key}_gen:skill.skillParams.1`, variant: "success" })
+      }, {
+        node: infoMut(dmgFormulas.skill.contRegen, { key: `char_${key}_gen:skill.skillParams.2`, variant: "success" })
+      }, {
+        node: infoMut(dmgFormulas.skill.tickDmg, { key: `char_${key}_gen:skill.skillParams.3` })
+      }, {
+        text: tr("skill.skillParams.4"),
+        value: datamine.skill.duration,
+        unit: 's'
+      }, {
+        text: tr("skill.skillParams.5"),
+        value: datamine.skill.cd,
+        unit: 's'
+      }]),
       burst: {
         name: tr("burst.name"),
         img: burst,
         sections: [{
           text: tr("burst.description"),
           fields: [{
-            text: "Burst DMG",
-            formulaText: stats => <span>{data.burst.dmg[stats.tlvl.burst]}% {Stat.printStat(getTalentStatKey("burst", stats), stats)}</span>,
-            formula: formula.burst.dmg,
-            variant: stats => getTalentStatKeyVariant("burst", stats),
+            node: infoMut(dmgFormulas.burst.dmg, { key: `char_${key}_gen:burst.skillParams.0` }),
+          },
+          {
+            node: infoMut(dmgFormulas.burst.heal, { key: `char_${key}_gen:burst.skillParams.1`, variant: "success" }),
           }, {
-            text: "Talisman Healing",
-            formulaText: stats => <span> ( {data.burst.healing.atk[stats.tlvl.burst]}% {Stat.printStat("finalATK", stats)} + {data.burst.healing.base[stats.tlvl.burst]} ) * {Stat.printStat("heal_multi", stats)}</span>,
-            formula: formula.burst.healing,
-            variant: "success",
+            text: tr("burst.skillParams.2"),
+            value: datamine.skill.duration,
+            unit: 's'
           }, {
-            text: "Talisman Duration",
-            value: "15s",
+            text: tr("burst.skillParams.3"),
+            value: datamine.skill.cd,
+            unit: 's'
           }, {
-            text: "CD",
-            value: "20s",
-          }, {
-            text: "Energy Cost",
-            value: 80,
-          }],
-        }],
+            text: tr("burst.skillParams.4"),
+            value: datamine.burst.cost,
+          }]
+        }]
       },
-      passive1: {
-        name: tr("passive1.name"),
-        img: passive1,
-        sections: [{
-          text: tr("passive1.description"),
-          conditional: { // LifeProlongingMethods
-            key: "a1",
-            canShow: stats => stats.ascension >= 1,
-            name: <span>Character under the effects of <b>Adeptus Art: Herald of Frost</b> triggers an Elemental Reaction</span>,
-            stats: { incHeal_: 20, },
+      passive1: talentTemplate("passive1", tr, passive1, undefined, {
+        name: trm("a1C"),
+        value: condA1,
+        path: condA1Path,
+        header: conditionalHeader("passive1", tr, passive1),
+        canShow: greaterEq(input.asc, 1, 1),
+        states: {
+          on: {
             fields: [{
-              text: "Duration",
-              value: "8s",
-            }],
-          },
-        }],
-      },
-      passive2: {
-        name: tr("passive2.name"),
-        img: passive2,
-        sections: [{
-          text: tr("passive2.description"),
-          fields: [{
-            canShow: stats => stats.ascension >= 4,
-            text: "Talisman Application Chance",
-            value: "50%",
-          }, {
-            canShow: stats => stats.ascension >= 4,
-            text: "CD",
-            value: "30s",
-          }],
-        },
-        ],
-      },
+              node: nodeA1HealingBonus
+            }, {
+              text: sgt("duration"),
+              value: 8,
+              unit: 's'
+            }]
+          }
+        }
+      }),
+      passive2: talentTemplate("passive2", tr, passive2),
       passive3: talentTemplate("passive3", tr, passive3),
-      constellation1: {
-        name: tr("constellation1.name"),
-        img: c1,
-        sections: [{
-          text: tr("constellation1.description"),
-          fields: [{
-            canShow: stats => stats.constellation >= 1,
-            text: "Energy on Hit",
-            value: 2,
-          }],
-        }],
-      },
-      constellation2: {
-        name: tr("constellation2.name"),
-        img: c2,
-        sections: [{
-          text: tr("constellation2.description"),
-          conditional: { // FrozenToTheBone
-            key: "c2",
-            canShow: stats => stats.constellation >= 2,
-            name: "Enemy Affected by Cryo",
-            stats: {
-              normal_dmg_: 15,
-              charged_dmg_: 15,
-            },
-          },
-        }],
-      },
-      constellation3: talentTemplate("constellation3", tr, c3, "burstBoost"),
-      constellation4: {
-        name: tr("constellation4.name"),
-        img: c4,
-        sections: [{
-          text: tr("constellation4.description"),
-          conditional: { // DivineSuppression
-            key: "c4",
-            canShow: stats => stats.constellation >= 4,
-            name: "Enemy marked by Talisman",
-            fields: [{//TODO: enemy atk decrease
-              text: "Enemy ATK Decrease",
-              value: "20%"
-            }],
-          },
-        }],
-      },
-      constellation5: talentTemplate("constellation5", tr, c5, "skillBoost"),
-      constellation6: {
-        name: tr("constellation6.name"),
-        img: c6,
-        sections: [{
-          text: tr("constellation6.description"),
-          fields: [{
-            canShow: stats => stats.constellation >= 6,
-            text: "Revival HP Regeneration",
-            value: `50% of Max HP`,
-          }, {
-            canShow: stats => stats.constellation >= 6,
-            text: "Cooldown",
-            value: `15m`,
-          }],
-        }],
-      },
+      constellation1: talentTemplate("constellation1", tr, c1),
+      constellation2: talentTemplate("constellation2", tr, c2, undefined, {
+        value: condC2,
+        path: condC2Path,
+        name: trm("c2C"),
+        header: conditionalHeader("constellation2", tr, c2),
+        canShow: greaterEq(input.constellation, 2, 1),
+        states: {
+          on: {
+            fields: [{
+              node: nodeC2NormalDmgInc
+            }, {
+              node: nodeC2ChargedDmgInc
+            }]
+          }
+        }
+      }),
+      constellation3: talentTemplate("constellation3", tr, c3, [{ node: nodeC3 }]),
+      constellation4: talentTemplate("constellation4", tr, c4),
+      constellation5: talentTemplate("constellation5", tr, c5, [{ node: nodeC5 }]),
+      constellation6: talentTemplate("constellation6", tr, c6),
     },
   },
 };
-export default char;
+export default new CharacterSheet(sheet, data);
