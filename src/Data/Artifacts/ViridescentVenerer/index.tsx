54d6a59b7967a1ff74aa1e55a6a08ac3a725d4cd
Van Nguyen <nguyentvan7@gmail.com>
2022-04-02 12:34:39 -0500

    Fix VV strings

--- src/Data/Artifacts/ViridescentVenerer/index.tsx
+++ src/Data/Artifacts/ViridescentVenerer/index.tsx
@@ -58,7 +58,7 @@ const sheet: IArtifactSheet = {
           name: st("eleSwirled"),
           states: {
             swirl: {
-              name: <ColorText color={eleKey}>{st(`element.${eleKey}`)}</ColorText>,
+              name: <ColorText color={eleKey}>{sgt(`element.${eleKey}`)}</ColorText>,
               fields: [{
                 node: condSwirlNodes[`${eleKey}_enemyRes_`]
               }, {
