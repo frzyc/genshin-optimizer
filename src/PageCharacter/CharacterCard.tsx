5f1d6c44c469fa3b5bb7a29d5d4ef7682286ab84
Van Nguyen <nguyentvan7@gmail.com>
2022-04-05 23:03:13 -0500

    Change to 3x2 grid with set effects

--- src/PageCharacter/CharacterCard.tsx
+++ src/PageCharacter/CharacterCard.tsx
@@ -1,4 +1,4 @@
-import { Box, CardActionArea, CardContent, Chip, Grid, Skeleton, Typography } from '@mui/material';
+import { Box, CardActionArea, CardContent, Chip, Grid, Skeleton, Stack, Typography } from '@mui/material';
 import { Suspense, useCallback, useContext, useMemo } from 'react';
 import { SlotIconSVG } from '../Components/Artifact/SlotNameWIthIcon';
 import BootstrapTooltip from '../Components/BootstrapTooltip';
@@ -62,11 +62,11 @@ export default function CharacterCard({ characterKey, artifactChildren, weaponCh
       <CardLight sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
         <ConditionalWrapper condition={!!onClick} wrapper={actionWrapperFunc} >
           <Header onClick={!onClick ? onClickHeader : undefined} />
-          <CardContent sx={{ width: "100%", display: "flex", flexDirection: "column", gap: 1, flexGrow: 1, pl: 1, pr: 1 }}>
+          <CardContent sx={{ width: "100%", display: "flex", flexDirection: "column", gap: 1, flexGrow: 1 }}>
             <Weapon weaponId={character.equippedWeapon} />
             {weaponChildren}
             {/* will grow to fill the 100% height */}
-            <Box flexGrow={1} display="flex" flexDirection="column" gap={0.5}>
+            <Box flexGrow={1} display="flex" flexDirection="column" gap={1}>
               <ArtifactDisplay />
               {artifactChildren}
             </Box>
@@ -194,22 +194,38 @@ function ArtifactDisplay() {
   const artifacts = useMemo(() =>
     allSlotKeys.map(k => [k, database._getArt(data.get(input.art[k].id).value ?? "")]),
     [data, database]) as Array<[SlotKey, ICachedArtifact | undefined]>;
+  const setEffects = useMemo(() => artifactSheets && ArtifactSheet.setEffects(artifactSheets, data), [artifactSheets, data])
   if (!artifactSheets) return null
 
-  return <Grid container spacing={0.25}>
+  return <Grid direction="row" container spacing={1} rowSpacing={0.5}>
+    {/* Set effect box */}
+    <Grid item key={"setEffects"} xs={4}>
+      <CardDark variant="outlined" sx={{ height: "100%" }}>
+        <Grid container>
+          {setEffects && Object.entries(setEffects).map(([setKey, setNumKeyArr]) =>
+            <Grid display="flex" alignItems="center" item xs={12} sx={{ pl: 1, pr: 1 }}>
+              <ImgIcon src={artifactSheets[setKey].slotIcons.flower ?? artifactSheets[setKey].slotIcons.circlet} sx={{ height: "2.5em", pt: 0.5 }} />
+              <SqBadge color="success">{setNumKeyArr.at(-1)}-Set</SqBadge>
+            </Grid>
+          )}
+        </Grid>
+      </CardDark>
+    </Grid>
+
+    {/* Artifact piece box */}
     {artifacts.map(([key, art]: [SlotKey, ICachedArtifact | undefined]) => {
-      if (!art) return <Grid item key={key} flexGrow={1}>
+      if (!art) return <Grid item key={key} xs={4}>
         <CardDark variant="outlined" sx={{ height: "100%" }}>
-          <Box sx={{ pl: 0.3, pr: 0.3, pt: 1, pb: 1, textAlign: "center" }}>
+          <Box sx={{ pl: 1, pr: 1, pt: 1, pb: 1, textAlign: "center" }}>
             <FontAwesomeIcon icon={SlotIconSVG[key]} key={key} className="fa-fw" size="3x" />
           </Box>
         </CardDark>
       </Grid>
 
       const { setKey, slotKey, mainStatKey, rarity, level, mainStatVal } = art
-      return <Grid item key={key} flexGrow={1}>
+      return <Grid item key={key} xs={4}>
         <CardDark variant="outlined" sx={{ borderColor: theme.palette[`star${rarity}`].main }}>
-          <Box sx={{ pl: 0.3, pr: 0.3, pt: 0.25, pb: 1, textAlign: "center" }}>
+          <Box sx={{ pl: 1, pr: 1, pt: 0.25, pb: 1, textAlign: "center" }}>
             <ImgIcon src={artifactSheets?.[setKey].slotIcons[slotKey]} sx={{ height: "3.5em" }} />
             <Typography variant='subtitle1' sx={{ display: "flex", gap: 1, mb: 0.5 }} >
               <SqBadge color="primary">Lv. {level}</SqBadge>
