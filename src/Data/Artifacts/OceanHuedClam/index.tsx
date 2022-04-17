22332c824480ea56d5c7ba97ecb263acb316ba7f
Van Nguyen <nguyentvan7@gmail.com>
2022-04-16 21:28:10 -0500

    Fix OHC damage calculation #238

--- src/Data/Artifacts/OceanHuedClam/index.tsx
+++ src/Data/Artifacts/OceanHuedClam/index.tsx
@@ -1,17 +1,17 @@
 import { input } from '../../../Formula'
 import { Data } from '../../../Formula/type'
-import { constant, infoMut, percent, prod, greaterEq } from '../../../Formula/utils'
+import { infoMut, percent, prod, greaterEq } from '../../../Formula/utils'
 import { ArtifactSetKey } from '../../../Types/consts'
-import { customDmgNode } from '../../Characters/dataUtil'
 import { ArtifactSheet, IArtifactSheet } from '../ArtifactSheet'
 import { dataObjForArtifactSheet } from '../dataUtil'
 import icons from './icons'
 const key: ArtifactSetKey = "OceanHuedClam"
 const set2 = greaterEq(input.artSet.OceanHuedClam, 2, percent(0.15))
 const heal = greaterEq(input.artSet.OceanHuedClam, 4,
-  customDmgNode(prod(percent(0.9), 30000), "elemental", {
-    hit: { ele: constant("physical"), hitMode: constant("hit") }
-  })
+  prod(
+    prod(percent(0.9), 30000),
+    input.enemy.physical_resMulti
+  )
 )
 
 export const data: Data = dataObjForArtifactSheet(key, {
