9ea12c9a69ea66899814ed2bf1b56e973a8637ae
frzyc <frzyc@users.noreply.github.com>
2022-04-17 20:28:07 -0400

    Updates

---
+++ static/js/970.ff463347.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgenshin_optimizer=self.webpackChunkgenshin_optimizer||[]).push([[970],{8970:function(e,n,t){t.r(n),t.d(n,{default:function(){return s}});var r=t(68870),i=t(72791),u=t(80184),a=(0,i.lazy)((function(){return Promise.all([t.e(958),t.e(0),t.e(455),t.e(147),t.e(282),t.e(684),t.e(966),t.e(918),t.e(561),t.e(129),t.e(40),t.e(839),t.e(743)]).then(t.bind(t,51839))}));function s(){return(0,u.jsx)(r.Z,{sx:{py:1},children:(0,u.jsx)(a,{characterKey:"Shenhe",tabName:"talent"})})}}}]);
+//# sourceMappingURL=970.ff463347.chunk.js.map
