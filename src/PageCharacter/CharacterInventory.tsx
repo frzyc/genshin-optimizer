6e8b7b720867bcc114162254267ca4607a9efe37
Van Nguyen <nguyentvan7@gmail.com>
2022-04-20 23:34:47 -0500

    Add quick buttons to CharacterInventory card footer

--- src/PageCharacter/CharacterInventory.tsx
+++ src/PageCharacter/CharacterInventory.tsx
@@ -1,10 +1,13 @@
 import { faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Box, Button, CardContent, Divider, Grid, Skeleton, Typography } from '@mui/material';
+import { Calculate, Checkroom, DeleteForever, FactCheck, Groups } from '@mui/icons-material';
+import { Box, Button, CardContent, Divider, Grid, IconButton, Skeleton, Typography } from '@mui/material';
 import i18next from 'i18next';
 import React, { Suspense, useCallback, useContext, useEffect, useMemo, useState } from 'react';
 import ReactGA from 'react-ga';
 import { Trans, useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
+import BootstrapTooltip from '../Components/BootstrapTooltip';
 import CardDark from '../Components/Card/CardDark';
 import { CharacterSelectionModal } from '../Components/Character/CharacterSelectionModal';
 import SortByButton from '../Components/SortByButton';
@@ -52,11 +55,13 @@ export default function CharacterInventory(props) {
     if (typeof name === "object")
       name = i18next.t(`char_${cKey}_gen:name`)
 
-    if (!window.confirm(`Are you sure you want to remove ${name}?`)) return
+    if (!window.confirm(t("removeCharacter", { value: name }))) return
     database.removeChar(cKey)
   }, [database])
 
   const editCharacter = useCharSelectionCallback()
+
+  const history = useHistory()
 
   const { element, weaponType } = state
   const sortConfigs = useMemo(() => characterSheets && characterSortConfigs(database, characterSheets), [database, characterSheets])
@@ -115,12 +120,32 @@ export default function CharacterInventory(props) {
               characterKey={charKey}
               onClick={editCharacter}
               footer={<><Divider /><Box sx={{ py: 1, px: 2, display: "flex", gap: 1, justifyContent: "space-between" }}>
-                <Box></Box>
-                {/* <Button size="small" component={Link} to={{
-                  pathname: "/build",
-                  characterKey: charKey
-                } as any} startIcon={<FontAwesomeIcon icon={faCalculator} />}>Build</Button> */}
-                <Button size="small" color="error" startIcon={<FontAwesomeIcon icon={faTrash} />} onClick={() => deleteCharacter(charKey)}>Delete</Button>
+                <BootstrapTooltip placement="top" title={<Typography>{t("tabs.talent")}</Typography>}>
+                  <IconButton onClick={() => history.push(`/character/${charKey}/talent`)}>
+                    <FactCheck />
+                  </IconButton>
+                </BootstrapTooltip>
+                <BootstrapTooltip placement="top" title={<Typography>{t("tabs.equip")}</Typography>}>
+                  <IconButton onClick={() => history.push(`/character/${charKey}/equip`)} >
+                    <Checkroom />
+                  </IconButton>
+                </BootstrapTooltip>
+                <BootstrapTooltip placement="top" title={<Typography>{t("tabs.buffs")}</Typography>}>
+                  <IconButton onClick={() => history.push(`/character/${charKey}/buffs`)} >
+                    <Groups />
+                  </IconButton>
+                </BootstrapTooltip>
+                <BootstrapTooltip placement="top" title={<Typography>{t("tabs.build")}</Typography>}>
+                  <IconButton onClick={() => history.push(`/character/${charKey}/build`)} >
+                    <Calculate />
+                  </IconButton>
+                </BootstrapTooltip>
+                <Divider orientation="vertical" />
+                <BootstrapTooltip placement="top" title={<Typography>{t("delete")}</Typography>}>
+                  <IconButton color="error" onClick={() => deleteCharacter(charKey)}>
+                    <DeleteForever />
+                  </IconButton>
+                </BootstrapTooltip>
               </Box></>}
             />
           </Grid>)}
