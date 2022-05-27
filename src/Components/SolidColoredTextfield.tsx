1ffed920147d7c30256ee721fdb4714b80620321
frzyc <frzyc@users.noreply.github.com>
2022-05-27 15:25:01 -0400

    round autocomplete textfield, fix padding

--- src/Components/SolidColoredTextfield.tsx
+++ src/Components/SolidColoredTextfield.tsx
@@ -1,11 +1,11 @@
 import { TextField, TextFieldProps, useTheme } from "@mui/material";
 
-type SolidColoredTextFieldProps = Omit<TextFieldProps, "multiline" | "variant" |"color" | "hiddenLabel" | "type" | "InputLabelProps"> & {
+type SolidColoredTextFieldProps = Omit<TextFieldProps, "multiline" | "variant" | "color" | "hiddenLabel" | "type" | "InputLabelProps"> & {
   hasValue: boolean
   startAdornment?: Displayable
   flattenCorners?: boolean
 }
-export default function SolidColoredTextField({ hasValue, startAdornment, flattenCorners = false, ...props }: SolidColoredTextFieldProps) {
+export default function SolidColoredTextField({ hasValue, startAdornment, flattenCorners = false, InputProps, sx, ...props }: SolidColoredTextFieldProps) {
   const theme = useTheme()
   console.log(flattenCorners)
   return <TextField
@@ -16,16 +16,16 @@ export default function SolidColoredTextField({ hasValue, startAdornment, flatte
     hiddenLabel={props.label ? false : true}
     type="search"
     InputProps={{
-      ...props.InputProps,
+      ...InputProps,
       startAdornment,
     }}
     InputLabelProps={{ style: { color: theme.palette.text.primary } }}
     sx={{
-      ...props.sx,
-      "& .MuiFilledInput-root": { paddingRight: 0, backgroundColor: hasValue ? theme.palette.success.main : theme.palette.primary.main, borderRadius: flattenCorners ? 0 : undefined },
+      ...sx,
+      "& .MuiFilledInput-root": { backgroundColor: hasValue ? theme.palette.success.main : theme.palette.primary.main, borderRadius: flattenCorners ? 0 : 1, paddingTop: props.label ? undefined : 0, paddingBottom: 0 },
       "& .MuiFilledInput-root:before": { border: "none" },
       "& .MuiFilledInput-root:after": { border: "none" },
-      "& .MuiFilledInput-root.Mui-focused": { paddingRight: 0, backgroundColor: hasValue ? theme.palette.success.main : theme.palette.primary.main, borderRadius: flattenCorners ? 0 : undefined },
+      "& .MuiFilledInput-root.Mui-focused": { backgroundColor: hasValue ? theme.palette.success.light : theme.palette.primary.light },
       "& .MuiFilledInput-root:hover": { backgroundColor: hasValue ? theme.palette.success.dark : theme.palette.primary.dark },
       "& .MuiFilledInput-root:hover:not(.Mui-disabled):before": { border: "none" },
     }}
