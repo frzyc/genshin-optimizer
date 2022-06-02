be124e0f188ce8f8b63d9f862ef1e38fe290a693
Van Nguyen <nguyentvan7@gmail.com>
2022-06-01 21:51:35 -0500

    Fix cosmetic issue with disabled autocomplete

--- src/Components/SolidColoredTextfield.tsx
+++ src/Components/SolidColoredTextfield.tsx
@@ -20,16 +20,18 @@ export default function SolidColoredTextField({ hasValue, startAdornment, flatte
     InputLabelProps={{ style: { color: theme.palette.text.primary } }}
     sx={{
       ...sx,
+      // Fill in the background without using the `focused` prop
       "& .MuiFilledInput-root": { backgroundColor: hasValue ? theme.palette.success.main : theme.palette.primary.main, borderRadius: flattenCorners ? 0 : 1, paddingTop: props.label ? undefined : 0, paddingBottom: 0 },
-      "& .MuiFilledInput-root:before": { border: "none" },
-      "& .MuiFilledInput-root:after": { border: "none" },
       "& .MuiFilledInput-root.Mui-focused": { backgroundColor: hasValue ? theme.palette.success.light : theme.palette.primary.light },
       "& .MuiFilledInput-root:hover": { backgroundColor: hasValue ? theme.palette.success.dark : theme.palette.primary.dark },
+      // Remove the colored bar at the bottom of the input
+      "& .MuiFilledInput-root:before": { border: "none" },
+      "& .MuiFilledInput-root.Mui-disabled:before": { border: "none" },
+      "& .MuiFilledInput-root:after": { border: "none" },
       "& .MuiFilledInput-root:hover:not(.Mui-disabled):before": { border: "none" },
       // Remove the x at the end of search input for IE
       "& input[type=search]::-ms-clear": { display: "none", width: 0, height: 0 },
       "& input[type=search]::-ms-reveal": { display: "none", width: 0, height: 0 },
-
       // Remove the x at the end of search input for Chrome
       "& input[type=search]::-webkit-search-decoration": { display: "none" },
       "& input[type=search]::-webkit-search-cancel-button": { display: "none" },
