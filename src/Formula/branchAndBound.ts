db839453c7e9e52ce18b300313d91e3e5ed16477
tooflesswulf <tooflesswulf0325@gmail.com>
2022-06-12 01:50:07 -0400

    remove old code

--- src/Formula/branchAndBound.ts
+++ src/Formula/branchAndBound.ts
@@ -220,11 +220,9 @@ function bnbWorker({ f, a, thresh, cachedCompute }: WorkerPayload): { branches: 
     let f2 = simplifyFormulaSubstitution(f0, statsMin, statsMax)
 
     const maxEst1 = maxWeight(z, lin)
-    // maxWeight & estimateMaximum should spit out a feasible build
     if (maxEst1 < thr0) return
 
     let est2 = estimateMaximum(f2, z)
-    // maxWeight & estimateMaximum should spit out a feasible build
     const { maxEst, lin: linToUse } = maxEst1 < est2.maxEst ? { maxEst: maxEst1, lin } : est2
     if (maxEst < thr0) return
 
@@ -340,243 +338,7 @@ export function optimizeBNB(func: NumNode, a: ArtifactsBySlot) {
   console.log({ enumerated, pruned, tottal })
 }
 
-export function optimizeBNB_old(func: NumNode, a: ArtifactsBySlot) {
-  a = {
-    base: a.base,
-    values: {
-      flower: a.values.flower.filter(art => art.id !== ''),
-      plume: a.values.plume.filter(art => art.id !== ''),
-      sands: a.values.sands.filter(art => art.id !== ''),
-      goblet: a.values.goblet.filter(art => art.id !== ''),
-      circlet: a.values.circlet.filter(art => art.id !== ''),
-    }
-  }
 
-  // debug3()
-  // return
-
-  console.log('arts: ', a)
-  console.log('f', func)
-
-  // 1 more optimize loop for good measure.
-  let { statsMin, statsMax } = boundsUpperLower(a)
-  func = simplifyFormulaSubstitution(func, statsMin, statsMax)
-
-  // careful, this may consume O(n^5) memory.
-  type Packet = { maxEst: number, f: NumNode, a: ArtifactsBySlot, lin: LinearForm }
-  let pq = new PriorityQueue<Packet>((a, b) => a.maxEst > b.maxEst)
-
-  const { maxEst, lin } = estimateMaximum(func, a)
-  let lowerBoundDmg = feasibleObjVal(func, a, lin).dmgVal
-  // First search node; contains ALL builds.
-  pq.push({ maxEst, f: func, a, lin })
-
-
-
-  let niter = 0
-  const tottal = Object.values(a.values).reduce((tot, arts) => tot * arts.length, 1)
-  let pruned = 0
-  let enumerated = 0
-  while (!pq.isEmpty()) {
-    let { maxEst, f, a, lin } = pq.pop()
-    let numBuilds = Object.values(a.values).reduce((tot, arts) => tot * arts.length, 1)
-    niter += 1
-    if (niter > 1000) break;
-
-    //  Stats *should* already be subbed in, but it doesn't hurt to try again.
-    let { statsMin, statsMax } = boundsUpperLower(a)
-    f = simplifyFormulaSubstitution(f, statsMin, statsMax)
-
-    let est = estimateMaximum(f, a)
-    if (est.maxEst < feasibleObjVal(f, a, est.lin).dmgVal) {
-      console.log('~~~~~~~~~~~~ BIG PROBLEM UH OH ~~~~~~~~~~~~')
-      debug2(a, f)
-      return
-    }
-    if (isNaN(est.maxEst)) {
-      console.log('uh oh 2')
-      // debug2(a, f)
-      return
-    }
-    if (est.maxEst < maxEst) {
-      maxEst = est.maxEst;
-      lin = est.lin;
-    }
-    // If re-evaluation tells us this branch is worthless, then just skip.
-    lowerBoundDmg = Math.max(feasibleObjVal(f, a, lin).dmgVal, lowerBoundDmg)
-    if (maxEst < lowerBoundDmg) {
-      console.log({ niter, numBuilds, pqsize: pq.size() }, { lower: lowerBoundDmg, upper: maxEst }, 'PRUNED')
-      pruned += numBuilds
-      continue
-    }
-
-    console.log({ niter, numBuilds, pqsize: pq.size() }, { lower: lowerBoundDmg, upper: maxEst, err: lin.err })
-    // console.log('Total pruned: ', pruned)
-
-    // TODO: count `a`, if small then just enumerate it all.
-    if (numBuilds < 2000) {
-      // TODO: enumerate
-      enumerated += numBuilds
-      console.log('--------------- ENUMERATE (implement me please) ---------------')
-      continue
-    }
-
-    if (niter < 3) {
-      // For earlier iterations, shatter search space on `score` to separate
-      //   obviously good vs obviously bad artifacts (e.g. fully leveled vs lvl 0 artifacts)
-      //
-      // Performance of shattering on score degrades quickly because overall BnB performance
-      //   depends mostly on approximation error; On an level playing field we prefer branching
-      //   on stat values or artifact set.
-      const scores = Object.fromEntries(Object.entries(a.values).map(([slotKey, arts]) => {
-        let scores = arts.map(a => Object.entries(lin.w).reduce((dot, [statKey, w]) => dot + w * a.values[statKey], 0))
-        return [slotKey, scores]
-      }))
-      const scoreBranchVal = Object.fromEntries(Object.entries(scores).map(([slotKey, scores]) =>
-        [slotKey, (Math.min(...scores) + Math.max(...scores)) / 2]
-      ))
-      cartesian([0, 1], [0, 1], [0, 1], [0, 1], [0, 1])
-        .forEach(([s1, s2, s3, s4, s5]) => {
-          let z = {
-            base: { ...a.base }, values: {
-              flower: a.values.flower.filter((art, i) => scores.flower[i] < scoreBranchVal.flower ? s1 === 0 : s1 === 1),
-              plume: a.values.plume.filter((art, i) => scores.plume[i] < scoreBranchVal.plume ? s2 === 0 : s2 === 1),
-              sands: a.values.sands.filter((art, i) => scores.sands[i] < scoreBranchVal.sands ? s3 === 0 : s3 === 1),
-              goblet: a.values.goblet.filter((art, i) => scores.goblet[i] < scoreBranchVal.goblet ? s4 === 0 : s4 === 1),
-              circlet: a.values.circlet.filter((art, i) => scores.circlet[i] < scoreBranchVal.circlet ? s5 === 0 : s5 === 1),
-            }
-          }
-          let numBuilds = Object.values(z.values).reduce((tot, arts) => tot * arts.length, 1)
-          if (numBuilds === 0) return;
-
-          const { statsMin, statsMax } = boundsUpperLower(z)
-          let f2 = simplifyFormulaSubstitution(f, statsMin, statsMax)
-
-          const maxEst1 = maxWeight(z, lin)
-          if (maxEst1 < lowerBoundDmg) {
-            pruned += numBuilds
-            return
-          }
-
-          // Possible TODO: re-compute `lin` on `z`.
-          let est2 = estimateMaximum(f2, z)
-
-          // const [maxEst, linToUse] = [maxEst1, lin]
-          const maxEst2 = maxWeight(z, est2.lin)
-          const [maxEst, linToUse] = maxEst1 < maxEst2 ? [maxEst1, lin] : [maxEst2, est2.lin]
-          lowerBoundDmg = Math.max(feasibleObjVal(f2, z, linToUse).dmgVal, lowerBoundDmg)
-          if (maxEst < lowerBoundDmg) {
-            pruned += numBuilds
-            return
-          }
-
-          // console.log(s1, s2, s3, s4, s5, '(', numBuilds, ')', maxEst, linToUse.err)
-          console.log(s1, s2, s3, s4, s5, { numBuilds, upper: maxEst })
-          pq.push({ maxEst, lin: linToUse, f: f2, a: z })
-        })
-
-      continue
-    }
-
-    // console.log('smin', statsMin)
-    // console.log('smax', statsMax)
-    // console.log('lin', lin)
-
-    // Heuristically pick branch parameter by picking the one with largest
-    //   linear weight & the largest reduction in stat range.
-    let shatterOn = { k: '', heur: -1 }
-    Object.keys(lin.w).forEach(k => {
-      const postShatterRange = Object.entries(a.values).reduce((newRange, [slotKey, arts]) => {
-        const vals = arts.map(a => a.values[k])
-        const minv = Math.min(...vals)
-        const maxv = Math.max(...vals)
-        const branchVal = (minv + maxv) / 2
-
-        // greatest lower bound/least upper bound for estimating range post-shatter
-        const glb = Math.max(...vals.filter(v => v < branchVal))
-        const lub = Math.min(...vals.filter(v => v >= branchVal))
-        const newRangeSlot = Math.max(glb - minv, maxv - lub)
-        return newRangeSlot + newRange
-      }, 0)
-      let shatterHeur = lin.w[k] * (statsMax[k] - statsMin[k] - postShatterRange)
-      // console.log(k, shatterHeur, statsMax[k] - statsMin[k], postShatterRange)
-      if (shatterHeur > shatterOn.heur) shatterOn = { k: k, heur: shatterHeur }
-    })
-
-    console.log('shatterOn', shatterOn)
-    if (shatterOn.heur < 0) {
-      console.log('wow this actually way better?')
-      return
-    }
-    // return;
-
-    const branchVals = Object.fromEntries(Object.entries(a.values).map(([slotKey, arts]) => {
-      const vals = arts.map(a => a.values[shatterOn.k])
-      return [slotKey, (Math.min(...vals) + Math.max(...vals)) / 2]
-    }))
-
-    // Instead of shattering all 32 branches in parallel, we can do it sequentially and have earlier
-    // branch pruning by checking their upper bound. Should speed up tail-end calculations
-    //  by 16-32x
-    cartesian([0, 1], [0, 1], [0, 1], [0, 1], [0, 1]).forEach(sel => {
-      const [s1, s2, s3, s4, s5] = sel
-      let z = {
-        base: { ...a.base }, values: {
-          flower: a.values.flower.filter(art => art.values[shatterOn.k] < branchVals.flower ? s1 === 0 : s1 === 1),
-          plume: a.values.plume.filter(art => art.values[shatterOn.k] < branchVals.plume ? s2 === 0 : s2 === 1),
-          sands: a.values.sands.filter(art => art.values[shatterOn.k] < branchVals.sands ? s3 === 0 : s3 === 1),
-          goblet: a.values.goblet.filter(art => art.values[shatterOn.k] < branchVals.goblet ? s4 === 0 : s4 === 1),
-          circlet: a.values.circlet.filter(art => art.values[shatterOn.k] < branchVals.circlet ? s5 === 0 : s5 === 1),
-        }
-      }
-      let numBuilds = Object.values(z.values).reduce((tot, arts) => tot * arts.length, 1)
-      if (numBuilds === 0) return;
-
-      const { statsMin, statsMax } = boundsUpperLower(z)
-      let f2 = simplifyFormulaSubstitution(f, statsMin, statsMax)
-
-      const maxEst1 = maxWeight(z, lin)
-      if (maxEst1 < lowerBoundDmg) {
-        pruned += numBuilds
-        return
-      }
-
-      // Possible TODO: re-compute `lin` on `z`.
-      let est2 = estimateMaximum(f2, z)
-      // TODO: remove this sanity checker.
-      if (est2.maxEst + 1 < feasibleObjVal(f2, z, est2.lin).dmgVal) {
-        console.log('!!!!!!!!!!!!! BIG PROBLEM UH OH !!!!!!!!!!!!!')
-        pq.push({ maxEst: Infinity, lin: est2.lin, f: f2, a: z })
-        return
-      }
-
-      const maxEst2 = maxWeight(z, est2.lin)
-      const [maxEst, linToUse] = maxEst1 < maxEst2 ? [maxEst1, lin] : [maxEst2, est2.lin]
-      lowerBoundDmg = Math.max(feasibleObjVal(f2, z, linToUse).dmgVal, lowerBoundDmg)
-      if (maxEst < lowerBoundDmg) {
-        pruned += numBuilds
-        return
-      }
-
-      console.log(s1, s2, s3, s4, s5, { numBuilds, upper: maxEst, err: linToUse.err })
-      pq.push({ maxEst, lin: linToUse, f: f2, a: z })
-    })
-    continue
-  }
-
-  console.log(pq)
-
-  let totbuilds = 0
-  while (!pq.isEmpty()) {
-    let { a } = pq.pop()
-    let numBuilds = Object.values(a.values).reduce((tot, arts) => tot * arts.length, 1)
-    totbuilds = totbuilds + numBuilds
-  }
-
-  console.log('builds left:', totbuilds)
-  console.log('Total enumerated vs pruned:', enumerated, pruned, ' out of total ', tottal)
-  console.log('Final damage:', lowerBoundDmg)
-}
 
 function debug2(a: ArtifactsBySlot, f: NumNode) {
   console.log(a)
@@ -652,8 +414,8 @@ function debug2(a: ArtifactsBySlot, f: NumNode) {
   console.log(estimateMaximum(f, a))
 
   console.log('=========== shitty debug time ===============')
-  console.log(ww[0])
-  console.log(toLinearUpperBound(ww[0], statsMin, statsMax))
+  // console.log(ww[0])
+  // console.log(toLinearUpperBound(ww[0], statsMin, statsMax))
 }
 
 function debug3() {
