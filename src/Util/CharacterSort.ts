05f5d7427a9a0027b1ca48600dda422b76afe8f2
frzyc <frzyc@users.noreply.github.com>
2022-06-20 06:28:50 -0400

    Allows "new" Traveler to be filtered by correct elements. Resolves #457

--- src/Util/CharacterSort.ts
+++ src/Util/CharacterSort.ts
@@ -34,7 +34,9 @@ export function characterSortConfigs(database: ArtCharDatabase, characterSheets:
 export type CharacterFilterConfigs = FilterConfigs<"element" | "weaponType" | "favorite" | "name", CharacterKey>
 export function characterFilterConfigs(database: ArtCharDatabase, characterSheets: Record<CharacterKey, CharacterSheet>): CharacterFilterConfigs {
   return {
-    element: (ck, filter) => filter.includes(characterSheets?.[ck]?.elementKey) || (ck === "Traveler" && !database._getChar(ck as CharacterKey)) || (ck === "Traveler" && filter.includes(database._getChar(ck as CharacterKey)?.elementKey)),
+    element: (ck, filter) => filter.includes(characterSheets?.[ck]?.elementKey) ||
+      (ck === "Traveler" && !database._getChar(ck as CharacterKey) && filter.some(fe => characterSheets.Traveler.elementKeys.includes(fe))) ||
+      (ck === "Traveler" && filter.includes(database._getChar(ck as CharacterKey)?.elementKey)),
     weaponType: (ck, filter) => filter.includes(characterSheets?.[ck]?.weaponTypeKey),
     favorite: (ck, filter) =>
       !filter || (filter === (database._getChar(ck as CharacterKey)?.favorite ? "yes" : "no")),
