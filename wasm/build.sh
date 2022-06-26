b6a48671f339731db683eac564f65bfd5f3f7ff8
tooflesswulf <tooflesswulf0325@gmail.com>
2022-06-26 00:29:03 -0400

    merge master-with-wasm into branch-and-bound

--- wasm/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "${0%/*}"
-
-docker run --rm -v `pwd`:/project stargate01/f90wasm bash -c 'cd /project && VERBOSE=1 EM_CACHE=/project/bin/.emscripten_cache make build'
-
-cp `pwd`/bin/assembly.js `pwd`/../wasmpack/
-cp `pwd`/bin/assembly.wasm `pwd`/../public/static/js/
-echo "export {Module};" >> `pwd`/../wasmpack/assembly.js
