f9043fe45fed6766d5c558acf2642b7db0f2f301
frzyc <frzyc@users.noreply.github.com>
2022-07-05 06:57:41 -0400

    Resolves #475 - Fix TC mode not updating when swapping dataabases

--- src/ReactHooks/useDBState.tsx
+++ src/ReactHooks/useDBState.tsx
@@ -3,11 +3,12 @@ import { DatabaseContext } from "../Database/Database";
 
 export default function useDBState<O extends object>(key: string, init: () => O): [O, (value: Partial<O>) => void] {
   const { database } = useContext(DatabaseContext)
-  const [state, setState] = useState(database.states.getWithInit<O>(key, init))
+  const [state, setState] = useState(() => database.states.getWithInit<O>(key, init))
 
   useEffect(() =>
     key ? database.states.follow(key, setState as any) : undefined,
     [key, setState, database])
+  useEffect(() => setState(database.states.getWithInit<O>(key, init)), [database, init, key])
   const updateState = useCallback(
     value => database.states.set(key, value),
     [database, key],
