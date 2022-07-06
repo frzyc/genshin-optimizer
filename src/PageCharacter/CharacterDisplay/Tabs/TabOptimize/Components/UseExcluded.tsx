34bfebd75a4bb4edf21d92c689b082ad6d07d170
Van Nguyen <nguyentvan7@gmail.com>
2022-07-05 22:50:04 -0500

    Fix excluded artifact count including current character artifacts

--- src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/UseExcluded.tsx
+++ src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/UseExcluded.tsx
@@ -12,7 +12,7 @@ export default function UseExcluded({ disabled = false, artsDirty }: { disabled?
   const { character: { key: characterKey } } = useContext(CharacterContext)
   const { buildSetting: { useExcludedArts }, buildSettingDispatch } = useBuildSetting(characterKey)
   const { database } = useContext(DatabaseContext)
-  const numExcludedArt = useMemo(() => artsDirty && database.arts.values.reduce((a, art) => a + (art.exclude ? 1 : 0), 0), [database, artsDirty])
+  const numExcludedArt = useMemo(() => artsDirty && database.arts.values.reduce((a, art) => a + ((art.exclude && art.location !== characterKey) ? 1 : 0), 0), [database, artsDirty, characterKey])
   return <Button fullWidth onClick={() => buildSettingDispatch({ useExcludedArts: !useExcludedArts })} disabled={!numExcludedArt || disabled} startIcon={useExcludedArts ? <CheckBox /> : <CheckBoxOutlineBlank />} color={useExcludedArts ? "success" : "secondary"}>
     <Box>
       <span><Trans t={t} i18nKey="tabOptimize.useExcluded.title" count={numExcludedArt}>Use Excluded Artifacts</Trans></span>
