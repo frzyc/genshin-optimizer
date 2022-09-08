8c47665121eb8f2919a7fa252fae85f67778163d
frzyc <frzyc@users.noreply.github.com>
2022-09-07 21:24:59 -0400

    Fix #603 - fix UI not showing up on swap UI when character has empty artifact slots. (#604)

--- src/PageCharacter/CharacterDisplay/Tabs/TabOverview/CompareBuildButton.tsx
+++ src/PageCharacter/CharacterDisplay/Tabs/TabOverview/CompareBuildButton.tsx
@@ -16,6 +16,17 @@ import BuildDisplayItem from "../TabOptimize/Components/BuildDisplayItem"
 export default function CompareBuildButton({ artId, weaponId }: { artId?: string, weaponId?: string }) {
   const { t } = useTranslation("page_character")
   const [show, onShow, onHide] = useBoolState(false)
+
+  return <>
+    <ModalWrapper open={show} onClose={onHide} containerProps={{ maxWidth: "xl" }}>
+      <CompareContent artId={artId} weaponId={weaponId} onHide={onHide} />
+    </ModalWrapper>
+    <Tooltip title={<Typography>{t`tabEquip.compare`}</Typography>} placement="top" arrow>
+      <Button color="info" size="small" onClick={onShow} ><Difference /></Button>
+    </Tooltip>
+  </>
+}
+function CompareContent({ artId, weaponId, onHide }: { artId?: string, weaponId?: string, onHide: () => void }) {
   const { database } = useContext(DatabaseContext)
   const { character: { key: characterKey, equippedArtifacts } } = useContext(CharacterContext)
   const { buildSetting: { mainStatAssumptionLevel } } = useBuildSetting(characterKey)
@@ -23,20 +34,13 @@ export default function CompareBuildButton({ artId, weaponId }: { artId?: string
   const build = useMemo(() => {
     const newArt = database.arts.get(artId ?? "")
     const artmap = objectMap(equippedArtifacts, (id, slot) => slot === newArt?.slotKey ? newArt : database.arts.get(id))
-    return Object.values(artmap)
+    return Object.values(artmap).filter(a => a)
   }, [database, equippedArtifacts, artId])
   const teamData = useTeamData(characterKey, mainStatAssumptionLevel, build, weaponId ? database.weapons.get(weaponId) : undefined)
   const dataProviderValue = useMemo(() => teamData && ({ data: teamData[characterKey]!.target, teamData, oldData }), [characterKey, teamData, oldData])
-  return <>
-    <ModalWrapper open={show} onClose={onHide} containerProps={{ maxWidth: "xl" }}>
-      <Suspense fallback={<Skeleton variant="rectangular" width="100%" height={600} />}>
-        {dataProviderValue && <DataContext.Provider value={dataProviderValue}>
-          <BuildDisplayItem compareBuild={true} extraButtons={<><HitModeToggle size="small" /><ReactionToggle size="small" /><Button size='small' color="error" onClick={onHide} ><Close /></Button></>} />
-        </DataContext.Provider>}
-      </Suspense>
-    </ModalWrapper>
-    <Tooltip title={<Typography>{t`tabEquip.compare`}</Typography>} placement="top" arrow>
-      <Button color="info" size="small" onClick={onShow} ><Difference /></Button>
-    </Tooltip>
-  </>
+  return <Suspense fallback={<Skeleton variant="rectangular" width="100%" height={600} />}>
+    {dataProviderValue && <DataContext.Provider value={dataProviderValue}>
+      <BuildDisplayItem compareBuild={true} extraButtons={<><HitModeToggle size="small" /><ReactionToggle size="small" /><Button size='small' color="error" onClick={onHide} ><Close /></Button></>} />
+    </DataContext.Provider>}
+  </Suspense>
 }
