fa47faa324697fa1b4b7d81911ede7d8258e223c
Van Nguyen <nguyentvan7@gmail.com>
2022-10-10 13:43:45 -0700

    Add dbmeta field for latest version the DB was opened in

--- src/Database/DataEntries/DBMetaEntry.ts
+++ src/Database/DataEntries/DBMetaEntry.ts
@@ -6,14 +6,16 @@ import { IGO, IGOOD, ImportResult } from "../exim";
 interface IDBMeta {
   name: string,
   lastEdit: number,
-  gender: Gender
+  gender: Gender,
+  latestVersion: string,
 }
 
 function dbMetaInit(): IDBMeta {
   return {
     name: `Database`,
     lastEdit: 0,
-    gender: "F"
+    gender: "F",
+    latestVersion: "1.0.0"
   }
 }
 
@@ -23,19 +25,20 @@ export class DBMetaEntry extends DataEntry<"dbMeta", "dbMeta", IDBMeta, IDBMeta>
   }
   validate(obj: any): IDBMeta | undefined {
     if (typeof obj !== "object") return
-    let { name, lastEdit, gender } = obj
+    let { name, lastEdit, gender, latestVersion } = obj
     if (typeof name !== "string") name = `Database ${this.database.storage.getDBIndex()}`
     if (typeof lastEdit !== "number") console.log("lastEdit INVALID")
     if (typeof lastEdit !== "number") lastEdit = 0
     if (!genderKeys.includes(gender)) gender = "F"
+    if (typeof latestVersion !== "string") latestVersion = "1.0.0"
 
-    return { name, lastEdit, gender } as IDBMeta
+    return { name, lastEdit, gender, latestVersion } as IDBMeta
   }
   importGOOD(go: IGO & IGOOD, result: ImportResult): void {
     const data = go[this.goKey as any]
     if (data) {
-      // Don't copy over lastEdit data
-      const { lastEdit, ...rest } = data
+      // Don't copy over lastEdit or latestVersion data
+      const { lastEdit, latestVersion, ...rest } = data
       this.set(rest)
     }
   }
