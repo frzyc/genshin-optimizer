712ed60a80a29ae11af7902e26f7c2f05a3c59d3
lantua <16190491+lantua@users.noreply.github.com>
2022-10-27 17:34:16 -0400

    Remove checking on `PercentBadge` (#728)

--- src/Components/PercentBadge.tsx
+++ src/Components/PercentBadge.tsx
@@ -10,7 +10,6 @@ export default function PercentBadge({ value, max = 1, valid }: {
 }) {
   let [badgeColor, text]: [color: ButtonProps['color'], text: string] = typeof value === 'number' ?
     [`roll${clamp(Math.floor((value / max) * 10) - 4, 1, 6)}` as RollColorKey, value.toFixed() + "%"] : ["secondary", value]
-  if (value > max) valid = false
   if (!valid) badgeColor = "error"
   return <SqBadge color={badgeColor} >{text}</SqBadge>
 }
