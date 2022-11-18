c7e931e00111528f1958299adfbe7939e14dd7c5
Van Nguyen <nguyentvan7@gmail.com>
2022-11-17 23:02:23 -0700

    Add translation

--- src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/OptimizationTargetEditorList.tsx
+++ src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/OptimizationTargetEditorList.tsx
@@ -1,11 +1,11 @@
 import { CheckBox, CheckBoxOutlineBlank, DeleteForever } from "@mui/icons-material"
 import { Button, ButtonGroup } from "@mui/material"
 import { useCallback, useContext } from "react"
+import { useTranslation } from "react-i18next"
 import CustomNumberInput, { CustomNumberInputButtonGroupWrapper } from "../../../../../Components/CustomNumberInput"
 import { DataContext } from "../../../../../Context/DataContext"
 import { StatFilters, StatFilterSetting } from "../../../../../Database/DataManagers/BuildSettingData"
 import { NodeDisplay } from "../../../../../Formula/uiData"
-import useMediaQueryUp from "../../../../../ReactHooks/useMediaQueryUp"
 import { objPathValue } from "../../../../../Util/Util"
 import OptimizationTargetSelector from "./OptimizationTargetSelector"
 
@@ -62,6 +62,7 @@ type OptimizationTargetEditorItemProps = {
   disabled: boolean,
 }
 function OptimizationTargetEditorItem({ path, setting, setTarget, delTarget, setValue, setDisabled, disabled }: OptimizationTargetEditorItemProps) {
+  const { t } = useTranslation("page_character_optimize")
   const { data } = useContext(DataContext)
   const onChange = useCallback((val: number | undefined) => path && setValue(path, val ?? 0), [setValue, path])
   const buttonGroupStyle = {
@@ -76,7 +77,7 @@ function OptimizationTargetEditorItem({ path, setting, setTarget, delTarget, set
     {!!setting && !!path && <Button sx={buttonStyle} color={setting.disabled ? "secondary" : "success"} onClick={() => setDisabled(path, !setting.disabled)} disabled={disabled}>
       {setting.disabled ? <CheckBoxOutlineBlank /> : <CheckBox />}
     </Button>}
-    <OptimizationTargetSelector ignoreGlobal optimizationTarget={path} setTarget={setTarget} defaultText={"Select a Build Constraint Target"} />
+    <OptimizationTargetSelector ignoreGlobal optimizationTarget={path} setTarget={setTarget} defaultText={t("targetSelector.selectBuildTarget")} />
     <CustomNumberInputButtonGroupWrapper sx={{ flexBasis: 150, flexGrow: 1 }}>
       <CustomNumberInput
         float
