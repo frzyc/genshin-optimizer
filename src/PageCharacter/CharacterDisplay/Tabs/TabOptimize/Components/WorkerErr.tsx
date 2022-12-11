6dbd02a8c43b512ac83df97cacc01dd5fb403c4a
eeeqeee <103794572+eeeqeee@users.noreply.github.com>
2022-12-11 01:49:50 -0500

    Trans the worker error (#820)
    * Stop using undeclared deps
    
    @mui/system -> @mui/material
    react-router -> react-router-dom
    
    * merge imports
    
    * trans the strings
    Translate the error when worker fails to load

--- src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/WorkerErr.tsx
+++ src/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/WorkerErr.tsx
@@ -1,10 +1,12 @@
 import Alert from "@mui/material/Alert";
+import { useTranslation } from "react-i18next";
 
 export default function WorkerErr() {
+  const { t } = useTranslation("page_character_optimize")
   return <Alert severity="error" variant="filled" onClick={() => window.location.reload()} sx={{
     "& .MuiAlert-message": {
       flexGrow: 1,
       cursor: "pointer"
     }
-  }}>Worker failed to load. Click here to reload the page and try again</Alert>
+  }}>{t`workerLoadFailed`}</Alert>
 }
