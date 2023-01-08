4179b0ae127f5d719a7dad67f203aac4c3e383f9
frzyc <frzyc@users.noreply.github.com>
2023-01-08 11:13:45 -0500

    update dm

--- libs/dm/scripts/loadDM.ts
+++ libs/dm/scripts/loadDM.ts
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import * as path from 'path';
 
 const projectDir = path.resolve(__dirname, "..")
@@ -8,7 +8,10 @@ fs.access(`${projectDir}/GenshinData`, (err) => {
     console.log("./GenshinData doesn't exist, cloning repo...");
     exec(`cd ${projectDir} && git clone https://github.com/dimbreath/GenshinData.git --depth 1`)
   } else {
-    console.log("./GenshinData exists, updating repo...");
-    exec(`cd ${projectDir}/GenshinData && git pull --depth 1`)
+    if (execSync(`cd ${projectDir}/GenshinData && git fetch --dry-run`).toString()) {
+      console.log("./GenshinData exists & needs update, updating repo...");
+      exec(`cd ${projectDir}/GenshinData && git pull --depth 1`)
+    } else
+      console.log("./GenshinData exists & doesn't need update");
   }
 })
