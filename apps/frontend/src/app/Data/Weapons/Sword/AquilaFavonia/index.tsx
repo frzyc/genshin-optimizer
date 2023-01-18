b434a0ef0adae3a8fd091a2d6350b54a825dcb82
Van Nguyen <nguyentvan7@gmail.com>
2023-01-18 16:19:45 -0700

    Remove Aquila conditional

--- apps/frontend/src/app/Data/Weapons/Sword/AquilaFavonia/index.tsx
+++ apps/frontend/src/app/Data/Weapons/Sword/AquilaFavonia/index.tsx
@@ -15,13 +15,18 @@ const data_gen = data_gen_json as WeaponData
 
 const atkDealt = [2, 2.3, 2.6, 2.9, 3.2]
 const hpRegen = [1, 1.15, 1.3, 1.45, 1.6]
-const [condPath, condNode] = cond(key, "FalconOfTheWest")
 const atk_ = subscript(input.weapon.refineIndex, data_gen.addProps.map(x => x.atk_ ?? NaN))
-const heal = equal(input.weapon.key, key, equal(condNode, 'on', prod(subscript(input.weapon.refineIndex, hpRegen, { unit: "%" }), input.premod.atk)))
+const heal = equal(input.weapon.key, key, prod(subscript(input.weapon.refineIndex, hpRegen, { unit: "%" }), input.premod.atk))
 const dmg = equal(input.weapon.key, key,
-  equal(condNode, 'on', customDmgNode(prod(subscript(input.weapon.refineIndex, atkDealt, { unit: "%" }), input.premod.atk), "elemental", {
-    hit: { ele: constant("physical") }
-  })))
+  customDmgNode(
+    prod(
+      subscript(input.weapon.refineIndex, atkDealt, { unit: "%" }),
+      input.premod.atk
+    ),
+    "elemental",
+    { hit: { ele: constant("physical") } }
+  )
+)
 
 export const data = dataObjForWeaponSheet(key, data_gen, {
   premod: {
@@ -39,23 +44,12 @@ const sheet: IWeaponSheet = {
       node: atk_,
     }],
   }, {
-    value: condNode,
-    path: condPath,
-    name: st('takeDmg'),
-    header: headerTemplate(key, icon, iconAwaken, st("conditional")),
-    states: {
-      on: {
-        fields: [{
-          node: infoMut(heal, { name: stg("healing"), variant: "heal" })
-        }, {
-          node: infoMut(dmg, { name: st("dmg") })
-        }, {
-          text: stg("cd"),
-          value: 15,
-          unit: "s"
-        }]
-      }
-    }
+    header: headerTemplate(key, icon, iconAwaken, st("base")),
+    fields: [{
+      node: infoMut(heal, { name: stg("healing"), variant: "heal" })
+    }, {
+      node: infoMut(dmg, { name: st("dmg") })
+    }]
   }],
 }
 export default new WeaponSheet(key, sheet, data_gen, data)
