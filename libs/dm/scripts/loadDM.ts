d97bd54345b7aafddd210d8c49ad8bedd6b67def
frzyc <frzyc@users.noreply.github.com>
2023-01-19 13:38:34 -0500

    fix some pipeline annoyances

--- libs/dm/scripts/loadDM.ts
+++ libs/dm/scripts/loadDM.ts
@@ -1,16 +1,13 @@
-import * as fs from 'fs';
 import { execSync } from "child_process";
+import * as fs from 'fs';
 import * as path from 'path';
 
 const projectDir = path.resolve(__dirname, "..")
 
 if (fs.existsSync(`${projectDir}/GenshinData`)) {
-  if (execSync(`cd ${projectDir}/GenshinData && git fetch --dry-run`).toString()) {
-    console.log("./GenshinData exists & needs update, updating repo...");
-    execSync(`cd ${projectDir}/GenshinData && git pull --depth 1`)
-  } else
-    console.log("./GenshinData exists & doesn't need update");
+  console.log(`${projectDir} exists. pulling repo...`);
+  execSync(`git reset --hard origin/master`, { cwd: `${projectDir}/GenshinData` })
 } else {
-  console.log("./GenshinData doesn't exist, cloning repo...");
-  execSync(`cd ${projectDir} && git clone git@github.com:Dimbreath/GenshinData.git --depth 1`)
+  console.log(`${projectDir} doesn't exist, cloning repo...`);
+  execSync(`git clone git@github.com:Dimbreath/GenshinData.git --depth 1`, { cwd: projectDir })
 }
