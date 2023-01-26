44956e787c64afae36c799ddfe98945e757dc793
lantua <16190491+lantua@users.noreply.github.com>
2023-01-26 13:41:38 -0500

    Add support for string-valued subscripts

--- libs/waverider/src/node/formula.ts
+++ libs/waverider/src/node/formula.ts
@@ -1,6 +1,6 @@
-import type { Lookup, Match, Max, Min, Prod, Subscript, Sum, SumFrac, Threshold } from '../node'
+import type { Lookup, Match, Max, Min, Prod, Sum, SumFrac, Threshold } from '../node'
 
-type Arithmetics = Sum | Prod | Min | Max | SumFrac | Subscript
+type Arithmetics = Sum | Prod | Min | Max | SumFrac
 type Branching<Output> = Match<Output> | Threshold<Output> | Lookup<Output>
 
 export const arithmetic: Record<Arithmetics['op'], (x: number[], ex: any) => number> = {
@@ -9,7 +9,6 @@ export const arithmetic: Record<Arithmetics['op'], (x: number[], ex: any) => num
   'min': x => Math.min(...x),
   'max': x => Math.max(...x),
   'sumfrac': ([x, c]) => x! / (x! + c!),
-  'subscript': ([x], table) => table[x!],
 }
 export const selectBranch: Record<Branching<unknown>['op'], (br: any[], ex: any) => number> = {
   'match': ([v1, v2]) => v1 === v2 ? 0 : 1,
