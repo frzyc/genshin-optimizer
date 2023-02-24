4e77f0e93f77d2d44c7b1040ec9c81a111c80b34
frzyc <frzyc@users.noreply.github.com>
2023-02-24 12:13:35 -0500

    WIP for PWA

--- apps/frontend/src/main.tsx
+++ apps/frontend/src/main.tsx
@@ -8,6 +8,19 @@ import ReactGA from 'react-ga4';
 import App from './app/App';
 import './index.css';
 import NewTab from './app/NewTab';
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/genshin-optimizer-beta/service-worker.js',{scope: '/genshin-optimizer-beta/'}).then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
 ReactGA.initialize(process.env.NX_GA_TRACKINGID as any, {
   testMode: process.env.NODE_ENV === "development"
 });
---
+++ apps/frontend/workbox-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: '../../dist/apps/frontend',
+  globPatterns: ['**/*.{txt,ico,html,js,png,jpg,mp3}'],
+  swDest: '../../dist/apps/frontend/service-worker.js',
+  maximumFileSizeToCacheInBytes:999999999999999,
+};
