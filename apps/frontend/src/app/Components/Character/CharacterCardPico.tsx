36d54f92aa0ac3b0359065ac7ba51ffd61b92eea
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-02-25 10:46:55 -0700

    Add favorite indicator to CharacterCardPico

--- apps/frontend/src/app/Components/Character/CharacterCardPico.tsx
+++ apps/frontend/src/app/Components/Character/CharacterCardPico.tsx
@@ -1,5 +1,7 @@
 import { CharacterKey } from '@genshin-optimizer/consts';
 import { characterAsset } from '@genshin-optimizer/g-assets';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import { Box, CardActionArea, Skeleton, Typography } from '@mui/material';
 import { ReactNode, Suspense, useCallback } from 'react';
 import Assets from '../../Assets/Assets';
@@ -7,7 +9,9 @@ import { getCharSheet } from '../../Data/Characters';
 import { ascensionMaxLevel } from '../../Data/LevelData';
 import { ElementIcon } from '../../KeyMap/StatIcon';
 import useCharacter from '../../ReactHooks/useCharacter';
+import useCharMeta from '../../ReactHooks/useCharMeta';
 import useDBMeta from '../../ReactHooks/useDBMeta';
+import { ICachedCharacter } from '../../Types/character';
 import BootstrapTooltip from '../BootstrapTooltip';
 import CardDark from '../Card/CardDark';
 import ConditionalWrapper from '../ConditionalWrapper';
@@ -16,13 +20,29 @@ import CharacterCard from './CharacterCard';
 
 export default function CharacterCardPico({ characterKey = "", index = -1, onClick, simpleTooltip = false, }:
   { characterKey: CharacterKey | "", index?: number, onClick?: (characterKey: CharacterKey) => void, simpleTooltip?: boolean }) {
-  const { gender } = useDBMeta()
-  const characterSheet = characterKey ? getCharSheet(characterKey, gender) : undefined
   const character = useCharacter(characterKey)
+  if (character && characterKey) {
+    return <RealCharacterCardPico characterKey={characterKey} character={character} onClick={onClick} simpleTooltip={simpleTooltip} />
+  } else {
+    return <BlankCharacterCardPico index={index} />
+  }
+}
+
+type RealCharacterCardPicoProps = {
+  characterKey: CharacterKey
+  character: ICachedCharacter
+  onClick?: (characterKey: CharacterKey) => void
+  simpleTooltip: boolean
+}
+function RealCharacterCardPico({ characterKey, character, onClick, simpleTooltip }: RealCharacterCardPicoProps) {
+  const { favorite } = useCharMeta(characterKey)
+  const { gender } = useDBMeta()
+  const characterSheet = getCharSheet(characterKey, gender)
   const onClickHandler = useCallback(() => characterKey && onClick?.(characterKey), [characterKey, onClick])
-  const actionWrapperFunc = useCallback(
-    children => <CardActionArea onClick={onClickHandler}>{children}</CardActionArea>,
-    [onClickHandler])
+  const actionWrapperFunc = useCallback((children: ReactNode) =>
+    <CardActionArea onClick={onClickHandler}>{children}</CardActionArea>,
+    [onClickHandler]
+  )
 
   const simpleTooltipWrapperFunc = useCallback((children: ReactNode) =>
     <BootstrapTooltip placement="top" title={
@@ -32,7 +52,9 @@ export default function CharacterCardPico({ characterKey = "", index = -1, onCli
       }} />} {characterSheet.name}</Typography></Suspense>
     }>
       {children as JSX.Element}
-    </BootstrapTooltip>, [characterSheet])
+    </BootstrapTooltip>,
+    [characterSheet]
+  )
   const charCardTooltipWrapperFunc = useCallback((children: ReactNode) =>
     <BootstrapTooltip enterNextDelay={1000} enterTouchDelay={1000} placement="top" title={
       <Box sx={{ width: 300, m: -1 }}>
@@ -40,32 +62,38 @@ export default function CharacterCardPico({ characterKey = "", index = -1, onCli
       </Box>
     }>
       {children as JSX.Element}
-    </BootstrapTooltip>, [characterKey])
-  if (characterSheet && character && characterKey) {
-    return <ConditionalWrapper condition={simpleTooltip} wrapper={simpleTooltipWrapperFunc} falseWrapper={charCardTooltipWrapperFunc}>
-      <CardDark sx={{ maxWidth: 128, position: "relative", display: "flex", flexDirection: "column", }}>
-        <ConditionalWrapper condition={!!onClick} wrapper={actionWrapperFunc}>
-          <Box display="flex" className={`grad-${characterSheet.rarity}star`}>
-            {characterKey && <Box
-              component="img"
-              src={characterAsset(characterKey, "iconSide", gender)}
-              maxWidth="100%"
-              maxHeight="100%"
-              sx={{ transform: "scale(1.4)", transformOrigin: "bottom" }}
-            />}
-          </Box>
-          <Typography sx={{ position: "absolute", fontSize: "0.75rem", lineHeight: 1, opacity: 0.85, pointerEvents: "none", top: 0, }}>
-            <strong><SqBadge color="primary" >{character.level}/{ascensionMaxLevel[character.ascension]}</SqBadge></strong>
-          </Typography>
-          <Typography sx={{ position: "absolute", fontSize: "0.75rem", lineHeight: 1, opacity: 0.85, pointerEvents: "none", bottom: 0, right: 0, }}>
-            <strong><SqBadge color="secondary" >C{character.constellation}</SqBadge></strong>
-          </Typography>
-        </ConditionalWrapper>
-      </CardDark>
-    </ConditionalWrapper>
-  } else {
-    return <CardDark sx={{ display: "flex", alignItems: "center", justifyContent: "center", py: "12.5%" }}>
-      <Box component="img" src={Assets.team[`team${index + 2}`]} sx={{ width: "75%", height: "auto", opacity: 0.7 }} />
+    </BootstrapTooltip>,
+    [characterKey]
+  )
+
+  return <ConditionalWrapper condition={simpleTooltip} wrapper={simpleTooltipWrapperFunc} falseWrapper={charCardTooltipWrapperFunc}>
+    <CardDark sx={{ maxWidth: 128, position: "relative", display: "flex", flexDirection: "column", }}>
+      <ConditionalWrapper condition={!!onClick} wrapper={actionWrapperFunc}>
+        <Box display="flex" className={`grad-${characterSheet.rarity}star`}>
+          {characterKey && <Box
+            component="img"
+            src={characterAsset(characterKey, "iconSide", gender)}
+            maxWidth="100%"
+            maxHeight="100%"
+            sx={{ transform: "scale(1.4)", transformOrigin: "bottom" }}
+          />}
+        </Box>
+        <Typography sx={{ position: "absolute", fontSize: "0.75rem", lineHeight: 1, opacity: 0.85, pointerEvents: "none", top: 0, }}>
+          <strong><SqBadge color="primary" >{character.level}/{ascensionMaxLevel[character.ascension]}</SqBadge></strong>
+        </Typography>
+        <Box sx={{ position: "absolute", top: 0, right: 0 }}>
+          {favorite ? <FavoriteIcon fontSize="small" /> : <FavoriteBorderIcon fontSize="small" />}
+        </Box>
+        <Typography sx={{ position: "absolute", fontSize: "0.75rem", lineHeight: 1, opacity: 0.85, pointerEvents: "none", bottom: 0, right: 0, }}>
+          <strong><SqBadge color="secondary" >C{character.constellation}</SqBadge></strong>
+        </Typography>
+      </ConditionalWrapper>
     </CardDark>
-  }
+  </ConditionalWrapper>
+}
+
+function BlankCharacterCardPico({ index }: { index: number }) {
+  return <CardDark sx={{ display: "flex", alignItems: "center", justifyContent: "center", py: "12.5%" }}>
+    <Box component="img" src={Assets.team[`team${index + 2}`]} sx={{ width: "75%", height: "auto", opacity: 0.7 }} />
+  </CardDark>
 }
