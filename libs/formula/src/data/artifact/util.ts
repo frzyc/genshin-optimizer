65d6e01b516d8f116d5fe3aebc77c98020d5e612
lantua <16190491+lantua@users.noreply.github.com>
2023-03-01 11:42:39 -0500

    Add comments

--- libs/formula/src/data/artifact/util.ts
+++ libs/formula/src/data/artifact/util.ts
@@ -3,8 +3,16 @@ import { AnyNode, NumNode, RawTagMapEntries, ReRead, tag } from '@genshin-optimi
 import { Data, self } from "../util";
 
 export function registerArt(src: ArtifactSetKey, ...data: (Data | Data[number])[]): Data {
+  /* Unlike character and weapon, artifact buff is all-or-nothing, so we can register every
+   * buff as `src:art` and tag the formula as `src:src`. This means that `src:art`, which is
+   * read on each `et:agg`, does not need to reread `src:src`. This greatly reduce `Read`
+   * traffic due to the sheer numbers of `et:agg` calculations and `src:src` it would reaquire
+   * for each `src:art` read.
+   */
+
   function internal({ tag: oldTag, value }: Data[number]): Data[number] {
     if (value.op === 'reread' || value.op === 'tag' || value.op === 'read')
+      // This reuses `value` since it is already changing tags
       value = { ...value, tag: { ...value.tag, src } }
     else
       value = tag(value, { src })
