e0766d6a4dd25524311c3412d21cd0baf9e98a04
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-03-05 04:15:08 -0700

    Fix graph bugs

--- apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/ChartCard/CustomTooltip.tsx
+++ apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabOptimize/Components/ChartCard/CustomTooltip.tsx
@@ -1,3 +1,4 @@
+import { allArtifactSlotKeys } from "@genshin-optimizer/consts"
 import { Button, CardContent, ClickAwayListener, Grid, Skeleton, Stack, Typography } from "@mui/material"
 import { Suspense, useCallback, useContext, useMemo } from "react"
 import { Trans, useTranslation } from "react-i18next"
@@ -12,7 +13,6 @@ import { DatabaseContext } from "../../../../../../Database/Database"
 import { input } from "../../../../../../Formula"
 import { Unit, valueString } from "../../../../../../KeyMap"
 import { ICachedArtifact } from "../../../../../../Types/artifact"
-import { allSlotKeys } from "../../../../../../Types/consts"
 import { ArtifactSetBadges } from "../ArtifactSetBadges"
 import EnhancedPoint from "./EnhancedPoint"
 
@@ -46,50 +46,50 @@ export default function CustomTooltip({ xLabel, xUnit, yLabel, yUnit, selectedPo
     }
   }, [setSelectedPoint])
 
-  const currentlyEquipped = artifactsBySlot && allSlotKeys.every(slotKey => artifactsBySlot[slotKey]?.id === data.get(input.art[slotKey].id).value)
+  const currentlyEquipped = artifactsBySlot && allArtifactSlotKeys.every(slotKey => artifactsBySlot[slotKey]?.id === data.get(input.art[slotKey].id).value)
 
-  const highlightLabel = useMemo(() => selectedPoint?.buildNumber && (selectedPoint.highlightedType === "graph"
-    ? <Trans t={t} i18nKey="graphBuildLabel" count={selectedPoint?.buildNumber}>Graph #{{ count: selectedPoint?.buildNumber + 1 }}</Trans>
-    : `#${selectedPoint?.buildNumber}`),
+  const generLabel = useMemo(() => selectedPoint?.generBuildNumber !== undefined && `#${selectedPoint?.generBuildNumber}`, [selectedPoint])
+  const graphLabel = useMemo(() => selectedPoint?.graphBuildNumber !== undefined && <Trans t={t} i18nKey="graphBuildLabel" count={selectedPoint?.graphBuildNumber}>Graph #{{ count: selectedPoint?.graphBuildNumber + 1 }}</Trans>,
   [selectedPoint, t])
 
-  if (tooltipProps.active && selectedPoint) {
-    return <ClickAwayListener onClickAway={clickAwayHandler}>
-      <CardDark sx={{ minWidth: "400px", maxWidth: "400px" }} onClick={(e) => e.stopPropagation()}>
-        <CardContent>
-          <Stack gap={1}>
-            <Stack direction="row" alignItems="start" gap={1}>
-              <Stack spacing={0.5} flexGrow={99}>
-                {currentlyEquipped && <SqBadge color="info"><strong>{t("currentlyEquippedBuild")}</strong></SqBadge>}
-                {!currentlyEquipped && selectedPoint.highlighted && <SqBadge color="info">{highlightLabel}</SqBadge>}
-                <Suspense fallback={<Skeleton width={300} height={50} />}>
-                  <ArtifactSetBadges artifacts={Object.values(artifactsBySlot)} currentlyEquipped={currentlyEquipped} />
-                </Suspense>
-              </Stack>
-              <Grid item flexGrow={1} />
-              <CloseButton onClick={() => setSelectedPoint(undefined)} />
+if (tooltipProps.active && selectedPoint) {
+  return <ClickAwayListener onClickAway={clickAwayHandler}>
+    <CardDark sx={{ minWidth: "400px", maxWidth: "400px" }} onClick={(e) => e.stopPropagation()}>
+      <CardContent>
+        <Stack gap={1}>
+          <Stack direction="row" alignItems="start" gap={1}>
+            <Stack spacing={0.5} flexGrow={99}>
+              {currentlyEquipped && <SqBadge color="info"><strong>{t("currentlyEquippedBuild")}</strong></SqBadge>}
+              {generLabel && <SqBadge color="info">{generLabel}</SqBadge>}
+              {graphLabel && <SqBadge color="info">{graphLabel}</SqBadge>}
+              <Suspense fallback={<Skeleton width={300} height={50} />}>
+                <ArtifactSetBadges artifacts={Object.values(artifactsBySlot)} currentlyEquipped={currentlyEquipped} />
+              </Suspense>
             </Stack>
-            <Grid container direction="row" spacing={0.75} columns={5}>
-              {allSlotKeys.map(key =>
-                <Grid item key={key} xs={1}>
-                  <Suspense fallback={<Skeleton width={75} height={75} />}>
-                    <ArtifactCardPico artifactObj={artifactsBySlot[key]} slotKey={key} />
-                  </Suspense>
-                </Grid>
-              )}
-            </Grid>
-            <Typography><strong>{xLabel}</strong>: {valueString(xUnit === "%" ? selectedPoint.x / 100 : selectedPoint.x, xUnit)}</Typography>
-            <Typography><strong>{yLabel}</strong>: {valueString(yUnit === "%" ? selectedPoint.y / 100 : selectedPoint.y, yUnit)}</Typography>
-            <BootstrapTooltip title={selectedPoint.highlighted ? t("tcGraph.buildAlreadyInList") : ""} placement="top">
-              <span>
-                <Button sx={{ width: "100%" }} disabled={!!selectedPoint.highlighted} color="info" onClick={() => addBuildToList(selectedPoint.artifactIds)}>{t("addBuildToList")}</Button>
-              </span>
-            </BootstrapTooltip>
+            <Grid item flexGrow={1} />
+            <CloseButton onClick={() => setSelectedPoint(undefined)} />
           </Stack>
-        </CardContent>
-      </CardDark>
-    </ClickAwayListener>
-  }
+          <Grid container direction="row" spacing={0.75} columns={5}>
+            {allArtifactSlotKeys.map(key =>
+              <Grid item key={key} xs={1}>
+                <Suspense fallback={<Skeleton width={75} height={75} />}>
+                  <ArtifactCardPico artifactObj={artifactsBySlot[key]} slotKey={key} />
+                </Suspense>
+              </Grid>
+            )}
+          </Grid>
+          <Typography><strong>{xLabel}</strong>: {valueString(xUnit === "%" ? selectedPoint.x / 100 : selectedPoint.x, xUnit)}</Typography>
+          <Typography><strong>{yLabel}</strong>: {valueString(yUnit === "%" ? selectedPoint.y / 100 : selectedPoint.y, yUnit)}</Typography>
+          <BootstrapTooltip title={selectedPoint.highlighted ? t("tcGraph.buildAlreadyInList") : ""} placement="top">
+            <span>
+              <Button sx={{ width: "100%" }} disabled={selectedPoint?.graphBuildNumber !== undefined} color="info" onClick={() => addBuildToList(selectedPoint.artifactIds)}>{t("addBuildToList")}</Button>
+            </span>
+          </BootstrapTooltip>
+        </Stack>
+      </CardContent>
+    </CardDark>
+  </ClickAwayListener>
+}
 
-  return null
+return null
 }
