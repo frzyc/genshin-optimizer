cb498cdcf5ef7223fc9abd7a8f73d7f56e5a4a97
lantua <16190491+lantua@users.noreply.github.com>
2023-03-17 16:55:07 -0400

    Minor refactoring

--- libs/util/src/lib/queue.ts
+++ libs/util/src/lib/queue.ts
@@ -1,35 +1,30 @@
 export class MaxPrio<V> {
   // Fibonacci Heap
   list: (PrioNode<V> | undefined)[] = []
-  pivot = -1
+  maxNode: PrioNode<V> | undefined
 
-  insert(key: number, value: V) {
-    const oldMaxKey = this.list[this.pivot]?.key
-    const degree = this.addNode({ key, value, children: [] }, 0)
-    if (this.pivot < degree || oldMaxKey! < key) this.pivot = degree
+  insert(key: number, value: V): void {
+    let node: PrioNode<V> | undefined = { key, value, children: [] }
+    if (!this.maxNode || this.maxNode.key < key)
+      [this.maxNode, node] = [node, this.maxNode]
+    if (node) this.addNode(node, node.children.length)
   }
   pop(): V | undefined {
-    const max = this.popNode(this.pivot)
-    if (!max) return
-    const { value, children } = max
-
+    if (!this.maxNode) return
+    const { value, children } = this.maxNode
     children.forEach((child, i) => this.addNode(child, i))
-    if (!this.list.at(-1)) this.list.pop()
 
-    let maxKey = -Infinity
-    this.pivot = -1
-    this.list.forEach((node, i) => {
-      if (node && node.key >= maxKey) {
-        maxKey = node.key
-        this.pivot = i
-      }
-    })
-
+    const [_, maxDegree] = this.list.reduce(
+      (a, b, iB) => (!b || (a[0] && a[0].key > b.key) ? a : [b, iB]),
+      [undefined as PrioNode<V> | undefined, -1]
+    )
+    if (maxDegree === this.list.length - 1) this.maxNode = this.list.pop()
+    else this.maxNode = this.popNode(maxDegree)
     return value
   }
 
   /** Returns the degree of the tree that `node` resides in */
-  private addNode(node: PrioNode<V>, degree: number): number {
+  private addNode(node: PrioNode<V>, degree: number): void {
     let other: PrioNode<V> | undefined
     while ((other = this.popNode(degree))) {
       if (other.key > node.key) {
@@ -39,7 +34,6 @@ export class MaxPrio<V> {
       degree += 1
     }
     this.list[degree] = node
-    return degree
   }
   private popNode(degree: number): PrioNode<V> | undefined {
     const result = this.list[degree]
