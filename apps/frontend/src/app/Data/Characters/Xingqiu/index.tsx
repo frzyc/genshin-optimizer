c404607248b3a164af4d8e796709d478ddf65984
PolomoPT <polomothebrazilman@gmail.com>
2023-03-23 21:52:02 -0300

    type
    type
    `there's a place where i go to cry my troubles away`

--- apps/frontend/src/app/Data/Characters/Xingqiu/index.tsx
+++ apps/frontend/src/app/Data/Characters/Xingqiu/index.tsx
@@ -1,4 +1,4 @@
-import { CharacterData } from '@genshin-optimizer/pipeline'
+import type { CharacterData } from '@genshin-optimizer/pipeline'
 import { input } from '../../../Formula/index'
 import {
   compareEq,
@@ -14,11 +14,11 @@ import {
   sum,
 } from '../../../Formula/utils'
 import KeyMap from '../../../KeyMap'
-import { CharacterKey, ElementKey } from '@genshin-optimizer/consts'
+import type { CharacterKey, ElementKey } from '@genshin-optimizer/consts'
 import { cond, st, stg } from '../../SheetUtil'
 import CharacterSheet from '../CharacterSheet'
 import { charTemplates } from '../charTemplates'
-import { ICharacterSheet } from '../ICharacterSheet.d'
+import type { ICharacterSheet } from '../ICharacterSheet.d'
 import { customHealNode, dataObjForCharacterSheet, dmgNode } from '../dataUtil'
 import data_gen_src from './data_gen.json'
 import skillParam_gen from './skillParam_gen.json'
