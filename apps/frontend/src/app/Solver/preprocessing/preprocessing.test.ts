18fb95985dcdcb7245d47f6efce6e03183996fb7
tooflesswulf <tooflesswulf0325@gmail.com>
2023-04-12 02:00:04 -0400

    foldProd and tests

---
+++ apps/frontend/src/app/Solver/preprocessing/preprocessing.test.ts
@@ -0,0 +1,262 @@
+import { foldProd, reaffine2 } from './reaffine'
+import { constant, customRead, prod, sum } from '../../Formula/utils'
+import type { ArtifactsBySlot } from '../common'
+
+const exampleArts: ArtifactsBySlot = {
+  base: {
+    hp: 13471,
+    hp_: 0.496,
+    atk: 842,
+    def: 657,
+    enerRech_: 1,
+    hydroDmg_: 0.288,
+    critDMG_: 0.5,
+    critRate_: 0.05,
+    zc1: -1,
+    zc2: -10,
+    zc3: -0.44,
+    zc4: -1,
+  },
+  values: {
+    flower: [
+      {
+        id: 'f1',
+        set: 'OceanHuedClam',
+        values: {
+          hp: 4780,
+          eleMas: 44,
+          hp_: 0.058,
+          atk: 31,
+          enerRech_: 0.227,
+          OceanHuedClam: 1,
+          zc1: 1,
+        },
+      },
+      {
+        id: 'f2',
+        set: 'OceanHuedClam',
+        values: {
+          hp: 4780,
+          eleMas: 43,
+          hp_: 0.06,
+          atk: 32,
+          enerRech_: 0.207,
+          OceanHuedClam: 1,
+          zc2: 10,
+        },
+      },
+      {
+        id: 'f3',
+        set: 'HeartOfDepth',
+        values: {
+          hp: 717,
+          critDMG_: 0.054,
+          hp_: 0.047,
+          critRate_: 0.031,
+          def_: 0.066,
+          HeartOfDepth: 1,
+          zc3: 2,
+        },
+      },
+    ],
+    plume: [
+      {
+        id: 'p1',
+        set: 'OceanHuedClam',
+        values: {
+          atk: 311,
+          hp_: 0.163,
+          def: 37,
+          eleMas: 37,
+          hp: 568,
+          OceanHuedClam: 1,
+          zc4: -0.002,
+        },
+      },
+      {
+        id: 'p2',
+        set: 'OceanHuedClam',
+        values: {
+          atk: 311,
+          hp_: 0.173,
+          def: 35,
+          eleMas: 39,
+          hp: 508,
+          OceanHuedClam: 1,
+          zc4: -0.0005,
+        },
+      },
+      {
+        id: 'p3',
+        set: 'HeartOfDepth',
+        values: {
+          atk: 311,
+          critDMG_: 0.062,
+          enerRech_: 0.065,
+          eleMas: 40,
+          atk_: 0.262,
+          HeartOfDepth: 1,
+          zc4: 0.002,
+        },
+      },
+    ],
+    sands: [
+      {
+        id: 's1',
+        set: 'OceanHuedClam',
+        values: {
+          enerRech_: 0.518,
+          hp: 568,
+          atk_: 0.105,
+          eleMas: 56,
+          hp_: 0.105,
+          OceanHuedClam: 1,
+          zc2: 22,
+        },
+      },
+      {
+        id: 's2',
+        set: 'OceanHuedClam',
+        values: {
+          enerRech_: 0.518,
+          hp: 538,
+          atk_: 0.115,
+          eleMas: 52,
+          hp_: 0.115,
+          OceanHuedClam: 1,
+          zc2: 21.95,
+        },
+      },
+      {
+        id: 's3',
+        set: 'HeartOfDepth',
+        values: {
+          hp_: 0.466,
+          atk_: 0.058,
+          eleMas: 40,
+          atk: 45,
+          enerRech_: 0.155,
+          HeartOfDepth: 1,
+        },
+      },
+    ],
+    goblet: [
+      {
+        id: 'g1',
+        set: 'OceanHuedClam',
+        values: {
+          hydroDmg_: 0.466,
+          hp: 478,
+          def_: 0.204,
+          def: 23,
+          eleMas: 35,
+          OceanHuedClam: 1,
+        },
+      },
+      {
+        id: 'g2',
+        set: 'OceanHuedClam',
+        values: {
+          hydroDmg_: 0.466,
+          hp: 498,
+          def_: 0.194,
+          def: 28,
+          eleMas: 31,
+          OceanHuedClam: 1,
+        },
+      },
+      {
+        id: 'g3',
+        set: 'HeartOfDepth',
+        values: {
+          hydroDmg_: 0.466,
+          critDMG_: 0.124,
+          eleMas: 79,
+          def: 23,
+          hp_: 0.099,
+          HeartOfDepth: 1,
+        },
+      },
+    ],
+    circlet: [
+      {
+        id: 'c1',
+        set: 'OceanHuedClam',
+        values: {
+          heal_: 0.359,
+          critRate_: 0.058,
+          hp_: 0.198,
+          atk: 14,
+          eleMas: 19,
+        },
+      },
+      {
+        id: 'c2',
+        set: 'OceanHuedClam',
+        values: {
+          heal_: 0.359,
+          critRate_: 0.063,
+          hp_: 0.178,
+          atk: 15,
+          eleMas: 18,
+          zc1: 2,
+        },
+      },
+      {
+        id: 'c3',
+        set: 'HeartOfDepth',
+        values: {
+          critRate_: 0.311,
+          hp: 299,
+          critDMG_: 0.155,
+          atk_: 0.105,
+          eleMas: 56,
+          zc1: 2,
+        },
+      },
+    ],
+  },
+}
+
+const hp = sum(
+  customRead(['dyn', 'hp']),
+  prod(13471, customRead(['dyn', 'hp_']))
+)
+const atk = sum(
+  customRead(['dyn', 'atk']),
+  prod(842, customRead(['dyn', 'atk_']))
+)
+const def = sum(
+  customRead(['dyn', 'def']),
+  prod(657, customRead(['dyn', 'def_']))
+)
+const crcd = sum(
+  1,
+  prod(customRead(['dyn', 'critRate_']), customRead(['dyn', 'critDMG_']))
+)
+
+describe('test', () => {
+  test('foldProd', () => {
+    const x1 = customRead(['dyn', 'x1']),
+      x2 = customRead(['dyn', 'x2']),
+      x3 = customRead(['dyn', 'x3'])
+
+    expect(foldProd(x1, constant(1), constant(2), prod(x2, x3, 3))).toEqual(
+      prod(x1, x2, x3, 6)
+    )
+    expect(foldProd(constant(15), constant(1/3))).toEqual(constant(5))
+    expect(foldProd(x1, prod(8, 0.25, 0.5))).toEqual(x1)
+    expect(foldProd()).toEqual(constant(1))
+    expect(foldProd(prod(), prod(), prod(prod(prod())))).toEqual(constant(1))
+    expect(foldProd(prod(), prod(4), prod(1/4, prod(prod(x3))))).toEqual(x3)
+  })
+  test('prods', () => {
+    const n = [prod(1, prod(2, prod(3, prod(hp, prod(atk, 4)))))]
+    console.log('Initial:', n[0])
+    reaffine2(n, exampleArts)
+  })
+  test('test', () => {
+    const n = [prod(sum(prod(0.08, hp), atk), crcd)]
+    reaffine2(n, exampleArts)
+  })
+})
---
+++ apps/frontend/src/app/Solver/preprocessing/reaffine.ts
@@ -0,0 +1,120 @@
+import { ArtifactSetKey } from '@genshin-optimizer/consts'
+import type { OptNode } from '../../Formula/optimization'
+import { constant, customRead, prod, sum, threshold } from '../../Formula/utils'
+import type { ArtifactsBySlot } from '../common'
+import { mapFormulas } from '../../Formula/internal'
+
+/**
+ * Smart product between nodes, where constants and nestings are automatically merged.
+ *
+ * Runtime is recursive and potentially redundant. Only use with pre-processing.
+ *
+ * @example
+ * prod(x1, c1, c2, prod(x2, x3, c3))  ->  prod(x1, x2, x3, c1*c2*c3)
+ * prod(x1, 8, 0.25, 0.5)              ->  x1
+ * prod(prod(prod(x1)))                ->  x1
+ * prod(c1, c2)                        ->  constant(c1*c2)
+ * prod()                              ->  constant(1)
+ */
+export function foldProd(...nodes: readonly OptNode[]): OptNode {
+  if (nodes.length === 1) {
+    if (nodes[0].operation === 'mul') return foldProd(...nodes[0].operands)
+    return nodes[0]
+  }
+  nodes = nodes.flatMap((n) => {
+    if (n.operation !== 'mul') return n
+    n = foldProd(...n.operands)
+    return n.operation === 'mul' ? n.operands : n
+  })
+  const constVal = nodes.reduce(
+    (pv, n) => (n.operation === 'const' ? pv * n.value : pv),
+    1
+  )
+  nodes = nodes.filter((n) => n.operation !== 'const')
+
+  if (nodes.length === 0) return constant(constVal)
+  if (constVal === 1 && nodes.length === 1) return nodes[0]
+  if (constVal === 1) return prod(...nodes)
+  return prod(...nodes, constant(constVal))
+}
+
+function canDistribute({ operation, operands }: OptNode): boolean {
+  return (
+    operation === 'const' ||
+    operation === 'read' ||
+    operation === 'threshold' ||
+    (operation === 'add' && operands.every((n) => canDistribute(n)))
+  )
+}
+
+export function reaffine2(nodes: OptNode[], arts: ArtifactsBySlot) {
+  const allKeys = Object.keys(arts.base)
+  const addedRegisters = {} as {
+    [key: string]: { base: number; coeff: number; sumKeys: string[] }
+  }
+
+  function distributeConst(n: OptNode, v: number): OptNode {
+    if (v === 1) return n
+    switch (n.operation) {
+      case 'threshold': {
+        const [branch, bval, ge, lt] = n.operands
+        return threshold(
+          branch,
+          bval,
+          distributeConst(ge, v),
+          distributeConst(lt, v)
+        )
+      }
+      case 'const':
+        return constant(n.value * v)
+      case 'add':
+        return sum(...n.operands.map((ni) => distributeConst(ni, v)))
+      case 'read': {
+        const newID = n.path[1] + '*'
+        addedRegisters[newID] = { base: 0, coeff: v, sumKeys: [n.path[1]] }
+        return customRead(['dyn', newID])
+      }
+      case 'mul': {
+        const distrInto = n.operands.findIndex((n) => canDistribute(n))
+        if (distrInto >= 0) {
+          const newOps = [...n.operands]
+          newOps[distrInto] = distributeConst(n.operands[distrInto], v)
+          return foldProd(...newOps)
+        }
+        return foldProd(n, constant(v))
+      }
+      default:
+        return prod(n, constant(v))
+    }
+  }
+
+  console.log('Original:', JSON.stringify(nodes[0]))
+  console.log('This should be bottom-up')
+  const hmm = mapFormulas(
+    nodes,
+    (_) => _,
+    (f) => {
+      switch (f.operation) {
+        case 'mul': {
+          if (f.operands.every((ni) => ni.operation !== 'const')) return f
+          const coeff = f.operands.reduce(
+            (coeff, ni) =>
+              ni.operation === 'const' ? coeff * ni.value : coeff,
+            1
+          )
+          const todistrib = f.operands
+            .filter((n) => n.operation !== 'const')
+            .map((n) => distributeConst(n, coeff))
+          if (todistrib.length === 1) return todistrib[0]
+          return prod(...todistrib)
+        }
+        case 'add': {
+          return f
+        }
+      }
+      return f
+    }
+  )
+
+  console.log(JSON.stringify(hmm[0]))
+}
