f496f78635085a3e8dfe104afd3a83278b4f074f
tooflesswulf <tooflesswulf0325@gmail.com>
2023-04-24 01:35:16 -0400

    bad files

--- apps/frontend/src/app/Solver/GOSolver/BNBSplitWorker/debug.test.ts
+++
@@ -1,9 +0,0 @@
-import { formula, arts } from './debugdata'
-import { polyUB } from './polyUB'
-
-test('debug', () => {
-  const n = [formula]
-  const poly = polyUB(n, arts)
-
-  console.log(poly[0])
-})
