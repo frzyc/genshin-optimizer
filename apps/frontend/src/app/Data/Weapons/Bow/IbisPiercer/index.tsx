b3eb0ab37149b54d4c95985f7db1da149d9cfb7c
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-05-23 11:19:31 -0700

    Fix format

--- apps/frontend/src/app/Data/Weapons/Bow/IbisPiercer/index.tsx
+++ apps/frontend/src/app/Data/Weapons/Bow/IbisPiercer/index.tsx
@@ -1,12 +1,12 @@
-import { input } from '../../../../Formula'
-import { lookup, naught, prod, subscript } from '../../../../Formula/utils'
 import type { WeaponKey } from '@genshin-optimizer/consts'
 import { allStats } from '@genshin-optimizer/gi-stats'
+import { input } from '../../../../Formula'
+import { lookup, naught, prod, subscript } from '../../../../Formula/utils'
 import { objectKeyMap, range } from '../../../../Util/Util'
-import { cond, stg, st } from '../../../SheetUtil'
-import { dataObjForWeaponSheet } from '../../util'
+import { cond, st, stg } from '../../../SheetUtil'
 import type { IWeaponSheet } from '../../IWeaponSheet'
 import WeaponSheet, { headerTemplate } from '../../WeaponSheet'
+import { dataObjForWeaponSheet } from '../../util'
 
 const key: WeaponKey = 'IbisPiercer'
 const data_gen = allStats.weapon.data[key]
@@ -18,10 +18,7 @@ const [condPassiveStacksPath, condPassiveStacks] = cond(key, 'passiveStacks')
 const eleMas = lookup(
   condPassiveStacks,
   objectKeyMap(stacksArr, (stack) =>
-    prod(
-      subscript(input.weapon.refineIndex, emArr),
-      stack
-    )
+    prod(subscript(input.weapon.refineIndex, emArr), stack)
   ),
   naught
 )
