de087ccc0f8c9394ef5de14b84d8f81a68b2594c
frzyc <frzyc@users.noreply.github.com>
2023-06-13 01:13:46 -0400

    remove formula

--- libs/formula/src/data/artifact/index.ts
+++
@@ -1,5 +0,0 @@
-import type { Data } from '../util'
-import NoblesseOblige from './NoblesseOblige'
-
-const data: Data[] = [NoblesseOblige]
-export default data.flat()
--- libs/formula/src/data/char/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Data } from '../util'
-import Candace from './Candace'
-import Nahida from './Nahida'
-import Nilou from './Nilou'
-
-const data: Data[] = [Candace, Nahida, Nilou]
-export default data.flat()
