054f5cacae6a233a9014f496598d37efe3ca1f75
PolomoPT <polomothebrazilman@gmail.com>
2023-06-16 19:23:34 -0300

    Nahida

--- apps/frontend/src/app/Data/Characters/Nahida/index.tsx
+++ apps/frontend/src/app/Data/Characters/Nahida/index.tsx
@@ -523,7 +523,6 @@ const sheet: ICharacterSheet = {
         teamBuff: true,
         path: condC2QSAPath,
         value: condC2QSA,
-        canShow: equal(condC2Bloom, 'on', 1),
         name: ct.ch('c2.qasCondName'),
         states: {
           on: {
