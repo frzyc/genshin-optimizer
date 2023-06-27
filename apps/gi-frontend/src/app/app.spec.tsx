f11c49440f7f824b6fa21d13bd15d6909b309ee3
frzyc <frzyc@users.noreply.github.com>
2023-06-27 01:03:30 -0400

    add new vite gi-frontend

--- apps/gi-frontend/src/app/app.spec.tsx
+++ apps/gi-frontend/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />)
-    expect(getByText(/Genshin Optimizer/gi)).toBeTruthy()
+    expect(getByText(/Welcome gi-frontend/gi)).toBeTruthy()
   })
 })
