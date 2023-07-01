52b5fd744619bbb7ab22a53d4e29a1a723937661
lantua <16190491+lantua@users.noreply.github.com>
2023-07-01 00:50:16 -0400

    Rename `waverider` => `pando`

--- libs/pando/jest.config.ts
+++ libs/pando/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'waverider',
+  displayName: 'pando',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/libs/waverider',
+  coverageDirectory: '../../coverage/libs/pando',
 }
