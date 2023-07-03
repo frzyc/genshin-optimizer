f41471f09d09800a093de6f11948e39b98b23d22
eeeqeee <103794572+eeeqeee@users.noreply.github.com>
2023-07-03 02:13:53 -0400

    drop jest imo

--- libs/pando/jest.config.ts
+++ libs/pando/jest.config.ts
@@ -1,15 +1,21 @@
 /* eslint-disable */
 export default {
+  extensionsToTreatAsEsm: ['.ts'],
   displayName: 'pando',
-  preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
+      useESM: true,
     },
   },
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
+    '^.+\\.[tj]s$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/libs/pando',
