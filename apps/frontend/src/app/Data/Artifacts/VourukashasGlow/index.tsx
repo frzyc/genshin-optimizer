ca9f7150876a09e606bc164efbbfac083ea22115
PolomoPT <polomothebrazilman@gmail.com>
2023-07-06 02:43:13 -0300

    vourukasha's glow (WIP)

--- apps/frontend/src/app/Data/Artifacts/VourukashasGlow/index.tsx
+++ apps/frontend/src/app/Data/Artifacts/VourukashasGlow/index.tsx
@@ -9,6 +9,7 @@ import {
   sum,
 } from '../../../Formula/utils'
 import { objKeyMap, range } from '@genshin-optimizer/util'
+import KeyMap from '../../../KeyMap'
 import { cond, st } from '../../SheetUtil'
 import { ArtifactSheet, setHeaderTemplate } from '../ArtifactSheet'
 import { dataObjForArtifactSheet } from '../dataUtil'
@@ -19,27 +20,47 @@ const setHeader = setHeaderTemplate(key)
 
 const set2 = greaterEq(input.artSet.VourukashasGlow, 2, 0.2)
 
-const [condSet4Path, condSet4] = cond(key, 'set4')
-const stacksArr = range(1, 5)
 const set4_skill_dmg_ = greaterEq(
   input.artSet.VourukashasGlow,
   4,
-  sum(
-    0.1,
-    lookup(
-      condSet4,
-      objKeyMap(stacksArr, (stack) => constant(stack * 0.08)),
-      naught
-    )
-  )
+  0.1,
+  KeyMap.info('skill_dmg_')
 )
-const set4_burst_dmg_ = { ...set4_skill_dmg_ }
+const set4_burst_dmg_ = greaterEq(
+  input.artSet.VourukashasGlow,
+  4,
+  0.1,
+  KeyMap.info('burst_dmg_')
+)
+
+const [condSet4Path, condSet4] = cond(key, 'set4')
+const stacksArr = range(1, 5)
+const set4_stacks_skill_dmg_ = greaterEq(
+  input.artSet.VourukashasGlow,
+  4,
+  lookup(
+    condSet4,
+    objKeyMap(stacksArr, (stack) => constant(stack * 0.08)),
+    naught
+  ),
+  KeyMap.info('skill_dmg_')
+)
+const set4_stacks_burst_dmg_ = greaterEq(
+  input.artSet.VourukashasGlow,
+  4,
+  lookup(
+    condSet4,
+    objKeyMap(stacksArr, (stack) => constant(stack * 0.08)),
+    naught
+  ),
+  KeyMap.info('burst_dmg_')
+)
 
 export const data: Data = dataObjForArtifactSheet(key, {
   premod: {
     hp_: set2,
-    skill_dmg_: set4_skill_dmg_,
-    burst_dmg_: set4_burst_dmg_,
+    skill_dmg_: sum(set4_skill_dmg_, set4_stacks_skill_dmg_),
+    burst_dmg_: sum(set4_burst_dmg_, set4_stacks_burst_dmg_),
   },
 })
 
@@ -52,6 +73,17 @@ const sheet: IArtifactSheet = {
       document: [
         {
           header: setHeader(4),
+          fields: [
+            {
+              node: set4_skill_dmg_,
+            },
+            {
+              node: set4_burst_dmg_,
+            },
+          ],
+        },
+        {
+          header: setHeader(4),
           path: condSet4Path,
           value: condSet4,
           name: st('takeDmg'),
@@ -59,10 +91,10 @@ const sheet: IArtifactSheet = {
             name: st('stack', { count: stack }),
             fields: [
               {
-                node: set4_skill_dmg_,
+                node: set4_stacks_skill_dmg_,
               },
               {
-                node: set4_burst_dmg_,
+                node: set4_stacks_burst_dmg_,
               },
             ],
           })),
