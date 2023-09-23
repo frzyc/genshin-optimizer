a172811f1f036932e0c1e2e7d149fa09050258bd
tooflesswulf <tooflesswulf0325@gmail.com>
2023-09-22 22:40:07 -0400

    Address comments in mvncdf

--- apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabUpgradeOpt/mvncdf.ts
+++ apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabUpgradeOpt/mvncdf.ts
@@ -7,35 +7,34 @@ import { erf, gaussPDF } from './mathUtil'
 
 /** From a 1D Gaussian mean & variance, get P(x > mu) and E[x | x > mu] */
 export function gaussianPE(
-  mean: number,
+  mu: number,
   variance: number,
   x: number
 ): { p: number; upAvg: number } {
   if (variance < 1e-5) {
-    if (mean > x) return { p: 1, upAvg: mean - x }
+    if (mu > x) return { p: 1, upAvg: mu - x }
     return { p: 0, upAvg: 0 }
   }
 
-  const z = (x - mean) / Math.sqrt(variance)
+  const z = (x - mu) / Math.sqrt(variance)
   const p = (1 - erf(z / Math.sqrt(2))) / 2
   const phi = gaussPDF(z)
 
   const y2 = 1 / (z * z)
 
   // When z is small, p and phi are both nonzero so (phi/p - z) is ok.
-  // When p and phi are both small, we can take use the Taylor expansion
-  //  of (phi/p - z) at z=inf (or if y=1/z, at y=0). Using 7th order expansion to ensure
-  //  upAvg is continuous at z=5.
+  // When p and phi are both small, we can take the Taylor expansion
+  //  of (phi/p - z) at z=inf (or if y=1/z, at y=0).
+  // Using 7th order expansion to ensure upAvg is continuous at z=5.
   const ppz = z < 5 ? phi / p - z : (1 - 2 * y2 * (1 - y2 * (5 + 37 * y2))) / z
   return { p, upAvg: Math.sqrt(variance) * ppz }
 }
 
 /**
  * From a multivariate Gaussian mean & covariance, get P(x > mu) and E[x0 | x > mu]
- * Javascript implementation ignores the cov off-diagonals (assumes all components are independent).
+ * Note: this implementation ignores the cov off-diagonals (assumes all components are independent).
  */
 export function mvnPE_bad(mu: number[], cov: number[][], x: number[]) {
-  // TODO: an implementation without using the independence assumption
   let ptot = 1
   let cptot = 1
   for (let i = 0; i < mu.length; ++i) {
