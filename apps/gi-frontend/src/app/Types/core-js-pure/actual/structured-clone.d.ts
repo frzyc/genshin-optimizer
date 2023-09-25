4408f369066916f77655c2c4f8403ac3e72fa157
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-09-24 19:39:56 -0700

    Add structured-clone.d.ts

---
+++ apps/gi-frontend/src/app/Types/core-js-pure/actual/structured-clone.d.ts
@@ -0,0 +1,9 @@
+import 'core-js-pure/actual/structured-clone'
+
+declare module 'core-js-pure/actual/structured-clone'
+// declare module 'core-js-pure/actal/structuredClone' {
+//   export default function structuredClone<T = any>(
+//     value: T,
+//     options?: StructuredSerializeOptions
+//   ): T
+// }
