5c756fec211fd3fe31ad759129c51cad9034b189
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-09-25 13:03:45 -0700

    Delete apps/gi-frontend/src/app/Types/core-js-pure/actual/structured-clone.d.ts

--- apps/gi-frontend/src/app/Types/core-js-pure/actual/structured-clone.d.ts
+++
@@ -1,9 +0,0 @@
-import 'core-js-pure/actual/structured-clone'
-
-declare module 'core-js-pure/actual/structured-clone'
-// declare module 'core-js-pure/actal/structuredClone' {
-//   export default function structuredClone<T = any>(
-//     value: T,
-//     options?: StructuredSerializeOptions
-//   ): T
-// }
