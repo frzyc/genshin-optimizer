aa2c890810c6a3bc5538bc575ef6fffebdf8b772
frzyc <frzyc@users.noreply.github.com>
2023-10-10 00:49:00 -0400

    migrate to nx 16.10.0 (#1297)

--- apps/frontend/webpack.config.js
+++ apps/frontend/webpack.config.js
@@ -1,6 +1,5 @@
 const { composePlugins, withNx } = require('@nx/webpack')
 const { withReact } = require('@nx/react')
-const TerserJSPlugin = require('terser-webpack-plugin')
 
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
@@ -10,17 +9,6 @@ module.exports = composePlugins(withNx(), withReact(), (config) => {
   // https://github.com/nrwl/nx/issues/10017#issuecomment-1405429851
   // Fix to web workers not working
   config.output.scriptType = 'text/javascript'
-
-  // https://github.com/nrwl/nx/issues/14483#issuecomment-1424029390
-
-  // Fix 1: resolves tree-shaking issue caused by using custom webpack config
-  // The default is [ 'browser', 'main', 'module' ]. Thus, 'main' had preference over 'module' when Webpack reads the `package.json` files, which is not what we want. Module should become before main - the order matters!
-  // See https://webpack.js.org/configuration/resolve/#resolvemainfields
-  config.resolve.mainFields = ['browser', 'module', 'main']
-
-  // Fix 2: resolves minification issue by adding Terser. Terser is also capable of eliminating dead code.
-  // TerserJS is the Webpack 5 default minifier but we have to specify it explicitly as soon as we include more minifiers
-  config.optimization.minimizer.unshift(new TerserJSPlugin())
 
   return config
 })
