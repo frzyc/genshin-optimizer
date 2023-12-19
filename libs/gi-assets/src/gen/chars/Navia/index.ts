29c173bea4a1d5babc8a501b41d656a16b33d171
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-12-18 23:13:14 -0700

    Add 4.3 first half content

---
+++ libs/gi-assets/src/gen/chars/Navia/index.ts
@@ -0,0 +1,35 @@
+// This is a generated index file.
+import banner from './UI_NameCardPic_Navia_P.png'
+import bar from './UI_NameCardPic_Navia_Alpha.png'
+import burst from './Skill_E_Navia_01_HD.png'
+import constellation1 from './UI_Talent_S_Navia_01.png'
+import constellation2 from './UI_Talent_S_Navia_02.png'
+import constellation3 from './UI_Talent_U_Navia_01.png'
+import constellation4 from './UI_Talent_S_Navia_03.png'
+import constellation5 from './UI_Talent_U_Navia_02.png'
+import constellation6 from './UI_Talent_S_Navia_04.png'
+import icon from './UI_AvatarIcon_Navia.png'
+import iconSide from './UI_AvatarIcon_Side_Navia.png'
+import passive1 from './UI_Talent_S_Navia_05.png'
+import passive2 from './UI_Talent_S_Navia_06.png'
+import passive3 from './UI_Talent_S_Navia_07.png'
+import skill from './Skill_S_Navia_01.png'
+
+const data = {
+  banner,
+  bar,
+  burst,
+  constellation1,
+  constellation2,
+  constellation3,
+  constellation4,
+  constellation5,
+  constellation6,
+  icon,
+  iconSide,
+  passive1,
+  passive2,
+  passive3,
+  skill,
+} as const
+export default data
---
+++ libs/gi-assets/src/gen/weapons/UltimateOverlordsMegaMagicSword/index.ts
@@ -0,0 +1,9 @@
+// This is a generated index file.
+import awakenIcon from './UI_EquipIcon_Claymore_Champion_Awaken.png'
+import icon from './UI_EquipIcon_Claymore_Champion.png'
+
+const data = {
+  awakenIcon,
+  icon,
+} as const
+export default data
