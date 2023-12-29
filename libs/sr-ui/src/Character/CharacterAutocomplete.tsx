feacea4c00413489cd8c114c4c9a5d169eb156e5
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-12-29 11:49:53 -0700

    Fix lint

--- libs/sr-ui/src/Character/CharacterAutocomplete.tsx
+++ libs/sr-ui/src/Character/CharacterAutocomplete.tsx
@@ -1,12 +1,12 @@
 import { useForceUpdate } from '@genshin-optimizer/react-util'
 import type { CharacterKey } from '@genshin-optimizer/sr-consts'
 import { allCharacterKeys } from '@genshin-optimizer/sr-consts'
-import { DatabaseContext } from '@genshin-optimizer/sr-ui'
 import type { GeneralAutocompleteOption } from '@genshin-optimizer/ui-common'
 import { GeneralAutocomplete } from '@genshin-optimizer/ui-common'
 import { objKeyMap } from '@genshin-optimizer/util'
 import { Skeleton } from '@mui/material'
 import { Suspense, useContext, useEffect, useMemo } from 'react'
+import { DatabaseContext } from '../Context'
 
 type CharacterAutocompleteProps = {
   charKey: CharacterKey | ''
