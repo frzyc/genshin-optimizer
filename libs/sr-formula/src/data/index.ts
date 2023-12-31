74fe6896bbf32824b129c8ce2a8266a68cdfbf17
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-12-31 11:02:10 -0700

    Update stat calculation

--- libs/sr-formula/src/data/index.ts
+++ libs/sr-formula/src/data/index.ts
@@ -1,11 +1,17 @@
 import {
   compileTagMapKeys,
   compileTagMapValues,
+  max,
+  min,
+  prod,
 } from '@genshin-optimizer/pando'
 import {
   fixedTags,
+  percent,
   queryTypes,
   reader,
+  self,
+  selfBuff,
   usedNames,
   usedQ,
   type TagMapNodeEntries,
@@ -21,6 +27,24 @@ const data: TagMapNodeEntries = [
   reader.src('agg').add(reader.sum.src('char')),
   // convert src:lightCone to src:total for accumulation
   reader.src('agg').add(reader.sum.src('lightCone')),
+
+  // Final <= Premod <= Base
+  reader
+    .withTag({ src: 'agg', et: 'self', qt: 'final' })
+    .add(reader.with('qt', 'premod').sum),
+  reader
+    .withTag({ src: 'agg', et: 'self', qt: 'premod' })
+    .add(reader.with('qt', 'base').sum),
+
+  // premod X += base X * premod X%
+  ...(['atk', 'def', 'hp', 'spd'] as const).map((s) =>
+    selfBuff.premod[s].add(prod(self.base[s], self.premod[`${s}_`]))
+  ),
+
+  // Capped CR = Max(Min(Final CR, 1), 0)
+  selfBuff.common.cappedCrit_.add(
+    max(min(self.final.crit_, percent(1)), percent(0))
+  ),
 ]
 export const keys = compileTagMapKeys([
   { category: 'qt', values: queryTypes },
