0546d10ba9bc45ec8351f4e6d679e3c125d1438a
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2023-12-31 22:22:28 -0700

    Add base stats to util

--- libs/sr-formula/src/data/char/util.ts
+++ libs/sr-formula/src/data/char/util.ts
@@ -1,4 +1,11 @@
-import { cmpEq, cmpGE } from '@genshin-optimizer/pando'
+import {
+  cmpEq,
+  cmpGE,
+  constant,
+  prod,
+  subscript,
+  sum,
+} from '@genshin-optimizer/pando'
 import type { StatBoostKey, TypeKey } from '@genshin-optimizer/sr-consts'
 import type {
   CharacterDataGen,
@@ -21,11 +28,34 @@ export function traceParams(data_gen: CharacterDataGen) {
 
 export function entriesForChar(data_gen: CharacterDataGen): TagMapNodeEntries {
   const { final: _final, char } = self
-  const { eidolon } = char
+  const { eidolon, ascension } = char
+  // The "add" only applies to currLvl - 1, since "base" is stat at lvl 1
+  const readLvl = sum(constant(-1), self.char.lvl)
   const statBoosts = data_gen.skillTreeList
     .map((s) => s.levels?.[0]?.stats)
     .filter((s): s is SkillTreeNodeBonusStat => !!s)
   return [
+    // Base stats
+    ...(['hp', 'atk', 'def'] as const).map((sk) => {
+      const basePerAsc = data_gen.ascension.map((p) => p[sk].base)
+      const addPerAsc = data_gen.ascension.map((p) => p[sk].add)
+      return selfBuff.base[sk].add(
+        sum(
+          subscript(ascension, basePerAsc),
+          prod(readLvl, subscript(ascension, addPerAsc))
+        )
+      )
+    }),
+    ...(['crit_', 'crit_dmg_', 'spd'] as const).map((sk) => {
+      const statAsc = data_gen.ascension.map((p) => p[sk])
+      switch (sk) {
+        case 'crit_':
+        case 'crit_dmg_':
+          return selfBuff.premod[sk].add(subscript(ascension, statAsc))
+        case 'spd':
+          return selfBuff.base[sk].add(subscript(ascension, statAsc))
+      }
+    }),
     // Small trace stat boosts
     ...statBoosts.flatMap((statBoost) =>
       Object.entries(statBoost).map(([key, amt], index) => {
