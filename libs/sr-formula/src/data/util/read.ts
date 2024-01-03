c9a87ac78dc4d3cb7e22681eae53f23bbe36a158
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-01-03 14:03:04 -0700

    Add debug calc for sr-formula (#1408)
    * Add debug calc
    
    * Fix lint
    
    * Fix lint
    
    * Fix lint
    
    * Update debug to match gi-formula
    
    * Add toDebug function

--- libs/sr-formula/src/data/util/read.ts
+++ libs/sr-formula/src/data/util/read.ts
@@ -108,6 +108,42 @@ export function tagVal(cat: keyof Tag): TagValRead {
   return baseTagVal(cat)
 }
 
+export function tagStr(tag: Tag, ex?: any): string {
+  const { name, member, dst, et, src, q, qt, dt, move, ...remaining } = tag
+
+  if (Object.keys(remaining).length) console.error(remaining)
+
+  let result = '{ ',
+    includedRequired = false,
+    includedBar = false
+  function required(str: string | undefined | null) {
+    if (!str) return
+    result += str + ' '
+    includedRequired = true
+  }
+  function optional(str: string | undefined | null) {
+    if (!str) return
+    if (includedRequired && !includedBar) {
+      includedBar = true
+      result += '| '
+    }
+    result += str + ' '
+  }
+  required(name && `#${name}`)
+  required(member)
+  required(dst && `(${dst})`)
+  required(src)
+  required(et)
+  if (qt && q) required(`${qt}.${q}`)
+  else if (qt) required(`${qt}.`)
+  else if (q) required(`.${q}`)
+
+  optional(dt)
+  optional(move)
+  required(ex && `[${ex}]`)
+  return result + '}'
+}
+
 export const reader = new Read({}, undefined)
 export const usedNames = new Set<string>()
 export const usedQ = new Set('_')
