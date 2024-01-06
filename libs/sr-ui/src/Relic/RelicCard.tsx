04746e5d42b766707d06bf7507b176f5181cca79
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-01-05 20:05:57 -0700

    Remove undefined

--- libs/sr-ui/src/Relic/RelicCard.tsx
+++ libs/sr-ui/src/Relic/RelicCard.tsx
@@ -2,28 +2,24 @@ import type { IRelic } from '@genshin-optimizer/sr-srod'
 import { getRelicMainStatDisplayVal } from '@genshin-optimizer/sr-util'
 import { Box, Stack, Typography } from '@mui/material'
 
-export function RelicCard({ relic }: { relic: IRelic | undefined }) {
+export function RelicCard({ relic }: { relic: IRelic }) {
   return (
     <Stack>
-      {!relic ? (
-        <Typography>Empty</Typography>
-      ) : (
-        <Box>
-          <Typography>
-            Main: {relic.mainStatKey} -{' '}
-            {getRelicMainStatDisplayVal(
-              relic.rarity,
-              relic.mainStatKey,
-              relic.level
-            )}
+      <Box>
+        <Typography>
+          Main: {relic.mainStatKey} -{' '}
+          {getRelicMainStatDisplayVal(
+            relic.rarity,
+            relic.mainStatKey,
+            relic.level
+          )}
+        </Typography>
+        {relic.substats.map((substat) => (
+          <Typography key={substat.key}>
+            Sub: {substat.key} - {substat.value}
           </Typography>
-          {relic.substats.map((substat) => (
-            <Typography key={substat.key}>
-              Sub: {substat.key} - {substat.value}
-            </Typography>
-          ))}
-        </Box>
-      )}
+        ))}
+      </Box>
     </Stack>
   )
 }
