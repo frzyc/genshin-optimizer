b6394a9764f83d8098b6dfa5d57174f25a64dd61
eeeqeee <103794572+eeeqeee@users.noreply.github.com>
2024-01-21 21:15:38 -0500

    trans for AddArtInfo

--- apps/frontend/src/app/Components/AddArtInfo.tsx
+++ apps/frontend/src/app/Components/AddArtInfo.tsx
@@ -1,14 +1,20 @@
 import { Alert, Link } from '@mui/material'
+import { Trans, useTranslation } from 'react-i18next'
 import { Link as RouterLink } from 'react-router-dom'
 
 export default function AddArtInfo() {
+  const { t } = useTranslation('artifact')
   return (
-    <Alert severity="info" variant="filled">
-      Looks like you haven't added any artifacts yet. If you want, there are
-      <Link color="warning.main" component={RouterLink} to="/scanner">
-        automatic scanners
-      </Link>
-      that can speed up the import process!
-    </Alert>
+    <Trans t={t} i18nKey={"noArtifacts"}>
+      <Alert severity="info" variant="filled">
+        {
+          "Looks like you haven't added any artifacts yet. If you want, there are "
+        }
+        <Link color="warning.main" component={RouterLink} to="/scanner">
+          automatic scanners
+        </Link>
+        {' that can speed up the import process!'}
+      </Alert>
+    </Trans>
   )
 }
