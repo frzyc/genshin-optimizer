9e13419d287c9f81bf33012a9b97b4b76f362665
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-01-23 11:42:16 -0700

    Fix relic sheet export

--- libs/sr-formula/src/data/relic/index.ts
+++ libs/sr-formula/src/data/relic/index.ts
@@ -1,4 +1,15 @@
+import { allRelicSetKeys } from '@genshin-optimizer/sr-consts'
+import { allStats } from '@genshin-optimizer/sr-stats'
 import type { TagMapNodeEntries } from '../util'
+import PrisonerInDeepConfinement from './PrisonerInDeepConfinement'
+import { entriesForRelic } from './util'
 
-const data: TagMapNodeEntries = []
+const data: TagMapNodeEntries = allRelicSetKeys.flatMap((key) => {
+  switch (key) {
+    case 'PrisonerInDeepConfinement':
+      return PrisonerInDeepConfinement
+    default:
+      return entriesForRelic(key, allStats.relic[key])
+  }
+})
 export default data
