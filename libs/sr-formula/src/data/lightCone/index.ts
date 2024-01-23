cfd6f0283ac07fc946ef397420a8139ba1bfa39f
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-01-23 09:26:26 -0700

    Fix data export

--- libs/sr-formula/src/data/lightCone/index.ts
+++ libs/sr-formula/src/data/lightCone/index.ts
@@ -2,6 +2,7 @@ import type { LightConeKey } from '@genshin-optimizer/sr-consts'
 import { allLightConeKeys } from '@genshin-optimizer/sr-consts'
 import { allStats } from '@genshin-optimizer/sr-stats'
 import { register, type TagMapNodeEntries } from '../util'
+import TheSeriousnessOfBreakfast from './TheSeriousnessOfBreakfast'
 import { entriesForLightCone } from './util'
 
 // Attach the base stats from the generated datamine
@@ -9,5 +10,9 @@ export function lightConeBaseStats(src: LightConeKey): TagMapNodeEntries {
   const dataGen = allStats.lightCone[src]
   return register(src, entriesForLightCone(dataGen))
 }
-const data: TagMapNodeEntries = allLightConeKeys.flatMap(lightConeBaseStats)
+const data: TagMapNodeEntries = allLightConeKeys.flatMap((key) =>
+  key === 'TheSeriousnessOfBreakfast'
+    ? TheSeriousnessOfBreakfast
+    : lightConeBaseStats(key)
+)
 export default data
