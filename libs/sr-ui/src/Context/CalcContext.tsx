538b59ae7ebff2647401879fd79414a119f356a9
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-01-25 20:38:40 -0700

    Add relics and teammates to calccontext

--- libs/sr-ui/src/Context/CalcContext.tsx
+++ libs/sr-ui/src/Context/CalcContext.tsx
@@ -1,5 +1,18 @@
-import type { RelicSubStatKey } from '@genshin-optimizer/sr-consts'
-import type { Calculator } from '@genshin-optimizer/sr-formula'
+import type {
+  CharacterKey,
+  RelicSlotKey,
+  RelicSubStatKey,
+} from '@genshin-optimizer/sr-consts'
+import type {
+  ICachedLightCone,
+  ICachedRelic,
+  ICachedSroCharacter,
+} from '@genshin-optimizer/sr-db'
+import type {
+  Calculator,
+  Member,
+  TagMapNodeEntries,
+} from '@genshin-optimizer/sr-formula'
 import {
   charData,
   enemyDebuff,
@@ -15,8 +28,15 @@ import { createContext, useContext, useMemo } from 'react'
 import { useCharacter, useEquippedRelics } from '../Hook'
 import { useLightCone } from '../Hook/useLightCone'
 import { useCharacterContext } from './CharacterContext'
+
 type CalcContextObj = {
   calc: Calculator | undefined
+}
+
+type CharacterFullData = {
+  character: ICachedSroCharacter | undefined
+  lightCone: ICachedLightCone | undefined
+  relics: Record<RelicSlotKey, ICachedRelic | undefined>
 }
 
 const CalcContext = createContext({} as CalcContextObj)
@@ -27,40 +47,36 @@ export function useCalcContext() {
 
 export function CalcProvider({ children }: { children: ReactNode }) {
   const { characterKey } = useCharacterContext()
-  const character = useCharacter(characterKey)
-  const lightCone = useLightCone(character?.equippedLightCone)
-  const relics = useEquippedRelics(character?.equippedRelics)
+  const mainChar = useCharacterAndEquipment(characterKey)
+
+  const teammate1 = useCharacterAndEquipment(mainChar.character?.team[0])
+  const teammate2 = useCharacterAndEquipment(mainChar.character?.team[1])
+  const teammate3 = useCharacterAndEquipment(mainChar.character?.team[2])
+
   const calcContextObj: CalcContextObj = useMemo(
     () => ({
       calc:
-        character &&
+        mainChar.character &&
         srCalculatorWithEntries([
-          ...teamData(['member0']),
-          ...withMember(
+          ...teamData([
             'member0',
-            ...charData(character),
-            ...lightConeData(lightCone),
-            ...relicsData(
-              Object.values(relics).map((relic) => ({
-                set: relic.setKey,
-                stats: [
-                  ...relic.substats
-                    .filter(({ key }) => key !== '')
-                    .map((substat) => ({
-                      key: substat.key as RelicSubStatKey, // Safe because of the above filter
-                      value: substat.accurateValue,
-                    })),
-                  { key: relic.mainStatKey, value: relic.mainStatVal },
-                ],
-              }))
-            )
-          ),
+            ...mainChar.character.team
+              .map((key, index) =>
+                key === '' ? undefined : (`member${index + 1}` as Member)
+              )
+              .filter((m): m is Member => !!m),
+          ]),
+          ...createMember(0, mainChar),
+          ...createMember(1, teammate1),
+          ...createMember(2, teammate2),
+          ...createMember(3, teammate3),
+          // TODO: Get these from db
           enemyDebuff.common.lvl.add(80),
           enemyDebuff.common.res.add(0.1),
           selfBuff.common.critMode.add('avg'),
         ]),
     }),
-    [character, lightCone, relics]
+    [mainChar, teammate1, teammate2, teammate3]
   )
 
   return (
@@ -68,4 +84,43 @@ export function CalcProvider({ children }: { children: ReactNode }) {
       {children}
     </CalcContext.Provider>
   )
+}
+
+function useCharacterAndEquipment(
+  characterKey: CharacterKey | '' | undefined
+): CharacterFullData {
+  const character = useCharacter(characterKey)
+  const lightCone = useLightCone(character?.equippedLightCone)
+  const relics = useEquippedRelics(character?.equippedRelics)
+  return { character, lightCone, relics }
+}
+
+function createMember(
+  memberIndex: 0 | 1 | 2 | 3,
+  { character, lightCone, relics }: CharacterFullData
+): TagMapNodeEntries {
+  return !character
+    ? []
+    : withMember(
+        `member${memberIndex}`,
+        ...charData(character),
+        ...lightConeData(lightCone),
+        ...relicsData(
+          Object.values(relics)
+            .filter((relic): relic is ICachedRelic => !!relic)
+            .map((relic) => ({
+              set: relic.setKey,
+              stats: [
+                ...relic.substats
+                  .filter(({ key }) => key !== '')
+                  .map((substat) => ({
+                    key: substat.key as RelicSubStatKey, // Safe because of the above filter
+                    value: substat.accurateValue,
+                  })),
+                { key: relic.mainStatKey, value: relic.mainStatVal },
+              ],
+            }))
+        )
+        // TODO: Conditionals
+      )
 }
