c82315b6649b49cffa88d5ba8d23e4b88b6b03f3
eeeqeee <103794572+eeeqeee@users.noreply.github.com>
2024-01-28 01:19:01 -0500

    Switch from `any` to `TransObject`
    Co-authored-by: Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>

--- apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabTheorycraft/ArtifactSubCard/index.tsx
+++ apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabTheorycraft/ArtifactSubCard/index.tsx
@@ -89,7 +89,7 @@ export function ArtifactSubCard({
               <Typography>
                 <Trans t={t} i18nKey={'tabTheorycraft.maxTotalRolls'}>
                   {'The current build can only have a maximum of '}
-                  <strong>{{ value: maxTotalRolls } as any}</strong> rolls.
+                  <strong>{{ value: maxTotalRolls } as TransObject}</strong> rolls.
                 </Trans>
               </Typography>
             }
