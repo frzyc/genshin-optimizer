62ea8886f730f3ecbadb574b1eeff612578715cf
eeeqeee <103794572+eeeqeee@users.noreply.github.com>
2024-01-28 01:03:02 -0500

    set OptimizationTargetContext for tc-opt

--- apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabTheorycraft/index.tsx
+++ apps/frontend/src/app/PageCharacter/CharacterDisplay/Tabs/TabTheorycraft/index.tsx
@@ -70,6 +70,7 @@ import {
 } from './optimizeTc'
 import useCharTC from './useCharTC'
 import { WeaponEditorCard } from './WeaponEditorCard'
+import { OptimizationTargetContext } from '../../../../Context/OptimizationTargetContext'
 export default function TabTheorycraft() {
   const { t } = useTranslation('page_character')
   const { database } = useContext(DatabaseContext)
@@ -531,6 +532,7 @@ export default function TabTheorycraft() {
           <Skeleton variant="rectangular" width="100%" height={500} />
         )}
         <CardLight sx={{ flexGrow: 1, p: 1 }}>
+        <OptimizationTargetContext.Provider value={optimizationTarget}>
           {dataContextValueWithOld ? (
             <DataContext.Provider value={dataContextValueWithOld}>
               <StatDisplayComponent />
@@ -538,6 +540,7 @@ export default function TabTheorycraft() {
           ) : (
             <Skeleton variant="rectangular" width="100%" height={500} />
           )}
+          </OptimizationTargetContext.Provider>
         </CardLight>
       </Stack>
     </CharTCContext.Provider>
