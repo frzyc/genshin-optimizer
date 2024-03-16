7e96360c9990691881227fa61904cee2a3426a4b
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-03-15 20:02:22 -0700

    Remove empty tooltip for teamcard description

--- apps/frontend/src/app/PageTeams/TeamCard.tsx
+++ apps/frontend/src/app/PageTeams/TeamCard.tsx
@@ -89,9 +89,11 @@ export default function TeamCard({
         <CardActionArea onClick={() => onClick()} sx={{ p: 1 }}>
           <Typography sx={{ display: 'flex', gap: 1 }}>
             <span>{name}</span>{' '}
-            <BootstrapTooltip title={<Typography>{description}</Typography>}>
-              <InfoIcon />
-            </BootstrapTooltip>
+            {description && (
+              <BootstrapTooltip title={<Typography>{description}</Typography>}>
+                <InfoIcon />
+              </BootstrapTooltip>
+            )}
           </Typography>
         </CardActionArea>
 
