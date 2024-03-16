ff13bb170f191c0e6ba24b6a6a74a4f0e6a3942b
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-03-15 19:54:24 -0700

    Add Debate Club dmg proc

--- apps/frontend/src/app/Data/Weapons/Claymore/DebateClub/index.tsx
+++ apps/frontend/src/app/Data/Weapons/Claymore/DebateClub/index.tsx
@@ -1,7 +1,13 @@
 import type { WeaponKey } from '@genshin-optimizer/gi/consts'
 import { allStats } from '@genshin-optimizer/gi/stats'
 import { input } from '../../../../Formula'
-import { constant, infoMut, prod, subscript } from '../../../../Formula/utils'
+import {
+  constant,
+  equal,
+  infoMut,
+  prod,
+  subscript,
+} from '../../../../Formula/utils'
 import { customDmgNode } from '../../../Characters/dataUtil'
 import { st } from '../../../SheetUtil'
 import type { IWeaponSheet } from '../../IWeaponSheet'
@@ -12,17 +18,21 @@ const key: WeaponKey = 'DebateClub'
 const data_gen = allStats.weapon.data[key]
 
 const dmgPerc = [-1, 0.6, 0.75, 0.9, 1.05, 1.2]
-const dmg = customDmgNode(
-  prod(
-    subscript(input.weapon.refinement, dmgPerc, { unit: '%' }),
-    input.total.atk
-  ),
-  'elemental',
-  {
-    hit: { ele: constant('physical') },
-  }
+const dmg = equal(
+  input.weapon.key,
+  key,
+  customDmgNode(
+    prod(
+      subscript(input.weapon.refinement, dmgPerc, { unit: '%' }),
+      input.total.atk
+    ),
+    'elemental',
+    {
+      hit: { ele: constant('physical') },
+    }
+  )
 )
-const data = dataObjForWeaponSheet(key, data_gen)
+const data = dataObjForWeaponSheet(key, data_gen, undefined, { dmg })
 const sheet: IWeaponSheet = {
   document: [
     {
