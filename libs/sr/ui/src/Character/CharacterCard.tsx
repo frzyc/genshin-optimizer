fda6ae36fc66cd4937030f3d1a908809edea6e3a
Jelte Dirks <j.dirks@outlook.com>
2024-04-01 17:32:05 +0200

    remove null check on character
    Signed-off-by: Jelte Dirks <j.dirks@outlook.com>

--- libs/sr/ui/src/Character/CharacterCard.tsx
+++ libs/sr/ui/src/Character/CharacterCard.tsx
@@ -7,13 +7,6 @@ export function CharacterCard({
 }: {
   character: ICachedSroCharacter
 }) {
-  if (!character) {
-    console.error(
-      'NO CHARACTER'
-    ) /* we should prevent rendering this in the calling component */
-    return <div />
-  }
-
   return (
     <Stack>
       <CardThemed>
