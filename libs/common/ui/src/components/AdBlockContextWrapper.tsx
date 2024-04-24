cac49b5c74e97ca454372ac1acfe77eaa9118800
frzyc <frzyc@users.noreply.github.com>
2024-04-23 21:55:16 -0400

    don't touch my body

--- libs/common/ui/src/components/AdBlockContextWrapper.tsx
+++ libs/common/ui/src/components/AdBlockContextWrapper.tsx
@@ -1,9 +1,22 @@
-import type { ReactNode } from 'react'
+import { useEffect, type ReactNode } from 'react'
 import { IsAdBlockedContext } from '../context'
 import { useIsAdblocked } from '../hooks/useIsAdblocked'
 
 export function AdBlockContextWrapper({ children }: { children: ReactNode }) {
   const isAdBlocked = useIsAdblocked()
+
+  useEffect(() => {
+    // observe changes on the <body> and reset them. This prevents padding from being added below the footer for intent ads.
+    const observer = new MutationObserver(() =>
+      document.body.removeAttribute('style')
+    )
+    observer.observe(document.body, {
+      attributes: true,
+      attributeFilter: ['style'],
+    })
+    return () => observer.disconnect()
+  }, [])
+
   return (
     <IsAdBlockedContext.Provider value={isAdBlocked}>
       {children}
