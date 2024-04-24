3604abb3ab911719c1cba532d302d9eb70fe102e
frzyc <frzyc@users.noreply.github.com>
2024-04-24 00:38:03 -0400

    intent ad fixes

--- libs/common/ui/src/components/AdBlockContextWrapper.tsx
+++ libs/common/ui/src/components/AdBlockContextWrapper.tsx
@@ -5,10 +5,19 @@ import { useIsAdblocked } from '../hooks/useIsAdblocked'
 export function AdBlockContextWrapper({ children }: { children: ReactNode }) {
   const isAdBlocked = useIsAdblocked()
 
+  useIntentAdHandler()
+
+  return (
+    <IsAdBlockedContext.Provider value={isAdBlocked}>
+      {children}
+    </IsAdBlockedContext.Provider>
+  )
+}
+function useIntentAdHandler() {
   useEffect(() => {
     // observe changes on the <body> and reset them. This prevents padding from being added below the footer for intent ads.
     const observer = new MutationObserver(() =>
-      document.body.removeAttribute('style')
+      document.body.style.removeProperty('padding-bottom')
     )
     observer.observe(document.body, {
       attributes: true,
@@ -16,10 +25,4 @@ export function AdBlockContextWrapper({ children }: { children: ReactNode }) {
     })
     return () => observer.disconnect()
   }, [])
-
-  return (
-    <IsAdBlockedContext.Provider value={isAdBlocked}>
-      {children}
-    </IsAdBlockedContext.Provider>
-  )
 }
