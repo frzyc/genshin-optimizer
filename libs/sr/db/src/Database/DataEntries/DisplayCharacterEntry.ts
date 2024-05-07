fc641204dc3122f04dcd780f6179a6dcab36fe72
Tiffany Yeung <tiffanynwyeung@gmail.com>
2024-05-06 23:23:29 -0700

    Port DB UI entries for characters, relics, and lightcones to SRO (#2073)
    * port UI display db entries for characters, relics, lightcones
    
    * remove editLightConeId property
    
    * add TODO for rvHigh value

---
+++ libs/sr/db/src/Database/DataEntries/DisplayCharacterEntry.ts
@@ -0,0 +1,70 @@
+import { validateArr } from '@genshin-optimizer/common/util'
+import type {
+  ElementalTypeKey,
+  PathKey,
+  RarityKey,
+} from '@genshin-optimizer/sr/consts'
+import {
+  allElementalTypeKeys,
+  allPathKeys,
+  allRarityKeys,
+} from '@genshin-optimizer/sr/consts'
+import { DataEntry } from '../DataEntry'
+import type { SroDatabase } from '../Database'
+
+export const characterSortKeys = [
+  'new',
+  'level',
+  'rarity',
+  'name',
+  'favorite',
+] as const
+export type CharacterSortKey = (typeof characterSortKeys)[number]
+
+export interface IDisplayCharacterEntry {
+  sortType: CharacterSortKey
+  ascending: boolean
+  path: PathKey[]
+  elementalType: ElementalTypeKey[]
+  rarity: RarityKey[]
+}
+
+const initialState = (): IDisplayCharacterEntry => ({
+  sortType: 'level',
+  ascending: false,
+  path: [...allPathKeys],
+  elementalType: [...allElementalTypeKeys],
+  rarity: [...allRarityKeys],
+})
+
+export class DisplayCharacterEntry extends DataEntry<
+  'display_character',
+  'display_character',
+  IDisplayCharacterEntry,
+  IDisplayCharacterEntry
+> {
+  constructor(database: SroDatabase) {
+    super(database, 'display_character', initialState, 'display_character')
+  }
+  override validate(obj: any): IDisplayCharacterEntry | undefined {
+    if (typeof obj !== 'object') return undefined
+    let { sortType, ascending, path, elementalType, rarity } = obj
+
+    //Disallow sorting by "new" explicitly.
+    if (sortType === 'new' || !characterSortKeys.includes(sortType))
+      sortType = 'level'
+    if (typeof ascending !== 'boolean') ascending = false
+    path = validateArr(path, allPathKeys)
+    elementalType = validateArr(elementalType, allElementalTypeKeys)
+    rarity = validateArr(rarity, allRarityKeys)
+
+    const data: IDisplayCharacterEntry = {
+      sortType,
+      ascending,
+      path,
+      elementalType,
+      rarity,
+    }
+    return data
+  }
+}
