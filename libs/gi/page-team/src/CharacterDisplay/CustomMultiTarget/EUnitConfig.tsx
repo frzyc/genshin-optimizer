ea5b4bb05ca255f6c1800427e85f28ac63c5f9b5
Estis <kulaykin2000@gmail.com>
2024-05-18 04:03:56 +0500

    Custom Target Expressions v3.7.1

--- libs/gi/page-team/src/CharacterDisplay/CustomMultiTarget/EUnitConfig.tsx
+++ libs/gi/page-team/src/CharacterDisplay/CustomMultiTarget/EUnitConfig.tsx
@@ -1,7 +1,7 @@
-import { DropdownButton, TextFieldLazy } from '@genshin-optimizer/common/ui'
-import { objPathValue } from '@genshin-optimizer/common/util'
+import { CustomNumberInput, CustomNumberInputButtonGroupWrapper, DropdownButton, TextFieldLazy } from '@genshin-optimizer/common/ui'
+import { deepClone, objPathValue } from '@genshin-optimizer/common/util'
 import { allMultiOptHitModeKeys } from '@genshin-optimizer/gi/consts'
-import type { CustomTarget } from '@genshin-optimizer/gi/db'
+import type { CustomTarget, ExpressionUnit } from '@genshin-optimizer/gi/db'
 import { CharacterContext } from '@genshin-optimizer/gi/db-ui'
 import { isCharMelee } from '@genshin-optimizer/gi/stats'
 import {
@@ -11,11 +11,114 @@ import {
 } from '@genshin-optimizer/gi/ui'
 import type { CalcResult } from '@genshin-optimizer/gi/uidata'
 import { allInputPremodKeys } from '@genshin-optimizer/gi/wr'
-import { Box, Grid, MenuItem } from '@mui/material'
-import { useCallback, useContext, useMemo } from 'react'
+import { Box, ButtonGroup, CardActionArea, Collapse, Divider, Grid, IconButton, MenuItem, Typography } from '@mui/material'
+import { useCallback, useContext, useMemo, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import OptimizationTargetSelector from '../Tabs/TabOptimize/Components/OptimizationTargetSelector'
 import ReactionDropdown from './ReactionDropdown'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+// import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
+
+export default function EUnitConfig({
+  unit,
+  setUnit,
+  addUnits,
+}: {
+  unit: ExpressionUnit
+  setUnit: (unit: ExpressionUnit) => void
+  addUnits: (units: ExpressionUnit[], moveSUI?: number) => void
+}) {
+  const [collapse, setcollapse] = useState(true)
+
+  const editConstantValue = (value: number | undefined) => {
+    if (value === undefined) return
+    unit.type === 'constant' && setUnit({ ...unit, value })
+  }
+
+  const onDup = () => addUnits([deepClone(unit)])
+
+  const config = (() => {
+    const result: JSX.Element[] = []
+    if (unit.type === 'constant') {
+      result.push(
+        <ButtonGroup fullWidth>
+          <CustomNumberInputButtonGroupWrapper>
+            <CustomNumberInput
+              float
+              value={unit.value}
+              onChange={editConstantValue}
+              inputProps={{ sx: { pl: 1 } }}
+            />
+          </CustomNumberInputButtonGroupWrapper>
+        </ButtonGroup>
+      )
+    } else if (unit.type === 'target') {
+      result.push(
+        <TargetUnitConfig
+          customTarget={unit.target}
+          setCustomTarget={(t) => setUnit({ ...unit, target: t })}
+          onDup={onDup}
+        />
+      )
+    }
+    if (unit.type !== 'target') {
+      result.push(
+        <TextFieldLazy
+          fullWidth
+          label="Description"
+          value={unit.description}
+          onChange={(description) =>
+            setUnit({
+              ...unit,
+              description: description === '' ? undefined : description,
+            })
+          }
+          multiline
+          minRows={1}
+        />
+      )
+    }
+    return (
+      <Box
+        sx={{ p: 1, flexGrow: 1 }}
+        display="flex"
+        flexDirection="column"
+        gap={1}
+      >
+        {result}
+      </Box>
+    )
+  })()
+
+  return (
+    <>
+      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
+        <CardActionArea
+          sx={{
+            display: 'flex',
+            flexGrow: 1,
+            gap: 1,
+            height: '100%',
+            py: 1,
+            alignItems: 'center',
+          }}
+          onClick={() => setcollapse((c) => !c)}
+        >
+          <Typography variant="h6">Unit Config</Typography>
+          {collapse ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </CardActionArea>
+
+        <IconButton color="info" onClick={onDup}>
+          <ContentCopyIcon />
+        </IconButton>
+      </Box>
+      <Divider />
+      <Collapse in={!collapse}>{config}</Collapse>
+    </>
+  )
+}
 
 const keys = [...allInputPremodKeys]
 const wrapperFunc = (e: JSX.Element, key?: string) => (
@@ -24,7 +127,7 @@ const wrapperFunc = (e: JSX.Element, key?: string) => (
   </Grid>
 )
 
-export default function TargetUnitConfig({
+function TargetUnitConfig({
   customTarget,
   setCustomTarget,
 }: {
--- libs/gi/page-team/src/CharacterDisplay/CustomMultiTarget/ExpressionDisplay.tsx
+++ libs/gi/page-team/src/CharacterDisplay/CustomMultiTarget/ExpressionDisplay.tsx
@@ -1,52 +1,31 @@
-import { useBoolState } from '@genshin-optimizer/common/react-util'
 import {
   CardThemed,
   ColorText,
-  CustomNumberInput,
-  CustomNumberInputButtonGroupWrapper,
   SolidToggleButtonGroup,
-  TextFieldLazy,
 } from '@genshin-optimizer/common/ui'
 import {
   arrayMove,
-  deepClone,
   objPathValue,
 } from '@genshin-optimizer/common/util'
 import type {
   CustomMultiTarget,
   EnclosingOperation,
-  ExpressionOperation,
   ExpressionUnit,
 } from '@genshin-optimizer/gi/db'
 import {
   OperationSpecs,
-  initCustomTarget,
   initExpressionUnit,
-  isEnclosing,
   partsFinder,
 } from '@genshin-optimizer/gi/db'
 import { DataContext, resolveInfo } from '@genshin-optimizer/gi/ui'
-import ContentCopyIcon from '@mui/icons-material/ContentCopy'
-import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import {
   Box,
-  Button,
-  ButtonGroup,
-  CardActionArea,
-  Collapse,
-  Divider,
-  Grid,
-  IconButton,
   ToggleButton,
-  Typography,
 } from '@mui/material'
 import type { Dispatch, SetStateAction } from 'react'
 import { useContext, useEffect, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { TargetSelectorModal } from '../Tabs/TabOptimize/Components/TargetSelectorModal'
-import TargetUnitConfig from './TargetUnitConfig'
+import EUnitConfig from './EUnitConfig'
+import EControlPanel from './EControlPanel'
 
 export default function ExpressionDisplay({
   expression,
@@ -197,7 +176,7 @@ export default function ExpressionDisplay({
           zIndex: 1000,
         }}
       >
-        <UnitConfig
+        <EUnitConfig
           unit={expression[sui]}
           setUnit={setUnit}
           addUnits={addUnits}
@@ -208,268 +187,6 @@ export default function ExpressionDisplay({
           onDelete={onDelete}
         />
       </CardThemed>
-    </>
-  )
-}
-
-function EControlPanel({
-  addUnits,
-  moveUnit,
-  onDelete,
-}: {
-  addUnits: (units: ExpressionUnit[]) => void
-  moveUnit: (direction: 'left' | 'right') => void
-  onDelete: (replace?: ExpressionUnit) => void
-}) {
-  const { t } = useTranslation('page_character')
-  const [show, onShow, onClose] = useBoolState(false)
-  let newNumber: number | undefined = undefined
-  const addOperation = (operation: ExpressionOperation) => {
-    addUnits([
-      initExpressionUnit(
-        isEnclosing(operation)
-          ? { type: 'enclosing', operation, part: 'head' }
-          : { type: 'operation', operation }
-      ),
-    ])
-  }
-
-  const [_newNumber, _setNewNumber] = useState<number | undefined>(newNumber)
-
-  const addConstant = () => {
-    addUnits([initExpressionUnit({ type: 'constant', value: newNumber ?? 1 })])
-    newNumber = undefined
-    _setNewNumber(undefined)
-  }
-
-  return (
-    <Box display="flex" gap={1}>
-      <Box sx={{ flexGrow: 1 }} display="flex" flexDirection="column" gap={1}>
-        <Box display="flex" gap={1}>
-          <Button
-            key={'addNewTarget'}
-            sx={{ flexGrow: 8 }}
-            onClick={onShow}
-          >{t`multiTarget.addNewTarget`}</Button>
-          <Box sx={{ flexGrow: 1 }}>
-            <ButtonGroup fullWidth>
-              <Button key={'addConstant'} onClick={addConstant}>
-                {t`multiTarget.add`}
-              </Button>
-              <CustomNumberInputButtonGroupWrapper>
-                <CustomNumberInput
-                  float
-                  value={_newNumber}
-                  onChange={(value) => {
-                    newNumber = value
-                    _setNewNumber(value)
-                  }}
-                  allowEmpty
-                  onEnter={addConstant}
-                  inputProps={{ sx: { pl: 1 } }}
-                />
-              </CustomNumberInputButtonGroupWrapper>
-            </ButtonGroup>
-          </Box>
-          <Box sx={{ flexGrow: 1 }}>
-            <ButtonGroup fullWidth>
-              <Button key={'left'} onClick={() => moveUnit('left')}>
-                ←
-              </Button>
-              <Button key={'right'} onClick={() => moveUnit('right')}>
-                →
-              </Button>
-            </ButtonGroup>
-          </Box>
-        </Box>
-        <ButtonGroup fullWidth>
-          {(
-            [
-              'addition',
-              'subtraction',
-              'multiplication',
-              // 'division', // Division is not supported yet.
-            ] as const
-          ).map((operation) => (
-            <Grid item xs={1.5} key={operation}>
-              <Button
-                key={operation}
-                onClick={() => addOperation(operation as any)}
-              >
-                {OperationSpecs[operation].symbol}
-              </Button>
-            </Grid>
-          ))}
-          {(['minimum', 'maximum', 'average'] as const).map((operation) => (
-            <Grid item xs={1.5} key={operation}>
-              <Button key={operation} onClick={() => addOperation(operation)}>
-                {OperationSpecs[operation].symbol}
-              </Button>
-            </Grid>
-          ))}
-          <Grid item xs={0.5} key={'priority'}>
-            <Button key={'priority'} onClick={() => addOperation('priority')}>
-              {'('}
-            </Button>
-          </Grid>
-          <Grid item xs={0.5} key={'comma'}>
-            <Button
-              key={'comma'}
-              onClick={() =>
-                addUnits([
-                  initExpressionUnit({ type: 'enclosing', part: 'comma' }),
-                ])
-              }
-            >
-              {','}
-            </Button>
-          </Grid>
-          <Grid item xs={0.5} key={'tail'}>
-            <Button
-              key={'tail'}
-              onClick={() =>
-                addUnits([
-                  initExpressionUnit({ type: 'enclosing', part: 'tail' }),
-                ])
-              }
-            >
-              {')'}
-            </Button>
-          </Grid>
-        </ButtonGroup>
-      </Box>
-      <ButtonGroup orientation="vertical" color="error">
-        <Button
-          key={'delete'}
-          size="small"
-          onClick={() => onDelete()}
-          sx={{ height: '100%' }}
-        >
-          <DeleteForeverIcon />
-        </Button>
-        <Button
-          key={'null'}
-          size="small"
-          onClick={() => onDelete(initExpressionUnit({ type: 'null' }))}
-          sx={{ height: '100%' }}
-        >
-          null
-        </Button>
-      </ButtonGroup>
-      <TargetSelectorModal
-        showEmptyTargets
-        show={show}
-        onClose={onClose}
-        setTarget={(target, multi) => {
-          onClose()
-          addUnits([
-            initExpressionUnit({
-              type: 'target',
-              target: initCustomTarget(target, multi),
-            }),
-          ])
-        }}
-        excludeSections={['custom']}
-      />
-    </Box>
-  )
-}
-
-function UnitConfig({
-  unit,
-  setUnit,
-  addUnits,
-}: {
-  unit: ExpressionUnit
-  setUnit: (unit: ExpressionUnit) => void
-  addUnits: (units: ExpressionUnit[], moveSUI?: number) => void
-}) {
-  const [collapse, setcollapse] = useState(true)
-
-  const editConstantValue = (value: number | undefined) => {
-    if (value === undefined) return
-    unit.type === 'constant' && setUnit({ ...unit, value })
-  }
-
-  const onDup = () => addUnits([deepClone(unit)])
-
-  const config = (() => {
-    const result: JSX.Element[] = []
-    if (unit.type === 'constant') {
-      result.push(
-        <ButtonGroup fullWidth>
-          <CustomNumberInputButtonGroupWrapper>
-            <CustomNumberInput
-              float
-              value={unit.value}
-              onChange={editConstantValue}
-              inputProps={{ sx: { pl: 1 } }}
-            />
-          </CustomNumberInputButtonGroupWrapper>
-        </ButtonGroup>
-      )
-    } else if (unit.type === 'target') {
-      result.push(
-        <TargetUnitConfig
-          customTarget={unit.target}
-          setCustomTarget={(t) => setUnit({ ...unit, target: t })}
-          onDup={onDup}
-        />
-      )
-    }
-    if (unit.type !== 'target') {
-      result.push(
-        <TextFieldLazy
-          fullWidth
-          label="Description"
-          value={unit.description}
-          onChange={(description) =>
-            setUnit({
-              ...unit,
-              description: description === '' ? undefined : description,
-            })
-          }
-          multiline
-          minRows={1}
-        />
-      )
-    }
-    return (
-      <Box
-        sx={{ p: 1, flexGrow: 1 }}
-        display="flex"
-        flexDirection="column"
-        gap={1}
-      >
-        {result}
-      </Box>
-    )
-  })()
-
-  return (
-    <>
-      <Box sx={{ display: 'flex', gap: 1, alignItems: 'center' }}>
-        <CardActionArea
-          sx={{
-            display: 'flex',
-            flexGrow: 1,
-            gap: 1,
-            height: '100%',
-            py: 1,
-            alignItems: 'center',
-          }}
-          onClick={() => setcollapse((c) => !c)}
-        >
-          <Typography variant="h6">Unit Config</Typography>
-          {collapse ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-        </CardActionArea>
-
-        <IconButton color="info" onClick={onDup}>
-          <ContentCopyIcon />
-        </IconButton>
-      </Box>
-      <Divider />
-      <Collapse in={!collapse}>{config}</Collapse>
     </>
   )
 }
