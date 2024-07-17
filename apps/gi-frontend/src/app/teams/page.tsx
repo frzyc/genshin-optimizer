101ac6cdaba4bfe257559c7308d50568d9abd190
frzyc <frzyc@users.noreply.github.com>
2024-07-16 17:11:58 -0700

    schema refactor + initial team/loadout schema (#2278)
    * schema refactor + initial team/loadout schema
    
    * add gen-types to readme
    
    * fix types
    
    * add weapon fix
    
    * add artifact fix

--- apps/gi-frontend/src/app/teams/page.tsx
+++ apps/gi-frontend/src/app/teams/page.tsx
@@ -3,14 +3,14 @@ import { getSupabase } from '../../utils/supabase/server'
 import getProfile from '../util/getProfile'
 import getUser from '../util/getUser'
 import Content from './Content'
-import getArtifacts from './getArtifacts'
+import getTeams from './getTeams'
 
-export default async function Artifacts() {
+export default async function Teams() {
   const supabase = getSupabase()
   const user = await getUser(supabase)
   const profile = await getProfile(supabase, user)
   if (!profile?.active_account) redirect('/profile')
-  const artifacts = await getArtifacts(supabase, profile?.active_account)
-  if (!artifacts) return null
-  return <Content artifacts={artifacts} accountId={profile?.active_account} />
+  const teams = await getTeams(supabase, profile?.active_account)
+  if (!teams) return null
+  return <Content teams={teams} accountId={profile?.active_account} />
 }
