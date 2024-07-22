b78862fbaa73566d85fb5f6484802d60edbb58fd
frzyc <frzyc@users.noreply.github.com>
2024-07-21 22:23:18 -0700

    go-next client-side translations (#2329)
    * client-side translations
    
    * update artifact type translation

--- apps/gi-frontend/src/app/artifacts/getArtifacts.ts
+++ apps/gi-frontend/src/app/artifacts/getArtifacts.ts
@@ -2,6 +2,8 @@
 import type { UnArray, Unpromise } from '@genshin-optimizer/common/util'
 import type { Database } from '@genshin-optimizer/gi/supabase'
 import type { SupabaseClient } from '@supabase/supabase-js'
+import { ARTIFACT_QUERY } from './artifactquery'
+
 export default async function getArtifacts(
   supabase: SupabaseClient<Database>,
   accountId: string | null
@@ -9,9 +11,7 @@ export default async function getArtifacts(
   if (!accountId) return []
   const { data, error } = await supabase
     .from('artifacts')
-    .select(
-      `id, created_at, setKey, slotKey, level, rarity, substats(key, value), lock, mainStatKey, character:characters(id, key)`
-    )
+    .select(ARTIFACT_QUERY)
     .eq('account_id', accountId)
 
   if (error) console.error(error)
