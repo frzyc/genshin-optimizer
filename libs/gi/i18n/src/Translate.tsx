1d3169f51665d6bf9a8c5f739fc1ec50937ce698
Tiffany Yeung <tiffanynwyeung@gmail.com>
2024-08-07 23:14:41 -0700

    remove unnecessary/obsolete Translate refs in GO

--- libs/gi/i18n/src/Translate.tsx
+++ libs/gi/i18n/src/Translate.tsx
@@ -1,11 +1,8 @@
 'use client'
 import { ColorText, SqBadge, TranslateObj } from '@genshin-optimizer/common/ui'
 import '@genshin-optimizer/gi/theme' // import to validate typing for color variants
-import { Skeleton, Typography } from '@mui/material'
-import type { TFunction } from 'i18next'
 import type { ReactNode } from 'react'
-import { Suspense } from 'react'
-import { Trans, useTranslation } from 'react-i18next'
+
 const textComponents = {
   anemo: <ColorText color="anemo" />,
   geo: <ColorText color="geo" />,
@@ -73,100 +70,4 @@ export function Translate({
       components={useBadge ? badgeComponents : textComponents}
     />
   )
-}
-/**this is used cause the `components` prop mess with tag interpolation. */
-export function TransWrapper({
-  ns,
-  key18,
-  values,
-  children,
-}: {
-  ns: string
-  key18: string
-  values?: any
-  children?: any
-}) {
-  const { t } = useTranslation(ns)
-  const textKey = `${ns}:${key18}`
-  return (
-    <Suspense fallback={<Skeleton>{children}</Skeleton>}>
-      <Trans i18nKey={textKey} t={t} values={values}>
-        {children}
-      </Trans>
-    </Suspense>
-  )
-}
-function Para({ children }: { children?: JSX.Element }) {
-  return <Typography gutterBottom>{children}</Typography>
-}
-
-function T({
-  key18,
-  obj,
-  li,
-  t,
-  values,
-  useBadge,
-}: {
-  key18: string
-  obj: any
-  li?: boolean
-  t: TFunction<string, undefined>
-  values?: any
-  useBadge?: boolean
-}) {
-  if (typeof obj === 'string')
-    return (
-      <Trans
-        i18nKey={key18}
-        components={useBadge ? badgeComponents : textComponents}
-        parent={Para}
-        t={t}
-        values={values}
-      />
-    )
-  if (Array.isArray(obj))
-    return (
-      <Typography component="div">
-        <ul>
-          <T
-            key18={key18}
-            obj={{ ...obj }}
-            li
-            t={t}
-            values={values}
-            useBadge={useBadge}
-          />
-        </ul>
-      </Typography>
-    )
-  return Object.entries(obj).map(([key, val]) => {
-    if (val === '<br/>') return null
-
-    if (typeof val === 'object')
-      return (
-        <T
-          key={key as any}
-          key18={`${key18}.${key as any}`}
-          obj={val}
-          t={t}
-          values={values}
-          useBadge={useBadge}
-        />
-      )
-    if (typeof val === 'string') {
-      const trans = (
-        <Trans
-          key={key as any}
-          i18nKey={`${key18}.${key}`}
-          components={useBadge ? badgeComponents : textComponents}
-          parent={Para}
-          t={t}
-          values={values}
-        />
-      )
-      return li ? <li key={key as any}>{trans}</li> : trans
-    }
-    return null
-  }) as any
 }
