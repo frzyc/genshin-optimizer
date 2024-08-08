29498dadb80e5713ed0a949186ab880416d9598c
Tiffany Yeung <tiffanynwyeung@gmail.com>
2024-08-07 22:09:04 -0700

    add Translate to SRO + sr/theme library

---
+++ libs/sr/i18n/src/Translate.tsx
@@ -0,0 +1,48 @@
+'use client'
+import { ColorText, SqBadge, TranslateObj } from '@genshin-optimizer/common/ui'
+import '@genshin-optimizer/sr/theme' // import to validate typing for color variants
+import type { ReactNode } from 'react'
+
+const textComponents = {
+  fire: <ColorText color="fire" />,
+  ice: <ColorText color="ice" />,
+  imaginary: <ColorText color="imaginary" />,
+  lightning: <ColorText color="lightning" />,
+  physical: <ColorText color="physical" />,
+  quantum: <ColorText color="quantum" />,
+  wind: <ColorText color="wind" />,
+}
+const badgeComponents = {
+  fire: <SqBadge color="fire" />,
+  ice: <SqBadge color="ice" />,
+  imaginary: <SqBadge color="imaginary" />,
+  lightning: <SqBadge color="lightning" />,
+  physical: <SqBadge color="physical" />,
+  quantum: <SqBadge color="quantum" />,
+  wind: <SqBadge color="wind" />,
+}
+
+export function Translate({
+  ns,
+  key18,
+  values,
+  children,
+  useBadge,
+}: {
+  ns: string
+  key18: string
+  values?: Record<string, string | number>
+  children?: ReactNode
+  useBadge?: boolean
+}) {
+  return (
+    <TranslateObj
+      ns={ns}
+      key18={key18}
+      values={values}
+      children={children}
+      useBadge={useBadge}
+      components={useBadge ? badgeComponents : textComponents}
+    />
+  )
+}
--- libs/sr/i18n/src/index.ts
+++ libs/sr/i18n/src/index.ts
@@ -1,2 +1,3 @@
 import './i18n'
+export * from './Translate'
 export * from './i18n'
