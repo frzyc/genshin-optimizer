86aae6af9b302a08da11a2d246e5aa83f54d6f58
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-08-08 22:46:28 -0700

    Add AvatarBreakDamage

--- libs/sr/stats/src/executors/gen-stats/executor.ts
+++ libs/sr/stats/src/executors/gen-stats/executor.ts
@@ -8,6 +8,7 @@ import type {
 import CharacterData from './src/characterData'
 import type { LightConeDatum } from './src/lightConeData'
 import LightConeData from './src/lightConeData'
+import { MiscData } from './src/miscData'
 import type { RelicSetDatum } from './src/relicData'
 import { RelicData } from './src/relicData'
 
@@ -16,6 +17,7 @@ const path = `${proj_path}/Data`
 const characterDataDump = CharacterData()
 const lightConeDataDump = LightConeData()
 const relicDataDump = RelicData()
+const miscDataDump = MiscData()
 
 export type {
   CharacterDatum,
@@ -28,6 +30,7 @@ const allStat = {
   char: characterDataDump,
   lightCone: lightConeDataDump,
   relic: relicDataDump,
+  misc: miscDataDump,
 } as const
 
 export type AllStats = typeof allStat
@@ -46,6 +49,11 @@ export default async function runExecutor(_options: GenStatsExecutorSchema) {
   console.log(`Writing relic data to ${path}/Relic`)
   Object.entries(relicDataDump).forEach(([key, data]) =>
     dumpFile(`${path}/Relic/${key}.json`, data)
+  )
+
+  console.log(`Writing misc data to ${path}/misc`)
+  Object.entries(miscDataDump).forEach(([key, data]) =>
+    dumpFile(`${path}/misc/${key}.json`, data)
   )
 
   console.log(`Writing combined data to ${proj_path}/src/allStat_gen.json`)
