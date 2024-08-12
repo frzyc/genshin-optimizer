59f20589975513c5b6a4404b52ede570c5064c42
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2024-08-12 08:41:31 -0700

    Fix format (#2375)
    * Fix format
    
    * format again

--- libs/gi/db/src/Database/DataEntries/index.ts
+++ libs/gi/db/src/Database/DataEntries/index.ts
@@ -11,7 +11,7 @@ import { teamSortKeys, type TeamSortKey } from './DisplayTeamEntry'
 import type { TimeZoneKey } from './DisplayTool'
 import { RESIN_MAX, timeZones } from './DisplayTool'
 import type { WeaponSortKey } from './DisplayWeaponEntry'
-export { RESIN_MAX, characterSortKeys, teamSortKeys, timeZones }
+export { characterSortKeys, RESIN_MAX, teamSortKeys, timeZones }
 export type {
   ArchiveArtifactOption,
   ArchiveCharacterOption,
--- libs/gi/i18n/src/index.ts
+++ libs/gi/i18n/src/index.ts
@@ -1,3 +1,3 @@
 import './i18n'
-export * from './Translate'
 export * from './i18n'
+export * from './Translate'
