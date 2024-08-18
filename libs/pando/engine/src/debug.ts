59aeb3910d87d6fa896e07f5e2b09a17e1e01952
lantua <16190491+lantua@users.noreply.github.com>
2024-08-17 20:06:29 -0400

    - Update calculator structure - Add `Calculator.markGathered` callback

--- libs/pando/engine/src/debug.ts
+++ libs/pando/engine/src/debug.ts
@@ -1,13 +1,148 @@
-import type { AnyNode, Calculator, ReRead } from './node'
-import { traverse } from './node'
+import type {
+  AnyNode,
+  CalcResult,
+  Calculator,
+  PreRead,
+  ReRead,
+  TagCache,
+} from './node'
+import { Calculator as BaseCalculator, map, traverse } from './node'
 import type { Tag, TagMapSubsetCache } from './tag'
 import { TagMapExactValues } from './tag'
-export * from './debugCalc'
+import { isDebug } from './util'
 
-export function detectCycle(tag: Tag, calc: Calculator) {
+const isRead = Symbol()
+type TagStr = (tag: Tag, ex?: any) => string
+
+export type DebugMeta = {
+  note?: string
+  formula: string
+  deps: DebugMeta[]
+  [isRead]: boolean
+}
+export class DebugCalculator extends BaseCalculator<DebugMeta> {
+  tagStr: TagStr
+  custom: typeof this.computeCustom
+
+  constructor(calc: BaseCalculator<any>, tagStr: TagStr) {
+    super(calc.keys)
+    this.nodes = calc.nodes
+    this.tagStr = tagStr
+    this.custom = calc.computeCustom
+  }
+
+  checkCycle(tag: Tag) {
+    checkCycle(tag, this)
+  }
+
+  override _gather(cache: TagCache): PreRead<DebugMeta> {
+    // The only thing we do differently from `super` is adding `note`,
+    // which requires `tag_db` debug mode. Skip if it is unavailable
+    if (!isDebug('tag_db')) return super._gather(cache)
+
+    const result = this.calculated.refExact(cache.id)
+    if (result.length) return result[0]!
+
+    const tags = cache.tags().map((t) => (t ? this.tagStr(t) : '!!'))
+    for (const entry of cache.internal.entries) {
+      if (entry.tags.length != entry.values.length) {
+        console.log(entry)
+      }
+    }
+    const pre = cache.subset().flatMap((n, i) => {
+      if (n.op === 'reread') {
+        return this._gather(cache.with(n.tag)).pre.map((x) => {
+          // Must be a new object in case `reread` entry is shared with a regular `read`.
+          // They would have the same `val` and `meta` but different `note`.
+          x = { ...x, meta: { ...x.meta } }
+          x.meta.note = `${tags[i]} <= ${this.tagStr(n.tag)} : ${x.meta.note!}`
+          return x
+        })
+      } else {
+        const { val, meta } = this._compute(n, cache)
+        return Object.freeze({
+          val,
+          meta: {
+            note: `${tags[i]} <- ${this.tagStr(cache.tag)}`,
+            ...meta,
+          },
+        })
+      }
+    })
+    result.push({ pre })
+    return result[0]!
+  }
+  override _compute(n: AnyNode, cache: TagCache): CalcResult<any, DebugMeta> {
+    try {
+      return super._compute(n, cache)
+    } catch (e) {
+      return {
+        val: NaN,
+        meta: {
+          note: `err: ${(e as any).message} in ${this.tagStr(cache.tag)}`,
+          formula: this.nodeString(n),
+          deps: [],
+          [isRead]: true,
+        },
+      }
+    }
+  }
+
+  override computeMeta(
+    n: AnyNode,
+    val: number | string,
+    x: (CalcResult<number | string, DebugMeta> | undefined)[],
+    br: CalcResult<number | string, DebugMeta>[],
+    tag: Tag | undefined
+  ): DebugMeta {
+    function valStr(val: number | string): string {
+      if (typeof val !== 'number') return `"${val}"`
+      if (Math.round(val) === val) return `${val}`
+      return val.toFixed(2)
+    }
+
+    const result: DebugMeta = {
+      note: '',
+      formula: `[${valStr(val)}] ${this.nodeString(n)}`,
+      deps: [
+        ...x.map((x) => x?.meta).filter((x) => !!x),
+        ...br.map((br) => br.meta),
+      ].flatMap((x) => (x[isRead] ? [x] : x.deps)),
+      [isRead]: n.op === 'read',
+    }
+    if (n.op === 'read') {
+      tag = Object.fromEntries(Object.entries(tag!).filter(([_, v]) => v))
+      result.note = `gather ${x.length} nodes for ${this.tagStr(tag)}`
+      result.formula = `[${valStr(val)}] read ${this.nodeString(n)}`
+      result.deps = x.map((x) => x!.meta)
+    } else delete result.note
+    return result
+  }
+
+  override computeCustom(args: (number | string)[], op: string): any {
+    return this.custom(args, op)
+  }
+
+  nodeString(node: AnyNode): string {
+    return map([node], (node, map: (n: AnyNode) => string) => {
+      const { op, tag, br, x } = node
+      let { ex } = node
+      if (op === 'const') return `${ex}`
+      if (op === 'read') return this.tagStr(tag, ex)
+      if (op === 'subscript') ex = undefined
+      const args: string[] = []
+      if (ex) args.push(JSON.stringify(ex))
+      if (tag) args.push(this.tagStr(tag))
+      args.push(...br.map(map), ...x.map(map))
+      return `${op}(` + args.join(', ') + ')'
+    })[0]
+  }
+}
+
+function checkCycle(tag: Tag, calc: Calculator) {
   const stack: Tag[] = []
-  /** Stack depth when first encountered the tag, or 0 if already visited */
   const tagKeys = calc.keys
+  /** Stack depth when first encountered the tag, or 0 if already visited */
   const openDepth = new TagMapExactValues<number>(tagKeys.tagLen, {})
 
   function internal(cache: TagMapSubsetCache<AnyNode | ReRead>) {
--- libs/sr/formula/src/calculator.ts
+++ libs/sr/formula/src/calculator.ts
@@ -8,50 +8,57 @@ import {
 import type { Member, Read, Sheet, Tag } from './data/util'
 import { reader, tagStr } from './data/util'
 
+const reusable = Symbol()
 const { arithmetic } = calculation
-const emptyCond: CondInfo = {}
 
-type CondInfo = Partial<
-  Record<
-    Member,
-    Partial<Record<Member, Partial<Record<Sheet, Record<string, number>>>>>
-  >
->
-export type CalcMeta = {
-  tag: Tag | undefined
-  op: 'const' | 'sum' | 'prod' | 'min' | 'max' | 'sumfrac'
-  ops: CalcResult<number, CalcMeta>[]
+type MemRec<V> = Partial<Record<Member, V>>
+type CondInfo = MemRec<MemRec<Partial<Record<Sheet, Record<string, number>>>>>
+
+export type CalcMeta = PartialMeta & Info & { [reusable]?: true }
+type PartialMeta = {
+  tag?: Tag
+  op: 'const' | 'sum' | 'prod' | 'min' | 'max' | 'sumfrac' | 'res'
+  ops: CalcResult<number, PartialMeta>[]
+}
+type Info = {
   /// conds[dst][src][sheet][name]
   conds: CondInfo
 }
 
 export class Calculator extends Base<CalcMeta> {
+  override computeCustom(val: any[], op: string): any {
+    if (op == 'res') return res(val[0])
+    return super.computeCustom(val, op)
+  }
   override computeMeta(
     { op, ex }: AnyNode,
     val: number | string,
-    x: (CalcResult<number | string, CalcMeta> | undefined)[],
+    _x: (CalcResult<number | string, CalcMeta> | undefined)[],
     _br: CalcResult<number | string, CalcMeta>[],
     tag: Tag | undefined
   ): CalcMeta {
-    let conds = emptyCond
-    for (const v of [...x, ..._br]) {
-      if (!v) continue
-      conds = merge(conds, v.meta.conds)
-    }
-    if (tag?.qt === 'cond') {
-      const { src, dst, sheet, q } = tag
-      conds = merge(conds, {
-        [dst!]: { [src!]: { [sheet!]: { [q!]: val } } },
-      })
-    }
+    const info = { conds: {} }
+    const x = _x.filter((x) => !!x).map((x) => extract(x, info))
+    _br.forEach((br) => extract(br, info))
 
-    function constOverride(): CalcMeta {
-      return { tag, op: 'const', ops: [], conds }
+    function withTag(tag: Tag | undefined, meta: CalcMeta): CalcMeta {
+      return !meta.tag && tag ? { tag, ...meta } : meta
+    }
+    function finalize(
+      op: CalcMeta['op'],
+      ops: CalcResult<number, PartialMeta>[]
+    ): CalcMeta {
+      return withTag(tag, { op, ops, ...info })
+    }
+    function wrap(result: CalcResult<number | string, PartialMeta>): CalcMeta {
+      const meta = result.meta as CalcMeta
+      const reuse = meta.conds === info.conds
+      return reuse ? meta : withTag(tag, { ...meta, ...info })
     }
 
     if (op === 'read' && ex !== undefined) {
       if (ex === 'min' || ex === 'max')
-        return { ...x.find((x) => x!.val === val)!.meta, conds }
+        return wrap(x.find((x) => x!.val === val)!)
       op = ex
       ex = undefined
     }
@@ -61,40 +68,56 @@ export class Calculator extends Base<CalcMeta> {
       case 'min':
       case 'max':
       case 'sumfrac': {
-        let ops = x as CalcResult<number, CalcMeta>[]
+        let ops = x as CalcResult<number, PartialMeta>[]
         if (ops.length > 1) {
           const empty = arithmetic[op]([], ex)
           ops = ops.filter((x) => x!.val !== empty)
         }
-        if (ops.length <= 1) {
-          let meta = ops[0]?.meta ?? constOverride()
-          if (meta.conds !== conds) meta = { ...meta, conds } // Use parent `conds` when short-circuiting
-          if (!meta.tag && tag) meta = { ...meta, tag }
-          return meta
-        }
-        if (op === 'prod' && val === 0) return constOverride()
-        return { tag, op, ops, conds }
+        if (ops.length === 1) return wrap(ops[0])
+        if (ops.length === 0) return finalize('const', [])
+        return finalize(op, ops)
       }
-
       case 'const':
       case 'vtag':
       case 'subscript':
-        return constOverride()
+        return finalize('const', [])
       case 'match':
       case 'thres':
       case 'lookup':
-        return { ...x.find((x) => x)!.meta, conds }
       case 'tag':
-      case 'read':
-      case 'dtag': {
-        const { tag: baseTag, op, ops } = x[0]!.meta
-        return { tag: baseTag ?? tag, op, ops, conds }
+      case 'dtag':
+        return wrap(x[0])
+      case 'read': {
+        const result = wrap(x[0])
+        // If it is already reusable, this does nothing.
+        // If it isn't, it will be a newly created object
+        result[reusable] = true
+        return result
       }
       case 'custom':
-        throw new Error(`Unsupported operation ${ex}`)
+        return finalize(ex, x as CalcResult<number, PartialMeta>[])
       default:
         assertUnreachable(op)
     }
+  }
+  override markGathered(
+    tag: Tag,
+    _n: AnyNode,
+    result: CalcResult<string | number, CalcMeta>
+  ): CalcResult<string | number, CalcMeta> {
+    let dirty = false
+    const val = result.val
+    const meta = { ...result.meta }
+
+    if (tag.qt === 'cond') {
+      const { src, dst, sheet, q } = tag
+      meta.conds = {
+        [dst ?? null!]: { [src ?? null!]: { [sheet!]: { [q!]: val } } },
+      }
+      dirty = true
+    }
+
+    return dirty ? { val, meta } : result
   }
 
   listFormulas(read: Read): Read[] {
@@ -105,32 +128,49 @@ export class Calculator extends Base<CalcMeta> {
   listCondFormulas(read: Read): CondInfo {
     return this.listFormulas(read)
       .map((x) => this.compute(x).meta.conds)
-      .reduce(merge, emptyCond)
+      .reduce(mergeConds, {})
   }
   toDebug(): DebugCalculator {
     return new DebugCalculator(this, tagStr)
   }
 }
+export function res(x: number): number {
+  if (x >= 0.75) return 1 / (1 + 4 * x)
+  if (x >= 0) return 1 - x
+  return 1 - 0.5 * x
+}
 
-function merge<T extends Record<string, any>>(a: T, b: T): T {
+function extract<V>(
+  x: CalcResult<V, CalcMeta>,
+  info: Info
+): CalcResult<V, PartialMeta> {
+  const { conds, ...meta } = x.meta
+  info.conds = mergeConds(info.conds, conds)
+  return x.meta[reusable] ? x : { val: x.val, meta }
+}
+
+function mergeConds(a: CondInfo, b: CondInfo): CondInfo {
+  return merge(a, b, (_, v) => (typeof v === 'number' ? v : undefined))
+}
+function merge<T extends Record<string, any>>(
+  a: T,
+  b: T,
+  leaf: (a: any, b: any) => any
+): T {
+  const l = leaf(a, b)
+  if (l !== undefined) return l
   if (Object.keys(a).length < Object.keys(b).length) [a, b] = [b, a]
-  if (Object.keys(b).length === 0) return a
+  if (!Object.keys(b).length) return a
 
   let dirty = false
   const result: any = { ...a }
-  for (const [key, val] of Object.entries(b)) {
-    if (result[key] === val) continue
-    if (result[key]) {
-      const new_val = merge(result[key], val)
-      if (new_val !== result[key]) {
-        dirty = true
-        result[key] = val
-      }
-    } else {
+  for (const [k, v] of Object.entries(b)) {
+    const oldV = result[k]
+    const newV = k in result ? merge(oldV, v, leaf) : v
+    if (oldV !== newV) {
+      result[k] = newV
       dirty = true
-      result[key] = val
     }
   }
-
   return dirty ? result : a
 }
