c3d2b2998326256186ba51f5705ae11a749ac255
Tiffany Yeung <tiffanynwyeung@gmail.com>
2024-09-24 02:35:20 -0700

    add path svg icons

---
+++ libs/sr/svgicons/src/components/PathIcon.tsx
@@ -0,0 +1,36 @@
+import type { PathKey } from '@genshin-optimizer/sr/consts'
+import type { SvgIconProps } from '@mui/material'
+import {
+  AbundanceIcon,
+  DestructionIcon,
+  EruditionIcon,
+  HarmonyIcon,
+  NihilityIcon,
+  PreservationIcon,
+  TheHuntIcon,
+} from '..'
+
+export function PathIcon({
+  pathKey,
+  iconProps = {},
+}: {
+  pathKey: PathKey
+  iconProps?: SvgIconProps
+}) {
+  switch (pathKey) {
+    case 'Abundance':
+      return <AbundanceIcon {...iconProps} />
+    case 'Destruction':
+      return <DestructionIcon {...iconProps} />
+    case 'Erudition':
+      return <EruditionIcon {...iconProps} />
+    case 'Harmony':
+      return <HarmonyIcon {...iconProps} />
+    case 'Nihility':
+      return <NihilityIcon {...iconProps} />
+    case 'Preservation':
+      return <PreservationIcon {...iconProps} />
+    case 'TheHunt':
+      return <TheHuntIcon {...iconProps} />
+  }
+}
