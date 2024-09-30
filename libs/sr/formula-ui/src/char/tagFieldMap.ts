7511c15236144a6c628cc2e56b06cf4adc5b982a
frzyc <frzyc@users.noreply.github.com>
2024-09-30 13:16:28 -0400

    createTagMap util

--- libs/sr/formula-ui/src/char/tagFieldMap.ts
+++ libs/sr/formula-ui/src/char/tagFieldMap.ts
@@ -1,11 +1,7 @@
-import {
-  compileTagMapValues,
-  TagMapSubset,
-} from '@genshin-optimizer/pando/engine'
 import type { TagField } from '@genshin-optimizer/pando/ui-sheet'
 import { isTagField } from '@genshin-optimizer/pando/ui-sheet'
 import type { Tag } from '@genshin-optimizer/sr/formula'
-import { keys } from '@genshin-optimizer/sr/formula'
+import { createTagMap } from '@genshin-optimizer/sr/formula'
 import { uiSheets } from './sheets'
 import { charBaseUiSheet } from './sheets/CharBase'
 
@@ -25,7 +21,4 @@ Object.values(uiSheets).forEach((sheet) => {
 charBaseUiSheet.forEach((field) => {
   tagValue.push({ tag: field.fieldRef, value: field })
 })
-export const tagFieldMap = new TagMapSubset(
-  keys,
-  compileTagMapValues(keys, tagValue)
-)
+export const tagFieldMap = createTagMap(tagValue)
