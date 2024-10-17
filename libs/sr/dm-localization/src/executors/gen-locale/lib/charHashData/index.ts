ce607d958b06b88edd141f45523a5a6bd1cbf4f1
Tiffany Yeung <tiffanynwyeung@gmail.com>
2024-10-17 16:59:00 -0700

    rework hashdata into more separated namespaces

---
+++ libs/sr/dm-localization/src/executors/gen-locale/lib/charHashData/index.ts
@@ -0,0 +1,2 @@
+export * from './allCharacters'
+export * from './character'
---
+++ libs/sr/dm-localization/src/executors/gen-locale/lib/relicHashData/relic.ts
@@ -0,0 +1,14 @@
+export const relicHashData = {
+  label: '-1823495414', // Relics
+  set: '1604210092',
+  mainStat: '-338586359',
+  slot: '-1409515362',
+  slotKey: {
+    head: '1826268247',
+    hands: '-2144127003',
+    body: '1799740313',
+    feet: '1154536283',
+    sphere: '1873322474',
+    rope: '74129613',
+  },
+}
