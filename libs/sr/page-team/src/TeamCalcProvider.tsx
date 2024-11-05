9a5104f78b3d5f0981a8c343ab25aad09195a22d
frzyc <frzyc@users.noreply.github.com>
2024-11-05 01:35:35 -0500

    fix team calc provider

--- libs/sr/page-team/src/TeamCalcProvider.tsx
+++ libs/sr/page-team/src/TeamCalcProvider.tsx
@@ -1,15 +1,7 @@
 import { constant } from '@genshin-optimizer/pando/engine'
 import { CalcContext } from '@genshin-optimizer/pando/ui-sheet'
-import type {
-  RelicSlotKey,
-  RelicSubStatKey,
-} from '@genshin-optimizer/sr/consts'
-import type {
-  ICachedCharacter,
-  ICachedLightCone,
-  ICachedRelic,
-  TeammateDatum,
-} from '@genshin-optimizer/sr/db'
+import type { RelicSubStatKey } from '@genshin-optimizer/sr/consts'
+import type { ICachedRelic, TeammateDatum } from '@genshin-optimizer/sr/db'
 import {
   useBuild,
   useCharacter,
@@ -17,11 +9,7 @@ import {
   useRelics,
   useTeam,
 } from '@genshin-optimizer/sr/db-ui'
-import type {
-  Member,
-  Preset,
-  TagMapNodeEntries,
-} from '@genshin-optimizer/sr/formula'
+import type { Member, Preset } from '@genshin-optimizer/sr/formula'
 import {
   charData,
   conditionalEntries,
@@ -37,12 +25,6 @@ import {
 } from '@genshin-optimizer/sr/formula'
 import type { ReactNode } from 'react'
 import { useMemo } from 'react'
-
-type CharacterFullData = {
-  character: ICachedCharacter | undefined
-  lightCone: ICachedLightCone | undefined
-  relics: Record<RelicSlotKey, ICachedRelic | undefined>
-}
 
 export function TeamCalcProvider({
   teamId,
@@ -69,11 +51,11 @@ export function TeamCalcProvider({
             .filter((m): m is Member => !!m)
         ),
         // Add actual member data
-        ...(member0 ? createMember(member0) : []),
-        ...(member1 ? createMember(member1) : []),
-        ...(member2 ? createMember(member2) : []),
-        ...(member3 ? createMember(member3) : []),
-        // TODO: Get these from db
+        ...member0,
+        ...member1,
+        ...member2,
+        ...member3,
+        // TODO: Get enemy values from db
         enemyDebuff.common.lvl.add(80),
         enemyDebuff.common.res.add(0.1),
         enemyDebuff.common.isBroken.add(0),
@@ -105,51 +87,46 @@ export function TeamCalcProvider({
   return <CalcContext.Provider value={calc}>{children}</CalcContext.Provider>
 }
 
-function useCharacterAndEquipment(
-  meta: TeammateDatum | undefined
-): CharacterFullData | undefined {
+function useCharacterAndEquipment(meta: TeammateDatum | undefined) {
   const character = useCharacter(meta?.characterKey)
   // TODO: Handle tc build
-  // TODO: Handle equipped build
   const build = useBuild(meta?.buildId)
-  const lightCone = useLightCone(build?.lightConeId)
-  const relics = useRelics(build?.relicIds ?? character?.equippedRelics) // TODO: Temporary for testing
-  return useMemo(
-    () => ({
-      character,
-      lightCone,
-      relics,
-    }),
-    [character, lightCone, relics]
+  const lightCone = useLightCone(
+    meta?.buildType === 'equipped'
+      ? character?.equippedLightCone
+      : meta?.buildType === 'real'
+      ? build?.lightConeId
+      : undefined
   )
-}
-
-function createMember({
-  character,
-  lightCone,
-  relics,
-}: CharacterFullData): TagMapNodeEntries {
-  if (!character) return []
-
-  return withMember(
-    character.key,
-    ...charData(character),
-    ...lightConeData(lightCone),
-    ...relicsData(
-      Object.values(relics)
-        .filter((relic): relic is ICachedRelic => !!relic)
-        .map((relic) => ({
-          set: relic.setKey,
-          stats: [
-            ...relic.substats
-              .filter(({ key }) => key !== '')
-              .map((substat) => ({
-                key: substat.key as RelicSubStatKey, // Safe because of the above filter
-                value: substat.accurateValue,
-              })),
-            { key: relic.mainStatKey, value: relic.mainStatVal },
-          ],
-        }))
+  const relics = useRelics(
+    meta?.buildType === 'equipped'
+      ? character?.equippedRelics
+      : meta?.buildType === 'real'
+      ? build?.relicIds
+      : undefined
+  )
+  return useMemo(() => {
+    if (!character) return []
+    return withMember(
+      character.key,
+      ...charData(character),
+      ...lightConeData(lightCone),
+      ...relicsData(
+        Object.values(relics)
+          .filter((relic): relic is ICachedRelic => !!relic)
+          .map((relic) => ({
+            set: relic.setKey,
+            stats: [
+              ...relic.substats
+                .filter(({ key }) => key !== '')
+                .map((substat) => ({
+                  key: substat.key as RelicSubStatKey, // Safe because of the above filter
+                  value: substat.accurateValue,
+                })),
+              { key: relic.mainStatKey, value: relic.mainStatVal },
+            ],
+          }))
+      )
     )
-  )
+  }, [character, lightCone, relics])
 }
