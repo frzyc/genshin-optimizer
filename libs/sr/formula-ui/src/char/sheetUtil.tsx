612a384409e3538a3cf39c62569977a2dc4bc6cf
frzyc <frzyc@users.noreply.github.com>
2024-12-05 22:32:14 -0500

    refactor + sheet utils

---
+++ libs/sr/formula-ui/src/char/sheetUtil.tsx
@@ -0,0 +1,133 @@
+import {
+  getUnitStr,
+  objKeyMap,
+  toPercent,
+} from '@genshin-optimizer/common/util'
+import type {
+  Document,
+  UISheetElement,
+} from '@genshin-optimizer/pando/ui-sheet'
+import { characterAsset } from '@genshin-optimizer/sr/assets'
+import type {
+  CharacterGenderedKey,
+  StatKey,
+} from '@genshin-optimizer/sr/consts'
+import { Translate } from '@genshin-optimizer/sr/i18n'
+import {
+  getCharInterpolateObject,
+  getCharStat,
+} from '@genshin-optimizer/sr/stats'
+import { StatDisplay } from '@genshin-optimizer/sr/ui'
+import { statToFixed } from '@genshin-optimizer/sr/util'
+import { trans } from '../util'
+import type {
+  TalentSheetElementBonusAbilityKey,
+  TalentSheetElementEidolonKey,
+  TalentSheetElementStatBoostKey,
+} from './consts'
+import { allTalentSheetElementStatBoostKey, getEidolonKey } from './consts'
+import { EidolonSubtitle } from './EidolonSubtitle'
+import { SkillSubtitle } from './SkillSubtitle'
+import { bonusAbilityReqMap, bonusStatsReqMap } from './StatBoostBonusAbility'
+
+export function talentSheet(
+  ckey: CharacterGenderedKey,
+  talentKey: 'basic' | 'skill' | 'ult' | 'talent' | 'technique',
+  docs: Document[] = []
+): UISheetElement {
+  const [chg] = trans('char', ckey)
+  return {
+    title: chg(`abilities.${talentKey}.0.name`),
+    subtitle: (
+      <SkillSubtitle talentKey={talentKey}>
+        {chg(`abilities.${talentKey}.0.tag`)}
+      </SkillSubtitle>
+    ),
+    img: characterAsset(ckey, `${talentKey}_0`),
+    documents: [
+      {
+        type: 'text',
+        text: () =>
+          chg(
+            `abilities.${talentKey}.0.fullDesc`,
+            getCharInterpolateObject(ckey, talentKey, 0)
+          ),
+      },
+      ...docs,
+    ],
+  }
+}
+export function bonusAbilitySheet(
+  ckey: CharacterGenderedKey,
+  talentKey: TalentSheetElementBonusAbilityKey,
+  docs: Document[] = []
+): UISheetElement {
+  const [chg] = trans('char', ckey)
+  return {
+    title: chg(`abilities.${talentKey}.0.name`),
+    subtitle: bonusAbilityReqMap[talentKey].subtitle,
+    img: characterAsset(ckey, talentKey),
+    documents: [
+      {
+        type: 'text',
+        text: () =>
+          chg(
+            `abilities.${talentKey}.0.fullDesc`,
+            getCharInterpolateObject(ckey, talentKey, 0) // TODO: FIXME: does not seem to work for bonus Stats (wrong array format)
+          ),
+      },
+      ...docs,
+    ],
+  }
+}
+export function bonusStatsSheets(
+  ckey: CharacterGenderedKey
+): Record<TalentSheetElementStatBoostKey, UISheetElement> {
+  return objKeyMap(allTalentSheetElementStatBoostKey, (key) => {
+    const stats = getCharStat(ckey)
+    const [statKey, value] = Object.entries(
+      stats.skillTree[key]?.levels?.[0].stats ?? {}
+    )[0]
+    return {
+      title: <Translate ns={'charSheet_gen'} key18={`statBoost.${statKey}`} />,
+      subtitle: bonusStatsReqMap[key].subtitle,
+      documents: [
+        {
+          type: 'fields',
+          fields: [
+            {
+              title: <StatDisplay statKey={statKey as StatKey} />,
+              fieldValue: toPercent(value, statKey).toFixed(
+                statToFixed(statKey as any)
+              ),
+              unit: getUnitStr(statKey),
+            },
+          ],
+        },
+      ],
+    }
+  })
+}
+export function eidolonSheet(
+  ckey: CharacterGenderedKey,
+  talentKey: TalentSheetElementEidolonKey,
+  docs: Document[] = []
+): UISheetElement {
+  const [chg] = trans('char', ckey)
+  const eidolonNum = getEidolonKey(talentKey)
+  return {
+    title: chg(`ranks.${eidolonNum}.name`),
+    subtitle: <EidolonSubtitle eidolon={eidolonNum} />,
+    img: characterAsset(ckey, 'eidolon4'),
+    documents: [
+      {
+        type: 'text',
+        text: chg(
+          `ranks.${eidolonNum}.desc`,
+          getCharInterpolateObject(ckey, 'eidolon', eidolonNum)
+        ),
+      },
+      ...docs,
+    ],
+  }
+}
--- libs/sr/formula-ui/src/char/sheets/RuanMei.tsx
+++ libs/sr/formula-ui/src/char/sheets/RuanMei.tsx
@@ -1,450 +1,213 @@
 import { ColorText, ImgIcon, SqBadge } from '@genshin-optimizer/common/ui'
-import { getUnitStr, toPercent } from '@genshin-optimizer/common/util'
 import type { UISheet } from '@genshin-optimizer/pando/ui-sheet'
 import { characterAsset } from '@genshin-optimizer/sr/assets'
-import type { CharacterKey, StatKey } from '@genshin-optimizer/sr/consts'
-import {
-  buffs,
-  conditionals,
-  formulas,
-  own,
-} from '@genshin-optimizer/sr/formula'
-import {
-  getCharInterpolateObject,
-  getCharStat,
-  mappedStats,
-} from '@genshin-optimizer/sr/stats'
+import type { CharacterKey } from '@genshin-optimizer/sr/consts'
+import { buffs, conditionals, formulas } from '@genshin-optimizer/sr/formula'
+import { mappedStats } from '@genshin-optimizer/sr/stats'
 import { StatDisplay } from '@genshin-optimizer/sr/ui'
-import { statToFixed } from '@genshin-optimizer/sr/util'
 import { trans } from '../../util'
+import { type TalentSheetElementKey } from '../consts'
 import {
-  allTalentSheetElementStatBoostKey,
-  type TalentSheetElementKey,
-} from '../consts'
-import { EidolonSubtitle } from '../EidolonSubtitle'
-import { SkillSubtitle } from '../SkillSubtitle'
-import { bonusAbilityReqMap, bonusStatsReqMap } from '../StatBoostBonusAbility'
+  bonusAbilitySheet,
+  bonusStatsSheets,
+  eidolonSheet,
+  talentSheet,
+} from '../sheetUtil'
 const key: CharacterKey = 'RuanMei'
 const [chg, _ch] = trans('char', key)
 const formula = formulas.RuanMei
 const cond = conditionals.RuanMei
 const buff = buffs.RuanMei
 const dm = mappedStats.char[key]
-const stats = getCharStat(key)
 const sheet: UISheet<TalentSheetElementKey> = {
-  basic: {
-    title: chg('abilities.basic.0.name'),
-    subtitle: (
-      <SkillSubtitle talentKey="basic">
-        {chg('abilities.basic.0.tag')}
-      </SkillSubtitle>
-    ),
-    img: characterAsset(key, 'basic_0'),
-    documents: [
-      {
-        type: 'text',
-        text: (calculator) =>
-          chg(
-            `abilities.basic.0.fullDesc`,
-            getCharInterpolateObject(
-              key,
-              'basic',
-              calculator.compute(own.char.basic).val
-            )
-          ),
-      },
-      {
-        type: 'fields',
+  basic: talentSheet(key, 'basic', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          title: chg('abilities.basic.0.name'),
+          fieldRef: formula.basicDmg_0.tag,
+        },
+      ],
+    },
+  ]),
+  skill: talentSheet(key, 'skill', [
+    {
+      type: 'conditional',
+      conditional: {
+        header: {
+          icon: <ImgIcon src={characterAsset(key, 'skill_0')} />,
+          // TODO: translate
+          text: 'skill active',
+          additional: <SqBadge>Skill</SqBadge>,
+        },
+        metadata: cond.skillOvertone,
+        label: 'Overtone',
         fields: [
           {
-            title: chg('abilities.basic.0.name'),
-            fieldRef: formula.basicDmg_0.tag,
-          },
-        ],
-      },
-    ],
-  },
-  skill: {
-    title: chg('abilities.skill.0.name'),
-    subtitle: (
-      <SkillSubtitle talentKey="skill">
-        {chg('abilities.skill.0.tag')}
-      </SkillSubtitle>
-    ),
-    img: characterAsset(key, 'skill_0'),
-    documents: [
-      {
-        type: 'text',
-        text: (calculator) =>
-          chg(
-            `abilities.skill.0.fullDesc`,
-            getCharInterpolateObject(
-              key,
-              'skill',
-              calculator.compute(own.char.skill).val
-            )
-          ),
-      },
-      {
-        type: 'conditional',
-        conditional: {
-          header: {
-            icon: <ImgIcon src={characterAsset(key, 'skill_0')} />,
             // TODO: translate
-            text: 'skill active',
-            additional: <SqBadge>Skill</SqBadge>,
+            title: 'Ally DMG Increase',
+            fieldRef: buff.skillOvertone_dmg_.tag,
           },
-          metadata: cond.skillOvertone,
-          label: 'Overtone',
-          fields: [
-            {
-              // TODO: translate
-              title: 'Ally DMG Increase',
-              fieldRef: buff.skillOvertone_dmg_.tag,
-            },
-            {
-              title: <StatDisplay statKey="weakness_" />,
-              fieldRef: buff.skillOvertone_weakness_.tag,
-            },
-            // TODO: translate DM "Duration"
-            { title: 'Duration', fieldValue: dm.skill.duration },
-          ],
-        },
-      },
-    ],
-  },
-  ult: {
-    title: chg('abilities.ult.0.name'),
-    subtitle: (
-      <SkillSubtitle talentKey="ult">
-        {chg('abilities.ult.0.tag')}
-      </SkillSubtitle>
-    ),
-    img: characterAsset(key, 'ult_0'),
-    documents: [
-      {
-        type: 'text',
-        text: (calculator) =>
-          chg(
-            `abilities.ult.0.fullDesc`,
-            getCharInterpolateObject(
-              key,
-              'ult',
-              calculator.compute(own.char.ult).val
-            )
-          ),
-      },
-      {
-        type: 'conditional',
-        conditional: {
-          header: {
-            icon: <ImgIcon src={characterAsset(key, 'ult_0')} />,
-            text: 'ult active',
-            additional: <SqBadge>Ult</SqBadge>,
+          {
+            title: <StatDisplay statKey="weakness_" />,
+            fieldRef: buff.skillOvertone_weakness_.tag,
           },
-          metadata: cond.ultZone,
-          label: 'Ult zone',
-          fields: [
-            {
-              title: <StatDisplay statKey="resPen_" />,
-              fieldRef: buff.ultZone_resPen_.tag,
-            },
-            // TODO: action delay % on Thanatoplum Rebloom trigger
-            {
-              // TODO: translate
-              title: (
-                <ColorText color="ice">
-                  Thanatoplum Rebloom Break Damage
-                </ColorText>
-              ),
-              fieldRef: formula.zoneBreakDmg.tag,
-            },
-            // TODO: show the increase of dmg multipler from e6
-            // TODO: translate DM "Duration"
-            // TODO: show the increase of zone duration from e6
-            { title: 'Duration', fieldValue: dm.ult.duration },
-          ],
+          // TODO: translate DM "Duration"
+          { title: 'Duration', fieldValue: dm.skill.duration },
+        ],
+      },
+    },
+  ]),
+  ult: talentSheet(key, 'ult', [
+    {
+      type: 'conditional',
+      conditional: {
+        header: {
+          icon: <ImgIcon src={characterAsset(key, 'ult_0')} />,
+          text: 'ult active',
+          additional: <SqBadge>Ult</SqBadge>,
         },
-      },
-    ],
-  },
-  talent: {
-    title: chg('abilities.talent.0.name'),
-    subtitle: (
-      <SkillSubtitle talentKey="talent">
-        {chg('abilities.talent.0.tag')}
-      </SkillSubtitle>
-    ),
-    img: characterAsset(key, 'talent_0'),
-    documents: [
-      {
-        type: 'text',
-        text: (calculator) =>
-          chg(
-            `abilities.talent.0.fullDesc`,
-            getCharInterpolateObject(
-              key,
-              'talent',
-              calculator.compute(own.char.talent).val
-            )
-          ),
-      },
-      {
-        type: 'fields',
+        metadata: cond.ultZone,
+        label: 'Ult zone',
         fields: [
           {
-            title: <StatDisplay statKey="spd_" />,
-            fieldRef: buff.talent_spd_.tag,
+            title: <StatDisplay statKey="resPen_" />,
+            fieldRef: buff.ultZone_resPen_.tag,
           },
+          // TODO: action delay % on Thanatoplum Rebloom trigger
           {
-            //TODO: translate
+            // TODO: translate
             title: (
-              <ColorText color="ice">Somatotypical Helix Break DMG</ColorText>
+              <ColorText color="ice">
+                Thanatoplum Rebloom Break Damage
+              </ColorText>
             ),
-            fieldRef: formula.talentBreakDmg.tag,
+            fieldRef: formula.zoneBreakDmg.tag,
           },
+          // TODO: show the increase of dmg multipler from e6
+          // TODO: translate DM "Duration"
+          // TODO: show the increase of zone duration from e6
+          { title: 'Duration', fieldValue: dm.ult.duration },
         ],
       },
-    ],
-  },
-  technique: {
-    title: chg('abilities.technique.0.name'),
-    subtitle: (
-      <SkillSubtitle talentKey="technique">
-        {chg('abilities.technique.0.tag')}
-      </SkillSubtitle>
-    ),
-    img: characterAsset(key, 'technique_0'),
-    documents: [
-      {
-        type: 'text',
-        text: () =>
-          chg(
-            `abilities.technique.0.fullDesc`,
-            getCharInterpolateObject(key, 'technique', 0)
-          ),
-      },
-    ],
-  },
-  bonusAbility1: {
-    title: chg('abilities.bonusAbility1.0.name'),
-    subtitle: bonusAbilityReqMap['bonusAbility1'].subtitle,
-    img: characterAsset(key, 'bonusAbility1'),
-    documents: [
-      {
-        type: 'text',
-        text: () =>
-          chg(
-            `abilities.bonusAbility1.0.fullDesc`,
-            getCharInterpolateObject(key, 'bonusAbility1', 0)
-          ),
-      },
-    ],
-  },
-  bonusAbility2: {
-    title: chg('abilities.bonusAbility2.0.name'),
-    subtitle: bonusAbilityReqMap['bonusAbility2'].subtitle,
-    img: characterAsset(key, 'bonusAbility2'),
-    documents: [
-      {
-        type: 'text',
-        text: () =>
-          chg(
-            `abilities.bonusAbility2.0.fullDesc`,
-            getCharInterpolateObject(key, 'bonusAbility2', 0)
-          ),
-      },
-    ],
-  },
-  bonusAbility3: {
-    title: chg('abilities.bonusAbility3.0.name'),
-    subtitle: bonusAbilityReqMap['bonusAbility3'].subtitle,
-    img: characterAsset(key, 'bonusAbility3'),
-    documents: [
-      {
-        type: 'text',
-        text: () =>
-          chg(
-            `abilities.bonusAbility3.0.fullDesc`,
-            getCharInterpolateObject(key, 'bonusAbility3', 0) // TODO: FIXME: does not seem to work for bonus Stats (wrong array format)
-          ),
-      },
-      {
-        type: 'fields',
-        fields: [
-          {
-            title: 'bonus ability 3 buff',
-            fieldRef: buff.ba3_brEff_.tag,
-          },
-        ],
-      },
-      {
-        type: 'fields',
-        fields: [
-          {
-            title: 'bonus ability 3 formula',
-            fieldRef: formula.ba3_brEff_.tag,
-          },
-        ],
-      },
-    ],
-  },
-  ...Object.fromEntries(
-    allTalentSheetElementStatBoostKey.map((key, i) => [
-      key,
-      {
-        title: `Boost ${i + 1}`, // TODO: needs "Speed Boost" from i18n
-        subtitle: bonusStatsReqMap[key].subtitle,
-        documents: [
-          {
-            type: 'fields',
-            fields: Object.entries(
-              stats.skillTree[key]?.levels?.[0].stats ?? {}
-            ).map(([statKey, value]) => ({
-              title: <StatDisplay statKey={statKey as StatKey} />,
-              fieldValue: toPercent(value, statKey).toFixed(
-                statToFixed(statKey as any)
-              ),
-              unit: getUnitStr(statKey),
-            })),
-          },
-        ],
-      },
-    ])
-  ),
-
-  eidolon1: {
-    title: chg('ranks.1.name'),
-    subtitle: <EidolonSubtitle eidolon={1} />,
-    img: characterAsset(key, 'eidolon1'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.1.desc`, getCharInterpolateObject(key, 'eidolon', 1)),
-      },
-      {
-        type: 'fields',
-        fields: [
-          {
-            // TODO: defIgn_ translation
-            title: 'Ignore DEF',
-            fieldRef: buff.e1_defIgn_.tag,
-          },
-        ],
-      },
-    ],
-  },
-  eidolon2: {
-    title: chg('ranks.2.name'),
-    subtitle: <EidolonSubtitle eidolon={2} />,
-    img: characterAsset(key, 'eidolon2'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.2.desc`, getCharInterpolateObject(key, 'eidolon', 2)),
-      },
-      {
-        type: 'fields',
-        fields: [
-          {
-            title: 'atk_',
-            fieldRef: buff.e2_atk_.tag,
-          },
-        ],
-      },
-    ],
-  },
-  eidolon3: {
-    title: chg('ranks.3.name'),
-    subtitle: <EidolonSubtitle eidolon={3} />,
-    img: characterAsset(key, 'eidolon3'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.3.desc`, getCharInterpolateObject(key, 'eidolon', 3)),
-      },
-      {
-        type: 'fields',
-        fields: [
-          {
-            //TODO: Translate
-            title: 'talent',
-            fieldRef: buff.eidolon3_talent.tag,
-          },
-          {
-            //TODO: Translate
-            title: 'ult',
-            fieldRef: buff.eidolon3_ult.tag,
-          },
-        ],
-      },
-    ],
-  },
-  eidolon4: {
-    title: chg('ranks.4.name'),
-    subtitle: <EidolonSubtitle eidolon={4} />,
-    img: characterAsset(key, 'eidolon4'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.4.desc`, getCharInterpolateObject(key, 'eidolon', 4)),
-      },
-      {
-        type: 'conditional',
-        conditional: {
-          header: {
-            icon: <ImgIcon src={characterAsset(key, 'eidolon4')} />,
-            text: chg('ranks.4.name'),
-            additional: <SqBadge>Eidolon 4</SqBadge>,
-          },
-          metadata: cond.e4Broken,
-          label: 'Enemy Weakness Broken',
-          fields: [
-            // TODO: Translation
-            { title: 'Break Effect', fieldRef: buff.e4_break_.tag },
-            // TODO: Translation? duration?
-            { title: 'Turns', fieldValue: dm.e4.duration },
-          ],
+    },
+  ]),
+  talent: talentSheet(key, 'talent', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          title: <StatDisplay statKey="spd_" />,
+          fieldRef: buff.talent_spd_.tag,
         },
-      },
-    ],
-  },
-  eidolon5: {
-    title: chg('ranks.5.name'),
-    subtitle: <EidolonSubtitle eidolon={5} />,
-    img: characterAsset(key, 'eidolon5'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.5.desc`, getCharInterpolateObject(key, 'eidolon', 5)),
-      },
-      {
-        type: 'fields',
+        {
+          //TODO: translate
+          title: (
+            <ColorText color="ice">Somatotypical Helix Break DMG</ColorText>
+          ),
+          fieldRef: formula.talentBreakDmg.tag,
+        },
+      ],
+    },
+  ]),
+  technique: talentSheet(key, 'technique'),
+  bonusAbility1: bonusAbilitySheet(key, 'bonusAbility1'),
+  bonusAbility2: bonusAbilitySheet(key, 'bonusAbility2'),
+  bonusAbility3: bonusAbilitySheet(key, 'bonusAbility3', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          title: 'bonus ability 3 buff',
+          fieldRef: buff.ba3_brEff_.tag,
+        },
+        {
+          title: 'bonus ability 3 formula',
+          fieldRef: formula.ba3_brEff_.tag,
+        },
+      ],
+    },
+  ]),
+
+  ...bonusStatsSheets(key),
+  eidolon1: eidolonSheet(key, 'eidolon1', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          // TODO: defIgn_ translation
+          title: 'Ignore DEF',
+          fieldRef: buff.e1_defIgn_.tag,
+        },
+      ],
+    },
+  ]),
+  eidolon2: eidolonSheet(key, 'eidolon2', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          title: 'atk_',
+          fieldRef: buff.e2_atk_.tag,
+        },
+      ],
+    },
+  ]),
+  eidolon3: eidolonSheet(key, 'eidolon3', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          //TODO: Translate
+          title: 'talent',
+          fieldRef: buff.eidolon3_talent.tag,
+        },
+        {
+          //TODO: Translate
+          title: 'ult',
+          fieldRef: buff.eidolon3_ult.tag,
+        },
+      ],
+    },
+  ]),
+  eidolon4: eidolonSheet(key, 'eidolon4', [
+    {
+      type: 'conditional',
+      conditional: {
+        header: {
+          icon: <ImgIcon src={characterAsset(key, 'eidolon4')} />,
+          text: chg('ranks.4.name'),
+          additional: <SqBadge>Eidolon 4</SqBadge>,
+        },
+        metadata: cond.e4Broken,
+        label: 'Enemy Weakness Broken',
         fields: [
-          {
-            //TODO: Translate
-            title: 'basic',
-            fieldRef: buff.eidolon5_basic.tag,
-          },
-          {
-            //TODO: Translate
-            title: 'skill',
-            fieldRef: buff.eidolon5_skill.tag,
-          },
+          // TODO: Translation
+          { title: 'Break Effect', fieldRef: buff.e4_break_.tag },
+          // TODO: Translation? duration?
+          { title: 'Turns', fieldValue: dm.e4.duration },
         ],
       },
-    ],
-  },
-  eidolon6: {
-    title: chg('ranks.6.name'),
-    subtitle: <EidolonSubtitle eidolon={6} />,
-    img: characterAsset(key, 'eidolon6'),
-    documents: [
-      {
-        type: 'text',
-        text: chg(`ranks.6.desc`, getCharInterpolateObject(key, 'eidolon', 6)),
-      },
-    ],
-  },
+    },
+  ]),
+  eidolon5: eidolonSheet(key, 'eidolon5', [
+    {
+      type: 'fields',
+      fields: [
+        {
+          //TODO: Translate
+          title: 'basic',
+          fieldRef: buff.eidolon5_basic.tag,
+        },
+        {
+          //TODO: Translate
+          title: 'skill',
+          fieldRef: buff.eidolon5_skill.tag,
+        },
+      ],
+    },
+  ]),
+  eidolon6: eidolonSheet(key, 'eidolon6'),
 }
 export default sheet
