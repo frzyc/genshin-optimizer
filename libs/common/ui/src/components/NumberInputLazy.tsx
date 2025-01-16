beb669ddbf52b20418b0eb2e8a548eb1db403549
failchon <kill.to@live.com>
2025-01-16 23:57:33 +0200

    Fixes: Clear TC autocomplete on selection, Allow commas in number inp… (#2597)
    * Fixes: Clear TC autocomplete on selection, Allow commas in number inputs, Allow ctrl+v on artifact editor
    
    * Resolve comments
    
    ---------
    
    Co-authored-by: Failchon <slay3r12@gmail.com>

--- libs/common/ui/src/components/NumberInputLazy.tsx
+++ libs/common/ui/src/components/NumberInputLazy.tsx
@@ -25,7 +25,8 @@ export function NumberInputLazy({
   }, [valueProp])
 
   const saveValue = () => {
-    let num = float ? parseFloat(value) : parseInt(value)
+    const normalizedValue = float ? value.replace(',', '.') : value
+    let num = float ? parseFloat(normalizedValue) : parseInt(normalizedValue)
     if (isNaN(num)) {
       num = 0
       setValue(num.toString())
@@ -48,7 +49,7 @@ export function NumberInputLazy({
   ) => {
     const val = event.target.value
 
-    if (val.match(float ? /[^0-9.-]/ : /[^0-9-]/)) {
+    if (val.match(float ? /[^0-9.,-]/ : /[^0-9-]/)) {
       return event.preventDefault()
     }
 
