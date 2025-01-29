9e6a46d7747c4298c7a618bc5ad56536908c811a
frzyc <frzyc@users.noreply.github.com>
2025-01-29 02:19:34 -0500

    fix typecheck

--- libs/common/database-ui/src/hooks/useDataEntryBase.tsx
+++ libs/common/database-ui/src/hooks/useDataEntryBase.tsx
@@ -1,9 +1,16 @@
 'use client'
-import type { DataEntryBase } from '@genshin-optimizer/common/database'
+import type {
+  Database,
+  DataEntryBase,
+} from '@genshin-optimizer/common/database'
 import { useEffect, useState } from 'react'
-export function useDataEntryBase<A extends string, B extends string, C, D>(
-  entry: DataEntryBase<A, B, C, D>
-) {
+export function useDataEntryBase<
+  A extends string,
+  B extends string,
+  C,
+  D,
+  E extends Database
+>(entry: DataEntryBase<A, B, C, D, E>) {
   const [data, setData] = useState(() => entry?.get())
 
   useEffect(() => {
