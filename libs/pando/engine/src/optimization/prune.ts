c062ecec025d9a391a81f4af74526ea126b7f51e
lantua <16190491+lantua@users.noreply.github.com>
2025-02-08 00:03:05 -0500

    Update `monotonicity` calculation

--- libs/pando/engine/src/optimization/prune.ts
+++ libs/pando/engine/src/optimization/prune.ts
@@ -7,16 +7,16 @@ const { arithmetic, branching } = calculation
 
 type Component = Record<string, number>
 type Builds = Component[][]
-type Tonicity = { inc: boolean; dec: boolean }
+type Monotonicity = { inc: boolean; dec: boolean }
 type Range = { min: number; max: number }
 
 type CompRanges = Record<string, Range>[]
 type NodeRanges = Map<AnyNode<OP>, Range>
-type Tonicities = Map<string, Tonicity>
+type Monotonicities = Map<string, Monotonicity>
 
 export type CustomInfo = {
   range: (r: Range[]) => Range
-  tonicity: (r: Range[]) => Tonicity[]
+  monotonicity: (r: Range[]) => Monotonicity[]
 }
 export const customInfo: Record<string, CustomInfo> = {}
 
@@ -121,7 +121,7 @@ function pruneCompRange<C extends Component>(
 /** Get range assuming any item in `comp` can be selected */
 function getCompRanges(comp: Component[]): CompRanges[number] {
   const iter = comp.values()
-  const first = iter.next().value
+  const first: Component = iter.next().value
   if (!first) return {}
   const result = Object.fromEntries(
     Object.entries(first).map(([k, v]) => [k, { min: v, max: v }])
@@ -221,38 +221,32 @@ function getNodeRanges(
   return result
 }
 
-function _getTonicities(
+function _getMonotonicities(
   n: NumNode<OP>[],
   cat: string,
   ranges: NodeRanges
-): Tonicities {
-  const t = new Map<AnyNode<OP>, Tonicity>()
-  const marked: { node: AnyNode<OP>; inc: boolean }[] = []
-  const result = new Map<string, Tonicity>()
+): Monotonicities {
+  const mon = new Map<AnyNode<OP>, Monotonicity>()
+  const toVisit: { node: AnyNode<OP>; ty: boolean }[] = []
+  const result = new Map<string, Monotonicity>()
 
-  function mark(node: AnyNode<OP>, inc: boolean) {
-    if (!t.has(node)) t.set(node, { inc: false, dec: false })
-    const ton = t.get(node)!
-    if (inc) {
-      if (!ton.inc) {
-        ton.inc = true
-        marked.push({ node, inc })
-      }
-    } else {
-      if (!ton.dec) {
-        ton.dec = true
-        marked.push({ node, inc })
-      }
-    }
+  // `ty` if inc or non-monotonic. `!ty` if dec or non-monotonic
+  function visit(node: AnyNode<OP>, ty: boolean) {
+    if (!mon.has(node)) mon.set(node, { inc: true, dec: true })
+    const ton = mon.get(node)!
+    toVisit.push({ node, ty })
+    if (ty && ton.dec) ton.dec = false
+    else if (!ty && ton.inc) ton.inc = false
+    else toVisit.pop() // no update
   }
 
   // Cannot use `traverse` because each node is visited twice, once for `inc` and once for `dec`
-  n.forEach((n) => mark(n, true))
-  while (marked.length) {
-    const { node, inc } = marked.pop()!
+  n.forEach((n) => visit(n, true))
+  while (toVisit.length) {
+    const { node, ty } = toVisit.pop()!
     switch (node.op) {
       case 'read':
-        result.set(node.tag[cat]!, t.get(node)!)
+        result.set(node.tag[cat]!, mon.get(node)!)
         break
       case 'const':
       case 'subscript':
@@ -260,68 +254,58 @@ function _getTonicities(
       case 'sum':
       case 'min':
       case 'max':
-        node.x.forEach((n) => mark(n, inc))
+        node.x.forEach((n) => visit(n, ty))
         break
       case 'match':
-        node.x.forEach((n) => mark(n, inc))
-        node.br.forEach((n) => mark(n, true))
-        node.br.forEach((n) => mark(n, false))
+      case 'lookup':
+        node.x.forEach((n) => visit(n, ty))
+        node.br.forEach((n) => visit(n, true))
+        node.br.forEach((n) => visit(n, false))
         break
       case 'thres': {
-        node.x.forEach((n) => mark(n, inc))
+        node.x.forEach((n) => visit(n, ty))
         const ge = ranges.get(node.x[0])!
         const lt = ranges.get(node.x[1])!
-        // if `br` is not marked, both branches are equal
+        // if `br` is not visited, both branches are equal
         if (ge.max > lt.min) {
-          mark(node.br[0], inc)
-          mark(node.br[1], !inc)
+          visit(node.br[0], ty)
+          visit(node.br[1], !ty)
         }
         if (ge.min < lt.max) {
-          mark(node.br[0], !inc)
-          mark(node.br[1], inc)
+          visit(node.br[0], !ty)
+          visit(node.br[1], ty)
         }
         break
       }
       case 'sumfrac': {
         const [x, c] = node.x.map((n) => ranges.get(n)!)
-        // if `x` is not marked, `c == 0` and `node == 1`
-        if (c.min < 0) mark(node.x[0], !inc)
-        if (c.max > 0) mark(node.x[0], inc)
-        // if `c` is not marked, `x == 0` and `node == 0`
-        if (x.min < 0) mark(node.x[1], inc)
-        if (x.max > 0) mark(node.x[1], !inc)
+        // if `x` is not visited, `c == 0` and `node == 1`
+        if (c.min < 0) visit(node.x[0], !ty)
+        if (c.max > 0) visit(node.x[0], ty)
+        // if `c` is not visited, `x == 0` and `node == 0`
+        if (x.min < 0) visit(node.x[1], ty)
+        if (x.max > 0) visit(node.x[1], !ty)
         break
       }
-      case 'lookup':
-        node.x.forEach((n) => mark(n, inc))
-        node.br.forEach((n) => mark(n, true))
-        node.br.forEach((n) => mark(n, false))
-        break
       case 'prod': {
         const r = ranges.get(node)!
-        if (r.min < 0 && r.max > 0) {
-          // unsupported zero-crossing
-          node.x.forEach((n) => mark(n, true))
-          node.x.forEach((n) => mark(n, false))
-        } else {
-          const pos = r.min >= 0
-          const neg = r.max <= 0
-          node.x.forEach((n) => {
-            const nr = ranges.get(n)!
-            const npos = nr.min >= 0
-            const nneg = nr.max <= 0
-            if ((pos && npos) || (neg && nneg)) mark(n, inc)
-            if ((pos && nneg) || (neg && npos)) mark(n, !inc)
-          })
-        }
+        const pos = r.min > 0
+        if (!pos && r.max >= 0) {
+          // unsupported zero-touching
+          node.x.forEach((n) => visit(n, true))
+          node.x.forEach((n) => visit(n, false))
+        } else
+          node.x.forEach((n) =>
+            visit(n, (pos === ty) === ranges.get(n)!.min > 0)
+          )
         break
       }
       case 'custom':
         customInfo[node.ex]
-          .tonicity(node.x.map((n) => ranges.get(n)!))
+          .monotonicity(node.x.map((n) => ranges.get(n)!))
           .forEach((t, i) => {
-            if (t.inc) mark(node.x[i], inc)
-            if (t.dec) mark(node.x[i], !inc)
+            if (!t.inc) visit(node.x[i], !ty)
+            if (!t.dec) visit(node.x[i], ty)
           })
         break
       default:
