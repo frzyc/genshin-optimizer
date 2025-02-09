b739b4def41f41020be00d3c97f4f5032c0e387e
frzyc <frzyc@users.noreply.github.com>
2025-02-08 23:59:17 -0500

    gen-file

--- libs/zzz/assets/src/gen/index.ts
+++ libs/zzz/assets/src/gen/index.ts
@@ -1,9 +1,11 @@
 // This is a generated index file.
 import chars from './chars'
 import discs from './discs'
+import wengines from './wengines'
 
 const data = {
   chars,
   discs,
+  wengines,
 } as const
 export default data
