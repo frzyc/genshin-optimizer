7ea2675ae16e22d1640c34e6514784074760b519
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-02-24 08:14:40 -0700

    Remove comment

--- libs/sr/solver/src/childWorker.ts
+++ libs/sr/solver/src/childWorker.ts
@@ -163,7 +163,6 @@ async function start() {
       sphere,
       rope,
     ])
-    // Constraints are offset by 1 because the opt target is first
     if (constraints.every((value, i) => results[i] >= value)) {
       builds.push({
         value: results[0], // We only pass 1 target to calculate, as the first entry
