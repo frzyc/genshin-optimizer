5fc1a17b2c1076e29c48ded028b4fd7c9d124494
frzyc <frzyc@users.noreply.github.com>
2025-02-26 12:41:33 -0500

    ZO: Add GA

--- apps/zzz-frontend/src/main.tsx
+++ apps/zzz-frontend/src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import ReactGA from 'react-ga4'
 import App from './app/App'
-
+ReactGA.initialize(process.env.NX_GA_TRACKINGID as any, {
+  testMode: process.env.NODE_ENV === 'development',
+})
 const root = createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
