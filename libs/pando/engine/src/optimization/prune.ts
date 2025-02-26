f4ef36b93c11d2e8ff7018800e330df0cae8313d
lantua <16190491+lantua@users.noreply.github.com>
2025-02-26 01:09:07 -0500

    Improve `reaffine` key reassignment

--- libs/pando/engine/src/optimization/prune.ts
+++ libs/pando/engine/src/optimization/prune.ts
@@ -10,7 +10,7 @@ import {
   traverse,
 } from '../node'
 import type { Monotonicity, Range } from '../util'
-import { assertUnreachable, customOps } from '../util'
+import { assertUnreachable, customOps, isDebug } from '../util'
 import { combineConst, flatten } from './simplify'
 const { arithmetic, branching } = calculation
 
@@ -265,13 +265,26 @@ export function reaffine(state: State<OP>) {
     }
   })
 
-  const weightNames = new Map(
-    [...topWeights.values()].map((w, i) => [w, `c${i}`])
+  // { cat1:w1 cat2:w2 .. } => "!cat1:<w1>:cat2:<w2>:.." with PUA chars
+  // instead of `!` and `:` to minimize the chance of them being in some `cat`s
+  let readNames = new Map(
+    [...topWeights.values()].map((w) => {
+      const keys = Object.keys(w).sort()
+      if (keys.length === 1 && w[keys[0]] === 1) return [w, keys[0]]
+      // skip `w[offset]` because it doesn't go into the new `Read` nodes
+      return [w, '\u{F33D}' + keys.flatMap((k) => [k, w[k]]).join('\u{F00D}')]
+    })
   )
+  if (!isDebug('calc')) {
+    // "!cat1:<w1>:cat2:<w2>:.." => "cX" (skipped in debug mode)
+    const map = new Map([...readNames.values()].map((id, i) => [id, `c${i}`]))
+    readNames = new Map([...readNames].map(([w, id]) => [w, map.get(id)!]))
+  }
   const newCandidates = candidates.map((comp) =>
     comp.map((c) => {
       const result: Component = { id: c['id'] }
-      weightNames.forEach((name, w) => {
+      readNames.forEach((name, w) => {
+        if (name in result) return // same weight, different offsets
         result[name] = Object.entries(w).reduce(
           (acu, [k, v]) => acu + (c[k] ?? 0) * v,
           0
@@ -281,40 +294,39 @@ export function reaffine(state: State<OP>) {
     })
   )
 
-  let shouldChange = false
+  const uniqueNames = [...new Set(readNames.values())]
+  const offsetShift = new Map<string, number>()
   for (const comp of newCandidates) {
-    for (const [w, name] of weightNames) {
+    for (const name of uniqueNames) {
       const freq = new Map<number, number>()
       for (const c of comp) freq.set(c[name], (freq.get(c[name]) ?? 0) + 1)
-      let [best, bestFreq] = [0, 0]
-      for (const [v, f] of freq)
-        if (f > bestFreq || (v === 0 && f >= bestFreq))
-          [best, bestFreq] = [v, f]
+      let [best, bestFreq] = [0, freq.get(0) ?? 0]
+      for (const [v, f] of freq) if (f > bestFreq) [best, bestFreq] = [v, f]
       if (best !== 0) {
         for (const c of comp) c[name] -= best
-        w[offset] += best
-        shouldChange = true
+        offsetShift.set(name, (offsetShift.get(name) ?? 0) + best)
       }
       for (const c of comp) if (c[name] === 0) delete c[name]
     }
   }
 
-  if (!shouldChange) {
-    for (const n of topWeights.keys()) {
-      if (n.op === 'const' || n.op === 'read') continue
-      if (n.op === 'sum' && n.x.length === 2)
-        if (n.x[0].op === 'const' && n.x[1].op === 'read') continue
-        else if (n.x[1].op === 'const' && n.x[0].op === 'read') continue
-      shouldChange = true
-      break
-    }
-  }
-
+  let shouldChange = !!offsetShift.size
+  shouldChange ||= [...topWeights.keys()].some((n) => {
+    if (n.op === 'const' || n.op === 'read') return false
+    if (n.op === 'sum' && n.x.length === 2)
+      if (n.x[0].op === 'const' && n.x[1].op === 'read') return false
+      else if (n.x[1].op === 'const' && n.x[0].op === 'read') return false
+    return true
+  })
   if (!shouldChange) return
 
+  const readNodes = new Map(
+    uniqueNames.map((n) => [n, read({ [cat]: n }, undefined)])
+  )
   const weightNodes = new Map<Weight, NumNode<OP>>()
-  for (const [w, name] of weightNames) {
-    let node: NumNode<OP> = read({ [cat]: name }, undefined)
+  for (const [w, name] of readNames) {
+    let node: NumNode<OP> = readNodes.get(name)!
+    w[offset] += offsetShift.get(name) ?? 0
     if (w[offset] !== 0) node = sum(w[offset], node)
     weightNodes.set(w, node)
   }
