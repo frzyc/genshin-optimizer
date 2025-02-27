c381452affb2bf4b7c342a75aaafef46c77f062c
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-02-27 09:10:52 -0700

    Fix crash on sro

--- libs/sr/formula-ui/src/char/CharBase.tsx
+++ libs/sr/formula-ui/src/char/CharBase.tsx
@@ -7,28 +7,30 @@ import {
 import type { Tag } from '@genshin-optimizer/sr/formula'
 import { own } from '@genshin-optimizer/sr/formula'
 import { StatDisplay } from '@genshin-optimizer/sr/ui'
-export const charBaseUiSheet: TagField[] = allStatKeys.map((statKey) => {
-  if (
-    allElementalDmgMainStatKeys.includes(
-      statKey as (typeof allElementalDmgMainStatKeys)[number]
-    )
-  ) {
-    const tag = {
-      ...own.final.dmg_.tag,
-      elementalType: statKey.slice(0, -5) as ElementalTypeKey,
-    } as Tag
+export const charBaseUiSheet: TagField[] = allStatKeys
+  .filter((k) => k !== 'baseSpd')
+  .map((statKey) => {
+    if (
+      allElementalDmgMainStatKeys.includes(
+        statKey as (typeof allElementalDmgMainStatKeys)[number]
+      )
+    ) {
+      const tag = {
+        ...own.final.dmg_.tag,
+        elementalType: statKey.slice(0, -5) as ElementalTypeKey,
+      } as Tag
+      return {
+        title: <StatDisplay statKey={statKey} />,
+        fieldRef: tag,
+      } as TagField
+    }
+    if (statKey === 'crit_')
+      return {
+        fieldRef: own.common.cappedCrit_.tag,
+        title: <StatDisplay statKey={statKey} />,
+      }
     return {
-      title: <StatDisplay statKey={statKey} />,
-      fieldRef: tag,
-    } as TagField
-  }
-  if (statKey === 'crit_')
-    return {
-      fieldRef: own.common.cappedCrit_.tag,
+      fieldRef: own.final[statKey as keyof typeof own.final].tag as Tag,
       title: <StatDisplay statKey={statKey} />,
     }
-  return {
-    fieldRef: own.final[statKey as keyof typeof own.final].tag as Tag,
-    title: <StatDisplay statKey={statKey} />,
-  }
-})
+  })
