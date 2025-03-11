0ff911f6931da02acded75757688f418b595401b
frzyc <frzyc@users.noreply.github.com>
2025-03-11 16:32:54 -0400

    migrate nx to 20.5.0

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -1,6 +1,17 @@
 import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 import { defineConfig } from 'cypress'
-
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname),
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'nx run frontend:serve',
+        production: 'nx run frontend:preview',
+      },
+      ciWebServerCommand: 'nx run frontend:preview',
+      ciBaseUrl: 'http://localhost:4300',
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
 })
