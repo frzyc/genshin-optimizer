d88895fd7ef185bd617285bedaed76f2538aaaa7
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-03-12 15:58:24 -0700

    ZO: Add wengine params to stats (#2845)
    * Add wengine params to stats
    
    * Regen files
    
    * Update to handle all numbers
    
    * Regen files
    
    * Regen files

--- libs/zzz/dm/src/dm/wengine/wengine.ts
+++ libs/zzz/dm/src/dm/wengine/wengine.ts
@@ -1,4 +1,4 @@
-import { isPercentStat } from '@genshin-optimizer/common/util'
+import { isPercentStat, parseFloatBetter } from '@genshin-optimizer/common/util'
 import type {
   SpecialityKey,
   WengineKey,
@@ -45,7 +45,12 @@ export type WengineData = {
   desc: string
   desc2: string
   desc3: string
-  phase: Array<{ name: string; desc: string }>
+  phase: PhaseData[]
+}
+type PhaseData = {
+  name: string
+  desc: string
+  params: number[]
 }
 export const wengineDetailedJSONData = Object.fromEntries(
   Object.entries(WengineIdMap).map(([id, name]) => {
@@ -68,6 +73,14 @@ export const wengineDetailedJSONData = Object.fromEntries(
       phase: Object.values(raw.Talents).map(({ Name, Desc }) => ({
         name: Name,
         desc: Desc,
+        // Match (number with possible decimal portion)(% or s or word boundary) and not followed by '>' such as for color tags
+        params: [...Desc.matchAll(/(\d+\.?\d*)(?:(%)|s?\b)(?!>)/g)].map(
+          (matches) => {
+            const [_match, value, percent] = matches
+            if (percent) return parseFloatBetter(value)
+            return +value
+          }
+        ),
       })),
     }
     return [name, data] as const
