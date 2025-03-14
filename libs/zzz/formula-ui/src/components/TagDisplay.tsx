f53e34b1b75a5d0c6398d0be9364a9496242f50c
frzyc <frzyc@users.noreply.github.com>
2025-03-14 11:43:42 -0400

    ZO: minor update to formula display (#2854)
    * minor update to formula display
    
    * format
    
    * ?
    
    * remove console

--- libs/zzz/formula-ui/src/components/TagDisplay.tsx
+++ libs/zzz/formula-ui/src/components/TagDisplay.tsx
@@ -1,11 +1,49 @@
+import { iconInlineProps } from '@genshin-optimizer/common/svgicons'
 import { ColorText } from '@genshin-optimizer/common/ui'
+import type { StatKey } from '@genshin-optimizer/zzz/consts'
+import { statKeyTextMap } from '@genshin-optimizer/zzz/consts'
 import type { Tag } from '@genshin-optimizer/zzz/formula'
+import { StatIcon } from '@genshin-optimizer/zzz/svgicons'
+import { StatDisplay } from '@genshin-optimizer/zzz/ui'
 import { getVariant, tagFieldMap } from '../char'
 import { getTagLabel } from '../util'
 export function TagDisplay({ tag }: { tag: Tag }) {
   return (
     <ColorText color={getVariant(tag)}>
-      {tagFieldMap.subset(tag)[0]?.title || getTagLabel(tag)}
+      <TagStrDisplay tag={tag} />
     </ColorText>
   )
+}
+const extraHandlingStats = ['hp', 'hp_', 'atk', 'atk_', 'def', 'def_'] as const
+const isExtraHandlingStats = (
+  stat: string
+): stat is (typeof extraHandlingStats)[number] =>
+  extraHandlingStats.includes(
+    stat as 'hp' | 'hp_' | 'atk' | 'atk_' | 'def' | 'def_'
+  )
+const qtMap = {
+  initial: 'Initial',
+  combat: 'Combat',
+  final: 'Final',
+  base: 'Base',
+}
+function TagStrDisplay({ tag }: { tag: Tag }) {
+  const title = tagFieldMap.subset(tag)[0]?.title
+  if (title) return title
+
+  const label = getTagLabel(tag)
+
+  if (isExtraHandlingStats(label))
+    return (
+      <span>
+        <StatIcon statKey={label} iconProps={iconInlineProps} />{' '}
+        <span>
+          {(tag.qt && qtMap[tag.qt as keyof typeof qtMap]) ?? tag.qt}{' '}
+          {statKeyTextMap[label]}
+          {tag.sheet && tag.sheet !== 'agg' ? ` (${tag.sheet})` : ''}
+        </span>
+      </span>
+    )
+
+  return <StatDisplay statKey={label as StatKey} />
 }
