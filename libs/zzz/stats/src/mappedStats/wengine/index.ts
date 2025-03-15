75894165377c6a47175cee5abc01b8dc9cc6b405
frzyc <frzyc@users.noreply.github.com>
2025-03-15 13:39:40 -0400

    gen-file

---
+++ libs/zzz/stats/src/mappedStats/wengine/index.ts
@@ -0,0 +1,127 @@
+// WARNING: Generated file, do not modify
+import BashfulDemon from './maps/BashfulDemon'
+import BigCylinder from './maps/BigCylinder'
+import BlazingLaurel from './maps/BlazingLaurel'
+import BoxCutter from './maps/BoxCutter'
+import BunnyBand from './maps/BunnyBand'
+import CannonRotor from './maps/CannonRotor'
+import DeepSeaVisitor from './maps/DeepSeaVisitor'
+import DemaraBatteryMarkII from './maps/DemaraBatteryMarkII'
+import DrillRigRedAxis from './maps/DrillRigRedAxis'
+import ElectroLipGloss from './maps/ElectroLipGloss'
+import ElegantVanity from './maps/ElegantVanity'
+import FlamemakerShaker from './maps/FlamemakerShaker'
+import FusionCompiler from './maps/FusionCompiler'
+import GildedBlossom from './maps/GildedBlossom'
+import HailstormShrine from './maps/HailstormShrine'
+import HeartstringNocturne from './maps/HeartstringNocturne'
+import HellfireGears from './maps/HellfireGears'
+import Housekeeper from './maps/Housekeeper'
+import IceJadeTeapot from './maps/IceJadeTeapot'
+import IdentityBase from './maps/IdentityBase'
+import IdentityInflection from './maps/IdentityInflection'
+import KaboomTheCannon from './maps/KaboomTheCannon'
+import LunarDecrescent from './maps/LunarDecrescent'
+import LunarNoviluna from './maps/LunarNoviluna'
+import LunarPleniluna from './maps/LunarPleniluna'
+import MagneticStormAlpha from './maps/MagneticStormAlpha'
+import MagneticStormBravo from './maps/MagneticStormBravo'
+import MagneticStormCharlie from './maps/MagneticStormCharlie'
+import MarcatoDesire from './maps/MarcatoDesire'
+import OriginalTransmorpher from './maps/OriginalTransmorpher'
+import PeacekeeperSpecialized from './maps/PeacekeeperSpecialized'
+import PreciousFossilizedCore from './maps/PreciousFossilizedCore'
+import RainforestGourmet from './maps/RainforestGourmet'
+import ReverbMarkI from './maps/ReverbMarkI'
+import ReverbMarkII from './maps/ReverbMarkII'
+import ReverbMarkIII from './maps/ReverbMarkIII'
+import RiotSuppressorMarkVI from './maps/RiotSuppressorMarkVI'
+import RoaringRide from './maps/RoaringRide'
+import SeveredInnocence from './maps/SeveredInnocence'
+import SharpenedStinger from './maps/SharpenedStinger'
+import SixShooter from './maps/SixShooter'
+import SliceOfTime from './maps/SliceOfTime'
+import SpectralGaze from './maps/SpectralGaze'
+import SpringEmbrace from './maps/SpringEmbrace'
+import StarlightEngine from './maps/StarlightEngine'
+import StarlightEngineReplica from './maps/StarlightEngineReplica'
+import SteamOven from './maps/SteamOven'
+import SteelCushion from './maps/SteelCushion'
+import StreetSuperstar from './maps/StreetSuperstar'
+import TheBrimstone from './maps/TheBrimstone'
+import TheRestrained from './maps/TheRestrained'
+import TheVault from './maps/TheVault'
+import Timeweaver from './maps/Timeweaver'
+import TusksOfFury from './maps/TusksOfFury'
+import UnfetteredGameBall from './maps/UnfetteredGameBall'
+import VortexArrow from './maps/VortexArrow'
+import VortexHatchet from './maps/VortexHatchet'
+import VortexRevolver from './maps/VortexRevolver'
+import WeepingCradle from './maps/WeepingCradle'
+import WeepingGemini from './maps/WeepingGemini'
+import ZanshinHerbCase from './maps/ZanshinHerbCase'
+
+const maps = {
+  BashfulDemon,
+  BigCylinder,
+  BlazingLaurel,
+  BoxCutter,
+  BunnyBand,
+  CannonRotor,
+  DeepSeaVisitor,
+  DemaraBatteryMarkII,
+  DrillRigRedAxis,
+  ElectroLipGloss,
+  ElegantVanity,
+  FlamemakerShaker,
+  FusionCompiler,
+  GildedBlossom,
+  HailstormShrine,
+  HeartstringNocturne,
+  HellfireGears,
+  Housekeeper,
+  IceJadeTeapot,
+  IdentityBase,
+  IdentityInflection,
+  KaboomTheCannon,
+  LunarDecrescent,
+  LunarNoviluna,
+  LunarPleniluna,
+  MagneticStormAlpha,
+  MagneticStormBravo,
+  MagneticStormCharlie,
+  MarcatoDesire,
+  OriginalTransmorpher,
+  PeacekeeperSpecialized,
+  PreciousFossilizedCore,
+  RainforestGourmet,
+  ReverbMarkI,
+  ReverbMarkII,
+  ReverbMarkIII,
+  RiotSuppressorMarkVI,
+  RoaringRide,
+  SeveredInnocence,
+  SharpenedStinger,
+  SixShooter,
+  SliceOfTime,
+  SpectralGaze,
+  SpringEmbrace,
+  StarlightEngine,
+  StarlightEngineReplica,
+  SteamOven,
+  SteelCushion,
+  StreetSuperstar,
+  TheBrimstone,
+  TheRestrained,
+  TheVault,
+  Timeweaver,
+  TusksOfFury,
+  UnfetteredGameBall,
+  VortexArrow,
+  VortexHatchet,
+  VortexRevolver,
+  WeepingCradle,
+  WeepingGemini,
+  ZanshinHerbCase,
+}
+export default maps
---
+++ libs/zzz/stats/src/mappedStats/wengine/maps/TusksOfFury.ts
@@ -0,0 +1,20 @@
+import type { WengineKey } from '@genshin-optimizer/zzz/consts'
+import { allStats } from '../../../allStats'
+
+const key: WengineKey = 'TusksOfFury'
+const data_gen = allStats.wengine[key]
+
+// TODO: Load scalings
+const dm = {
+  cond_dmg_: [
+    -1,
+    ...data_gen.phase.map(({ params }) => params[0] ?? 0),
+  ] as number[],
+  passive_atk: [
+    -1,
+    ...data_gen.phase.map(({ params }) => params[1] ?? 0),
+  ] as number[],
+  duration: data_gen.phase[0].params[2],
+} as const
+
+export default dm
