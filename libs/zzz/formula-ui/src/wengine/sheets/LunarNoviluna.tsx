7e3f87e142520051a5dfeee962c3a9cf5a069b03
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2025-03-15 22:33:44 +0200

    Generate wengine UI sheets

---
+++ libs/zzz/formula-ui/src/wengine/sheets/LunarNoviluna.tsx
@@ -0,0 +1,43 @@
+import type { UISheetElement } from '@genshin-optimizer/game-opt/sheet-ui'
+import { wengineAsset } from '@genshin-optimizer/zzz/assets'
+import type { WengineKey } from '@genshin-optimizer/zzz/consts'
+import { buffs, conditionals } from '@genshin-optimizer/zzz/formula'
+import { mappedStats } from '@genshin-optimizer/zzz/stats'
+import { trans } from '../../util'
+import { PhaseWrapper } from '../components'
+
+const key: WengineKey = 'LunarNoviluna'
+const [chg, _ch] = trans('wengine', key)
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const dm = mappedStats.wengine[key]
+const icon = wengineAsset(key, 'icon')
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const cond = conditionals[key]
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const buff = buffs[key]
+
+const sheet: UISheetElement = {
+  title: chg('passive.name'),
+  img: icon,
+  documents: [
+    {
+      type: 'text',
+      text: (
+        <PhaseWrapper wKey={key}>
+          {(phase) => chg(`phaseDescs.${phase - 1}`)}
+        </PhaseWrapper>
+      ),
+    },
+  ],
+}
+
+export default sheet
---
+++ libs/zzz/formula-ui/src/wengine/sheets/index.ts
@@ -0,0 +1,129 @@
+// WARNING: Generated file, do not modify
+import type { UISheetElement } from '@genshin-optimizer/game-opt/sheet-ui'
+import type { WengineKey } from '@genshin-optimizer/zzz/consts'
+
+import BashfulDemon from './BashfulDemon'
+import BigCylinder from './BigCylinder'
+import BlazingLaurel from './BlazingLaurel'
+import BoxCutter from './BoxCutter'
+import BunnyBand from './BunnyBand'
+import CannonRotor from './CannonRotor'
+import DeepSeaVisitor from './DeepSeaVisitor'
+import DemaraBatteryMarkII from './DemaraBatteryMarkII'
+import DrillRigRedAxis from './DrillRigRedAxis'
+import ElectroLipGloss from './ElectroLipGloss'
+import ElegantVanity from './ElegantVanity'
+import FlamemakerShaker from './FlamemakerShaker'
+import FusionCompiler from './FusionCompiler'
+import GildedBlossom from './GildedBlossom'
+import HailstormShrine from './HailstormShrine'
+import HeartstringNocturne from './HeartstringNocturne'
+import HellfireGears from './HellfireGears'
+import Housekeeper from './Housekeeper'
+import IceJadeTeapot from './IceJadeTeapot'
+import IdentityBase from './IdentityBase'
+import IdentityInflection from './IdentityInflection'
+import KaboomTheCannon from './KaboomTheCannon'
+import LunarDecrescent from './LunarDecrescent'
+import LunarNoviluna from './LunarNoviluna'
+import LunarPleniluna from './LunarPleniluna'
+import MagneticStormAlpha from './MagneticStormAlpha'
+import MagneticStormBravo from './MagneticStormBravo'
+import MagneticStormCharlie from './MagneticStormCharlie'
+import MarcatoDesire from './MarcatoDesire'
+import OriginalTransmorpher from './OriginalTransmorpher'
+import PeacekeeperSpecialized from './PeacekeeperSpecialized'
+import PreciousFossilizedCore from './PreciousFossilizedCore'
+import RainforestGourmet from './RainforestGourmet'
+import ReverbMarkI from './ReverbMarkI'
+import ReverbMarkII from './ReverbMarkII'
+import ReverbMarkIII from './ReverbMarkIII'
+import RiotSuppressorMarkVI from './RiotSuppressorMarkVI'
+import RoaringRide from './RoaringRide'
+import SeveredInnocence from './SeveredInnocence'
+import SharpenedStinger from './SharpenedStinger'
+import SixShooter from './SixShooter'
+import SliceOfTime from './SliceOfTime'
+import SpectralGaze from './SpectralGaze'
+import SpringEmbrace from './SpringEmbrace'
+import StarlightEngine from './StarlightEngine'
+import StarlightEngineReplica from './StarlightEngineReplica'
+import SteamOven from './SteamOven'
+import SteelCushion from './SteelCushion'
+import StreetSuperstar from './StreetSuperstar'
+import TheBrimstone from './TheBrimstone'
+import TheRestrained from './TheRestrained'
+import TheVault from './TheVault'
+import Timeweaver from './Timeweaver'
+import TusksOfFury from './TusksOfFury'
+import UnfetteredGameBall from './UnfetteredGameBall'
+import VortexArrow from './VortexArrow'
+import VortexHatchet from './VortexHatchet'
+import VortexRevolver from './VortexRevolver'
+import WeepingCradle from './WeepingCradle'
+import WeepingGemini from './WeepingGemini'
+import ZanshinHerbCase from './ZanshinHerbCase'
+
+export const wengineUiSheets: Record<WengineKey, UISheetElement> = {
+  BashfulDemon,
+  BigCylinder,
+  BlazingLaurel,
+  BoxCutter,
+  BunnyBand,
+  CannonRotor,
+  DeepSeaVisitor,
+  DemaraBatteryMarkII,
+  DrillRigRedAxis,
+  ElectroLipGloss,
+  ElegantVanity,
+  FlamemakerShaker,
+  FusionCompiler,
+  GildedBlossom,
+  HailstormShrine,
+  HeartstringNocturne,
+  HellfireGears,
+  Housekeeper,
+  IceJadeTeapot,
+  IdentityBase,
+  IdentityInflection,
+  KaboomTheCannon,
+  LunarDecrescent,
+  LunarNoviluna,
+  LunarPleniluna,
+  MagneticStormAlpha,
+  MagneticStormBravo,
+  MagneticStormCharlie,
+  MarcatoDesire,
+  OriginalTransmorpher,
+  PeacekeeperSpecialized,
+  PreciousFossilizedCore,
+  RainforestGourmet,
+  ReverbMarkI,
+  ReverbMarkII,
+  ReverbMarkIII,
+  RiotSuppressorMarkVI,
+  RoaringRide,
+  SeveredInnocence,
+  SharpenedStinger,
+  SixShooter,
+  SliceOfTime,
+  SpectralGaze,
+  SpringEmbrace,
+  StarlightEngine,
+  StarlightEngineReplica,
+  SteamOven,
+  SteelCushion,
+  StreetSuperstar,
+  TheBrimstone,
+  TheRestrained,
+  TheVault,
+  Timeweaver,
+  TusksOfFury,
+  UnfetteredGameBall,
+  VortexArrow,
+  VortexHatchet,
+  VortexRevolver,
+  WeepingCradle,
+  WeepingGemini,
+  ZanshinHerbCase,
+}
