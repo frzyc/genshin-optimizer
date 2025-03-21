b8771ceff19951a1fd65e1efa371483381b01634
frzyc <frzyc@users.noreply.github.com>
2025-03-21 00:24:00 -0400

    ZO: minor UI updates

--- libs/zzz/page-optimize-pando/src/Optimize/EquippedGrid.tsx
+++ libs/zzz/page-optimize-pando/src/Optimize/EquippedGrid.tsx
@@ -2,7 +2,7 @@
 import { CharacterContext } from '@genshin-optimizer/zzz/db-ui'
 import { EquipGrid } from '@genshin-optimizer/zzz/ui'
 import { useContext } from 'react'
-
+const columns = { xs: 2, sm: 1, md: 2, lg: 3, xl: 4 } as const
 export function EquippedGrid({ onClick }: { onClick?: () => void }) {
   const character = useContext(CharacterContext)
 
@@ -11,6 +11,7 @@ export function EquippedGrid({ onClick }: { onClick?: () => void }) {
       discIds={character?.equippedDiscs}
       wengineId={character?.equippedWengine}
       onClick={onClick}
+      columns={columns}
     />
   )
 }
