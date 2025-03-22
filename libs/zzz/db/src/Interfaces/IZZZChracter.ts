21c9e2ac44ada17b2cddf6d9c581a4dbdf7ff515
frzyc <frzyc@users.noreply.github.com>
2025-03-22 17:59:34 -0400

    remove M3 libs

--- libs/zzz/db/src/Interfaces/IZZZChracter.ts
+++ libs/zzz/db/src/Interfaces/IZZZChracter.ts
@@ -1,39 +1,11 @@
-import type {
-  CondKey,
-  DiscMainStatKey,
-  DiscSetKey,
-  DiscSlotKey,
-  FormulaKey,
-  WengineKey,
-} from '@genshin-optimizer/zzz/consts'
+import type { DiscSlotKey } from '@genshin-optimizer/zzz/consts'
 import type { ICharacter } from '@genshin-optimizer/zzz/zood'
-
-export type Constraints = Record<string, { value: number; isMax: boolean }>
-export type Stats = Record<string, number>
 
 export interface ICharMeta {
   description: string
 }
 
-export interface IDbCharacter extends ICharacter {
-  stats: Stats
-  formulaKey?: FormulaKey
-  constraints: Constraints
-  useEquipped: boolean
-  setFilter2: DiscSetKey[]
-  setFilter4: DiscSetKey[]
-  slot4: DiscMainStatKey[]
-  slot5: DiscMainStatKey[]
-  slot6: DiscMainStatKey[]
-  wengineKey: WengineKey
-  wengineLvl: number
-  wenginePhase: number
-  conditionals: Partial<Record<CondKey, number>>
-  levelLow: number
-  levelHigh: number
-}
-
-export interface ICachedCharacter extends IDbCharacter {
+export interface ICachedCharacter extends ICharacter {
   equippedDiscs: Record<DiscSlotKey, string | undefined>
   equippedWengine?: string
 }
--- libs/zzz/solver/README.md
+++
@@ -1,7 +0,0 @@
-# zzz-solver
-
-This library was generated with [Nx](https://nx.dev).
-
-## Running unit tests
-
-Run `nx test zzz-solver` to execute the unit tests via [Jest](https://jestjs.io).
