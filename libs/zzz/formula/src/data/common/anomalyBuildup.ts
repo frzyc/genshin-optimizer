8a74b30169d33341f94e5fea135e6c90e350a6a7
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2025-03-24 19:49:43 +0200

    fancy formulas

--- libs/zzz/formula/src/data/common/anomalyBuildup.ts
+++ libs/zzz/formula/src/data/common/anomalyBuildup.ts
@@ -3,13 +3,24 @@ import type { TagMapNodeEntries } from '../util'
 import { enemy, own, ownBuff, percent } from '../util'
 
 const data: TagMapNodeEntries = [
-  // Anomaly buildup
-  ownBuff.formula.anomalyBuildup.add(
+  // AM Bonus
+  ownBuff.formula.anomMas_mult_.add(prod(own.final.anomMas, 0.01)),
+  // Anomaly Buildup Bonus Multiplier
+  ownBuff.formula.anomBuildup_mult_.add(
+    sum(percent(1), own.final.anomBuildup_)
+  ),
+  // Enemy Anomaly Buildup RES Multiplier
+  ownBuff.formula.enemyAnomBuildupRes_mult_.add(
+    sum(percent(1), prod(-1, enemy.common.anomBuildupRes_))
+  ),
+
+  // Anomaly Buildup Formula
+  ownBuff.formula.anomBuildup.add(
     prod(
       own.formula.base,
-      prod(own.final.anomMas, 0.01),
-      sum(percent(1), own.final.anomBuildup_),
-      sum(percent(1), prod(-1, enemy.common.anomBuildupRes_))
+      own.formula.anomMas_mult_,
+      own.formula.anomBuildup_mult_,
+      ownBuff.formula.enemyAnomBuildupRes_mult_
     )
   ),
 ]
