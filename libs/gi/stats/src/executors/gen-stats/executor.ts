9386ed9d51ede5baad2e8c33a45befd80a1685fc
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-03-27 15:45:16 -0700

    Update formatting again

--- libs/gi/stats/src/executors/gen-stats/executor.ts
+++ libs/gi/stats/src/executors/gen-stats/executor.ts
@@ -1,4 +1,4 @@
-import { dumpFile } from '@genshin-optimizer/common/pipeline'
+import { dumpPrettyFile } from '@genshin-optimizer/common/pipeline'
 import {
   artifactMainstatData,
   artifactSubstatData,
@@ -52,42 +52,45 @@ export type AllStats = typeof allStat
 export default async function runExecutor(_options: GenStatsExecutorSchema) {
   console.log(`Writing basic character data to ${path}/Characters`)
   Object.entries(characterDataDump).forEach(([characterKey, data]) =>
-    dumpFile(`${path}/Characters/${characterKey}/data.json`, data)
+    dumpPrettyFile(`${path}/Characters/${characterKey}/data.json`, data)
   )
 
-  // dumpFile(`${__dirname}/allChar_gen.json`, characterSkillParamDump)
+  // dumpPrettyFile(`${__dirname}/allChar_gen.json`, characterSkillParamDump)
   console.log(`Writing weapon data to ${path}/Weapons`)
   Object.entries(weaponDataDump).forEach(([weaponKey, data]) =>
-    dumpFile(
+    dumpPrettyFile(
       `${path}/Weapons/${
         data.weaponType[0].toUpperCase() + data.weaponType.slice(1)
       }/${weaponKey}/data.json`,
       data
     )
   )
-  dumpFile(`${path}/Weapons/expCurve.json`, weaponExpCurve)
+  dumpPrettyFile(`${path}/Weapons/expCurve.json`, weaponExpCurve)
 
   console.log(`Writing character data to ${path}/Characters`)
   Object.entries(characterSkillParamDump).forEach(([characterKey, data]) =>
-    dumpFile(`${path}/Characters/${characterKey}/skillParam.json`, data)
+    dumpPrettyFile(`${path}/Characters/${characterKey}/skillParam.json`, data)
   )
-  dumpFile(`${path}/Characters/expCurve.json`, charExpCurve)
+  dumpPrettyFile(`${path}/Characters/expCurve.json`, charExpCurve)
 
   console.log(`Writing artifact data to ${path}/Artifacts`)
-  dumpFile(`${path}/Artifacts/artifact_set.json`, artifactDataDump)
-  dumpFile(`${path}/Artifacts/artifact_sub.json`, artifactSubstatData)
-  dumpFile(`${path}/Artifacts/artifact_main.json`, artifactMainstatData)
-  dumpFile(`${path}/Artifacts/artifact_sub_rolls.json`, artifactSubstatRollData)
-  dumpFile(
+  dumpPrettyFile(`${path}/Artifacts/artifact_set.json`, artifactDataDump)
+  dumpPrettyFile(`${path}/Artifacts/artifact_sub.json`, artifactSubstatData)
+  dumpPrettyFile(`${path}/Artifacts/artifact_main.json`, artifactMainstatData)
+  dumpPrettyFile(
+    `${path}/Artifacts/artifact_sub_rolls.json`,
+    artifactSubstatRollData
+  )
+  dumpPrettyFile(
     `${path}/Artifacts/artifact_sub_rolls_correction.json`,
     artifactSubstatRollCorrection
   )
 
   console.log(`Writing material data to ${path}/Materials`)
-  dumpFile(`${path}/Materials/material.json`, materialDataDump)
+  dumpPrettyFile(`${path}/Materials/material.json`, materialDataDump)
 
   console.log(`Writing combined data to ${proj_path}/src/allStats_gen.json`)
-  dumpFile(`${proj_path}/src/allStat_gen.json`, allStat)
+  dumpPrettyFile(`${proj_path}/src/allStat_gen.json`, allStat)
 
   return { success: true }
 }
