c2c8e6b49f4be251dd67522fd64bb2e29243b315
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-03-27 16:00:25 -0700

    Format stats

--- libs/gi/stats/src/executors/gen-stats/executor.ts
+++ libs/gi/stats/src/executors/gen-stats/executor.ts
@@ -50,47 +50,70 @@ const allStat = {
 export type AllStats = typeof allStat
 
 export default async function runExecutor(_options: GenStatsExecutorSchema) {
+  const promises = []
   console.log(`Writing basic character data to ${path}/Characters`)
   Object.entries(characterDataDump).forEach(([characterKey, data]) =>
-    dumpPrettyFile(`${path}/Characters/${characterKey}/data.json`, data)
+    promises.push(
+      dumpPrettyFile(`${path}/Characters/${characterKey}/data.json`, data)
+    )
   )
 
   // dumpPrettyFile(`${__dirname}/allChar_gen.json`, characterSkillParamDump)
   console.log(`Writing weapon data to ${path}/Weapons`)
   Object.entries(weaponDataDump).forEach(([weaponKey, data]) =>
-    dumpPrettyFile(
-      `${path}/Weapons/${
-        data.weaponType[0].toUpperCase() + data.weaponType.slice(1)
-      }/${weaponKey}/data.json`,
-      data
+    promises.push(
+      dumpPrettyFile(
+        `${path}/Weapons/${
+          data.weaponType[0].toUpperCase() + data.weaponType.slice(1)
+        }/${weaponKey}/data.json`,
+        data
+      )
     )
   )
-  dumpPrettyFile(`${path}/Weapons/expCurve.json`, weaponExpCurve)
+  promises.push(dumpPrettyFile(`${path}/Weapons/expCurve.json`, weaponExpCurve))
 
   console.log(`Writing character data to ${path}/Characters`)
   Object.entries(characterSkillParamDump).forEach(([characterKey, data]) =>
-    dumpPrettyFile(`${path}/Characters/${characterKey}/skillParam.json`, data)
+    promises.push(
+      dumpPrettyFile(`${path}/Characters/${characterKey}/skillParam.json`, data)
+    )
   )
-  dumpPrettyFile(`${path}/Characters/expCurve.json`, charExpCurve)
+  promises.push(
+    dumpPrettyFile(`${path}/Characters/expCurve.json`, charExpCurve)
+  )
 
   console.log(`Writing artifact data to ${path}/Artifacts`)
-  dumpPrettyFile(`${path}/Artifacts/artifact_set.json`, artifactDataDump)
-  dumpPrettyFile(`${path}/Artifacts/artifact_sub.json`, artifactSubstatData)
-  dumpPrettyFile(`${path}/Artifacts/artifact_main.json`, artifactMainstatData)
-  dumpPrettyFile(
-    `${path}/Artifacts/artifact_sub_rolls.json`,
-    artifactSubstatRollData
+  promises.push(
+    dumpPrettyFile(`${path}/Artifacts/artifact_set.json`, artifactDataDump)
   )
-  dumpPrettyFile(
-    `${path}/Artifacts/artifact_sub_rolls_correction.json`,
-    artifactSubstatRollCorrection
+  promises.push(
+    dumpPrettyFile(`${path}/Artifacts/artifact_sub.json`, artifactSubstatData)
+  )
+  promises.push(
+    dumpPrettyFile(`${path}/Artifacts/artifact_main.json`, artifactMainstatData)
+  )
+  promises.push(
+    dumpPrettyFile(
+      `${path}/Artifacts/artifact_sub_rolls.json`,
+      artifactSubstatRollData
+    )
+  )
+  promises.push(
+    dumpPrettyFile(
+      `${path}/Artifacts/artifact_sub_rolls_correction.json`,
+      artifactSubstatRollCorrection
+    )
   )
 
   console.log(`Writing material data to ${path}/Materials`)
-  dumpPrettyFile(`${path}/Materials/material.json`, materialDataDump)
+  promises.push(
+    dumpPrettyFile(`${path}/Materials/material.json`, materialDataDump)
+  )
 
   console.log(`Writing combined data to ${proj_path}/src/allStats_gen.json`)
-  dumpPrettyFile(`${proj_path}/src/allStat_gen.json`, allStat)
+  promises.push(dumpPrettyFile(`${proj_path}/src/allStat_gen.json`, allStat))
+
+  await Promise.all(promises)
 
   return { success: true }
 }
