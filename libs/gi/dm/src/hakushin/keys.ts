7dc915bc03dbdf82222862bfd1772a0e1018befa
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-03-27 13:50:15 -0700

    Remove 5.5 keys

--- libs/gi/dm/src/hakushin/keys.ts
+++ libs/gi/dm/src/hakushin/keys.ts
@@ -1,7 +1,4 @@
 // Keys that should have data fetched from Hakushin instead of regular DM
-export const hakushinChars = ['Iansan', 'Varesa'] as const
-export const hakushinArtis = [
-  'FinaleOfTheDeepGalleries',
-  'LongNightsOath',
-] as const
-export const hakushinWeapons = ['VividNotions'] as const
+export const hakushinChars = [] as const
+export const hakushinArtis = [] as const
+export const hakushinWeapons = [] as const
