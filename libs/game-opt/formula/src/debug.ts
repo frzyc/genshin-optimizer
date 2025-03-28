d7498441ee2e5fd2ceaade34345c8b91631ada2c
lantua <16190491+lantua@users.noreply.github.com>
2025-03-28 16:41:41 -0400

    Pando: Update `DebugCalculator` (#2900)
    - Update `DebugCalculator` output format
    - Update `Read.ex` in `computeMeta`
    - Add `debugMeta` to `solver`
    - Update `arithmetic` signature
    - Update `branching` signature
    - Add tests for #2886
    - Remove debug print

--- libs/game-opt/formula/src/debug.ts
+++ libs/game-opt/formula/src/debug.ts
@@ -1,18 +1,13 @@
 import type { DebugMeta } from '@genshin-optimizer/pando/engine'
 
 export function createFilterDebug(equipmentKeys: string[]) {
-  return (debug: DebugMeta) => !debugIsUnequippedEquipment(debug, equipmentKeys)
+  const sheets = new Set(equipmentKeys)
+  return (meta: DebugMeta): boolean => !useCounter(meta, sheets)
 }
 
-function debugIsUnequippedEquipment(debug: DebugMeta, equipmentKeys: string[]) {
-  if (!debug.formula || !debug.deps || debug.deps.length < 1) return false
-  const f = debug.formula
-  const disabledBuffForEquip =
-    f.includes('[0]') &&
-    f.includes('common.count') &&
-    equipmentKeys.some((key) => f.includes(key))
-  const depGathered0ForCount =
-    debug.deps[0].formula?.includes('gather 0 node') &&
-    debug.deps[0].formula?.includes('common.count')
-  return disabledBuffForEquip && depGathered0ForCount
+function useCounter(meta: DebugMeta, sheets: Set<string>): boolean {
+  const counter = meta.formula.match(/(\S+) (\S+) common.count/)
+  if (!counter) return false
+  const [, sheet, et] = counter
+  return et === 'own' && sheets.has(sheet)
 }
