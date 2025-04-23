d7db01107e5d188957bd6bdcd233843c769dda15
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-04-23 14:04:05 -0700

    Add faction image

--- libs/zzz/assets/src/common/faction/index.ts
+++ libs/zzz/assets/src/common/faction/index.ts
@@ -1,6 +1,7 @@
 import BelebogHeavyIndustries from './IconCampBelobogIndustries.png'
 import CunningHares from './IconCampGentleHouse.png'
 import HollowSpecialOoperationsSection6 from './IconCampH.S.O-S6.png'
+import Mockingbird from './IconCampMockingBird.png'
 import CriminalInvestigationSpecialResponseTeam from './IconCampN.E.P.S..png'
 import NewEriduDefenseForce from './IconCampObols.png'
 import RandomPlay from './IconCampRandomPlay.png'
@@ -11,12 +12,13 @@ import VictoriaHousekeepingCo from './IconCampVictoriaHousekeepingCo.png'
 const data = {
   BelebogHeavyIndustries,
   CunningHares,
-  HollowSpecialOoperationsSection6,
   CriminalInvestigationSpecialResponseTeam,
+  HollowSpecialOoperationsSection6,
+  Mockingbird,
   NewEriduDefenseForce,
+  RandomPlay,
   SonsOfCalydon,
   StarsOfLyra,
   VictoriaHousekeepingCo,
-  RandomPlay,
 } as const
 export default data
