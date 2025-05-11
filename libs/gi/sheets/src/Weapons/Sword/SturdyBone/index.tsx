d8922237d786954ea1c4d5af71ee3cfbc07e7dee
Failchon <slay3r12@gmail.com>
2025-05-11 11:44:59 +0300

    Moved equal func to same variable

--- libs/gi/sheets/src/Weapons/Sword/SturdyBone/index.tsx
+++ libs/gi/sheets/src/Weapons/Sword/SturdyBone/index.tsx
@@ -11,15 +11,17 @@ const normal_dmgInc_arr = [-1, 0.16, 0.2, 0.24, 0.28, 0.32]
 
 const [condAfterSprintPath, condAfterSprint] = cond(key, 'afterSprint')
 const normal_dmgInc = equal(
-  condAfterSprint,
-  'on',
-  prod(
-    subscript(input.weapon.refinement, normal_dmgInc_arr, { unit: '%' }),
-    input.total.atk
+  input.weapon.key,
+  key,
+  equal(
+    condAfterSprint,
+    'on',
+    prod(
+      subscript(input.weapon.refinement, normal_dmgInc_arr, { unit: '%' }),
+      input.total.atk
+    )
   )
 )
-
-const dmg = equal(input.weapon.key, key, normal_dmgInc)
 
 const data = dataObjForWeaponSheet(
   key,
@@ -28,7 +30,7 @@ const data = dataObjForWeaponSheet(
       normal_dmgInc,
     },
   },
-  { dmg }
+  { normal_dmgInc }
 )
 const sheet: IWeaponSheet = {
   document: [
