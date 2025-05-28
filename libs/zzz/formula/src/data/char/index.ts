502726ec2874ae0753bd3abdf60cd8f5ea282e09
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-05-28 11:04:51 -0700

    Hide full sheets behind dev flag

--- libs/zzz/formula/src/data/char/index.ts
+++ libs/zzz/formula/src/data/char/index.ts
@@ -1,3 +1,4 @@
+import { shouldShowDevComponents } from '@genshin-optimizer/common/util'
 import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
 import { allCharacterKeys } from '@genshin-optimizer/zzz/consts'
 import { allStats } from '@genshin-optimizer/zzz/stats'
@@ -7,10 +8,14 @@ import { entriesForChar } from './util'
 
 const sheetedChars: CharacterKey[] = ['Anby']
 
-const data: TagMapNodeEntries[] = [
-  ...allCharacterKeys
-    .filter((k) => !sheetedChars.includes(k))
-    .map((key) => register(key, entriesForChar(allStats.char[key]))),
-  Anby,
-]
+const data: TagMapNodeEntries[] = shouldShowDevComponents
+  ? allCharacterKeys.map((key) =>
+      register(key, entriesForChar(allStats.char[key]))
+    )
+  : [
+      ...allCharacterKeys
+        .filter((k) => !sheetedChars.includes(k))
+        .map((key) => register(key, entriesForChar(allStats.char[key]))),
+      Anby,
+    ]
 export default data.flat()
