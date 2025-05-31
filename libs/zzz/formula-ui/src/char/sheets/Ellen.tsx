d6ea4ec9f5ae873f37b37fd234245d6dbeba126a
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-05-31 08:51:35 -0700

    Generate char sheets

---
+++ libs/zzz/formula-ui/src/char/sheets/Ellen.tsx
@@ -0,0 +1,25 @@
+import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
+import { buffs, conditionals } from '@genshin-optimizer/zzz/formula'
+import { trans } from '../../util'
+import { createBaseSheet } from '../sheetUtil'
+
+const key: CharacterKey = 'Ellen'
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const [, ch] = trans('char', key)
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const cond = conditionals[key]
+// TODO: Cleanup
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const buff = buffs[key]
+
+const sheet = createBaseSheet(key)
+
+export default sheet
---
+++ libs/zzz/formula-ui/src/char/sheets/index.ts
@@ -0,0 +1,72 @@
+// WARNING: Generated file, do not modify
+import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
+import type { CharUISheet } from '../consts'
+import Anby from './Anby'
+import Anton from './Anton'
+import Astra from './Astra'
+import Ben from './Ben'
+import Billy from './Billy'
+import Burnice from './Burnice'
+import Caesar from './Caesar'
+import Corin from './Corin'
+import Ellen from './Ellen'
+import Evelyn from './Evelyn'
+import Grace from './Grace'
+import Harumasa from './Harumasa'
+import Hugo from './Hugo'
+import Jane from './Jane'
+import Koleda from './Koleda'
+import Lighter from './Lighter'
+import Lucy from './Lucy'
+import Lycaon from './Lycaon'
+import Miyabi from './Miyabi'
+import Nekomata from './Nekomata'
+import Nicole from './Nicole'
+import Piper from './Piper'
+import Pulchra from './Pulchra'
+import QingYi from './QingYi'
+import Rina from './Rina'
+import Seth from './Seth'
+import Soldier0Anby from './Soldier0Anby'
+import Soldier11 from './Soldier11'
+import Soukaku from './Soukaku'
+import Trigger from './Trigger'
+import Vivian from './Vivian'
+import Yanagi from './Yanagi'
+import ZhuYuan from './ZhuYuan'
+
+export const charSheets: Record<CharacterKey, CharUISheet> = {
+  Anby,
+  Anton,
+  Astra,
+  Ben,
+  Billy,
+  Burnice,
+  Caesar,
+  Corin,
+  Ellen,
+  Evelyn,
+  Grace,
+  Harumasa,
+  Hugo,
+  Jane,
+  Koleda,
+  Lighter,
+  Lucy,
+  Lycaon,
+  Miyabi,
+  Nekomata,
+  Nicole,
+  Piper,
+  Pulchra,
+  QingYi,
+  Rina,
+  Seth,
+  Soldier0Anby,
+  Soldier11,
+  Soukaku,
+  Trigger,
+  Vivian,
+  Yanagi,
+  ZhuYuan,
+} as const
