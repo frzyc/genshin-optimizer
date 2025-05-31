8ec1206ef33d79fcef3d44fa22febd86057febea
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-05-31 08:48:01 -0700

    Update text extraction for characters

--- libs/zzz/dm-localization/src/executors/gen-locale/lib/util.ts
+++ libs/zzz/dm-localization/src/executors/gen-locale/lib/util.ts
@@ -1,5 +1,18 @@
 export function processText(text: string) {
-  return text
+  const processedText = text
     .replaceAll('<color=', '<ct color=')
     .replaceAll('</color>', '</ct>')
+    // Process IconMap input buttons like <IconMap:Icon_Normal> to be <IconNormal />
+    .replace(
+      /<IconMap:([a-zA-Z_]*?)>/g,
+      (_match, capture: string) => `<${capture.replaceAll('_', '')} />`
+    )
+
+  // Convert \n to real breaks
+  if (processedText.includes('\n'))
+    return Object.fromEntries(
+      processedText.split('\n').map((str, index) => [index, str])
+    )
+
+  return processedText
 }
