8b02cb4d06f07d1abd9fea0b742273bb8d53da50
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-06-02 15:40:37 -0700

    Update missing params

--- libs/zzz/stats/src/executors/gen-stats/src/characterData.ts
+++ libs/zzz/stats/src/executors/gen-stats/src/characterData.ts
@@ -57,35 +57,22 @@ export function getCharactersData(): CharactersData {
 function extractSkillParams(skills: CharacterData['skills']) {
   const skillParams = Object.fromEntries(
     Object.entries(skills).map(([key, skill]) => {
-      // Only record each skill once.
-      // Many skills show up twice, e.g. once for dmg, once for daze
-      // But we have all the dmg, daze, anom info in the params
-      const ids: Record<string, boolean> = {}
       return [
         hakushinSkillMap[key],
         Object.fromEntries(
-          skill.Description.map((desc) => [
-            nameToKey(desc.Name),
-            (desc.Param ?? []).flatMap((par) =>
-              Object.entries(par.Param ?? {})
-                .map(
-                  ([
-                    id,
-                    {
-                      DamagePercentage,
-                      DamagePercentageGrowth,
-                      StunRatio,
-                      StunRatioGrowth,
-                      SpRecovery,
-                      SpRecoveryGrowth,
-                      FeverRecovery,
-                      FeverRecoveryGrowth,
-                      AttributeInfliction,
-                    },
-                  ]) => {
-                    if (!ids[id]) {
-                      ids[id] = true
-                      return {
+          skill.Description.map((desc) => {
+            // Only record each skill once.
+            // Many skills show up twice, e.g. once for dmg, once for daze
+            // But we have all the dmg, daze, anom info in the params
+            const ids: Record<string, boolean> = {}
+            return [
+              nameToKey(desc.Name),
+              (desc.Param ?? []).flatMap((par) =>
+                Object.entries(par.Param ?? {})
+                  .map(
+                    ([
+                      id,
+                      {
                         DamagePercentage,
                         DamagePercentageGrowth,
                         StunRatio,
@@ -95,14 +82,29 @@ function extractSkillParams(skills: CharacterData['skills']) {
                         FeverRecovery,
                         FeverRecoveryGrowth,
                         AttributeInfliction,
+                      },
+                    ]) => {
+                      if (!ids[id]) {
+                        ids[id] = true
+                        return {
+                          DamagePercentage,
+                          DamagePercentageGrowth,
+                          StunRatio,
+                          StunRatioGrowth,
+                          SpRecovery,
+                          SpRecoveryGrowth,
+                          FeverRecovery,
+                          FeverRecoveryGrowth,
+                          AttributeInfliction,
+                        }
                       }
+                      return undefined
                     }
-                    return undefined
-                  }
-                )
-                .filter(notEmpty)
-            ),
-          ])
+                  )
+                  .filter(notEmpty)
+              ),
+            ]
+          })
         ),
       ]
     })
