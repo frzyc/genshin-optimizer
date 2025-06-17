0e946e29199389b4cc1277c751c17470127329e1
Adomas Volungevičius <81253282+priolette@users.noreply.github.com>
2025-06-17 17:23:05 +0300

    ZO: Fix counts overriding each other (#2985)
    * fix counts overriding each other
    
    * add a comment

--- libs/zzz/formula/src/data/util/tag.ts
+++ libs/zzz/formula/src/data/util/tag.ts
@@ -220,6 +220,12 @@ export const own = convert(ownTag, { et: 'own', dst: null })
 export const team = convert(ownTag, { et: 'team', dst: null, ...noName })
 export const target = convert(ownTag, { et: 'target', ...noName })
 export const enemy = convert(enemyTag, { et: 'enemy', dst: null, ...noName })
+// Avoid reading from two counts at once
+team.common.count = team.common.count.withTag({
+  specialty: null,
+  faction: null,
+  attribute: null,
+})
 
 // Default tag DB keys
 export const ownBuff = convert(ownTag, { et: 'own' })
