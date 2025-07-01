262edde2ef7d2fe36b2d0f186a3616b5bcd43325
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2025-07-01 21:52:43 +0300

    corin sheet

--- libs/zzz/formula/src/data/char/sheets/Corin.ts
+++ libs/zzz/formula/src/data/char/sheets/Corin.ts
@@ -1,18 +1,30 @@
-import { cmpGE } from '@genshin-optimizer/pando/engine'
+import {
+  cmpEq,
+  cmpGE,
+  prod,
+  subscript,
+  sum,
+} from '@genshin-optimizer/pando/engine'
 import { type CharacterKey } from '@genshin-optimizer/zzz/consts'
 import { allStats, mappedStats } from '@genshin-optimizer/zzz/stats'
 import {
   allBoolConditionals,
-  allListConditionals,
   allNumConditionals,
+  customDmg,
+  enemy,
   enemyDebuff,
   own,
   ownBuff,
+  percent,
   register,
   registerBuff,
-  teamBuff,
+  team,
 } from '../../util'
-import { entriesForChar, registerAllDmgDazeAndAnom } from '../util'
+import {
+  dmgDazeAndAnomOverride,
+  entriesForChar,
+  registerAllDmgDazeAndAnom,
+} from '../util'
 
 const key: CharacterKey = 'Corin'
 const data_gen = allStats.char[key]
@@ -20,10 +32,18 @@ const dm = mappedStats.char[key]
 
 const { char } = own
 
-// TODO: Add conditionals
-const { boolConditional } = allBoolConditionals(key)
-const { listConditional } = allListConditionals(key, ['val1', 'val2'])
-const { numConditional } = allNumConditionals(key, true, 0, 2)
+const { chain_ult_hit } = allBoolConditionals(key)
+const { exSpecial_chain_ult_hits } = allNumConditionals(
+  key,
+  true,
+  0,
+  dm.m2.stacks
+)
+const { charge } = allNumConditionals(key, true, 0, dm.m6.stacks)
+
+const core_common_dmg_ = ownBuff.combat.common_dmg_.add(
+  percent(subscript(char.core, dm.core.extended_slash_dmg_))
+)
 
 const sheet = register(
   key,
@@ -31,19 +51,154 @@ const sheet = register(
   entriesForChar(data_gen),
 
   // Formulas
-  ...registerAllDmgDazeAndAnom(key, dm),
+  ...registerAllDmgDazeAndAnom(
+    key,
+    dm,
+    // Per-hit buffs
+    dmgDazeAndAnomOverride(
+      dm,
+      'basic',
+      'BasicAttackWipeout',
+      2,
+      { damageType1: 'basic' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'basic',
+      'BasicAttackWipeout',
+      4,
+      { damageType1: 'basic' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'dodge',
+      'DashAttackOopsyDaisy',
+      0,
+      { damageType1: 'dash' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'dodge',
+      'DodgeCounterNope',
+      1,
+      { damageType1: 'dodgeCounter' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'special',
+      'SpecialAttackCleanSweep',
+      1,
+      { damageType1: 'special' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'special',
+      'EXSpecialAttackSkirtAlert',
+      1,
+      { damageType1: 'exSpecial' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'chain',
+      'ChainAttackSorry',
+      0,
+      { damageType1: 'chain' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'chain',
+      'UltimateVeryVerySorry',
+      0,
+      { damageType1: 'ult' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'assist',
+      'QuickAssistEmergencyMeasures',
+      1,
+      { damageType1: 'quickAssist' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'assist',
+      'AssistFollowUpQuickSweep',
+      0,
+      { damageType1: 'assistFollowUp' },
+      'atk',
+      undefined,
+      core_common_dmg_
+    )
+  ),
+
+  ...customDmg(
+    'm6_dmg',
+    { damageType1: 'elemental' },
+    cmpGE(char.mindscape, 6, prod(own.final.atk, percent(dm.m6.dmg), charge))
+  ),
 
   // Buffs
   registerBuff(
-    'm6_dmg_',
+    'core_common_dmg_',
+    core_common_dmg_,
+    undefined,
+    undefined,
+    false
+  ),
+  registerBuff(
+    'ability_common_dmg_',
     ownBuff.combat.common_dmg_.add(
-      cmpGE(char.mindscape, 6, boolConditional.ifOn(1))
+      cmpGE(
+        sum(
+          team.common.count.physical,
+          team.common.count.withFaction('VictoriaHousekeepingCo')
+        ),
+        3,
+        cmpEq(enemy.common.isStunned, 1, percent(dm.ability.common_dmg_))
+      )
     )
   ),
   registerBuff(
-    'team_dmg_',
-    teamBuff.combat.common_dmg_.add(listConditional.map({ val1: 1, val2: 2 }))
+    'm1_common_dmg_',
+    ownBuff.combat.common_dmg_.add(
+      cmpGE(char.mindscape, 1, chain_ult_hit.ifOn(percent(dm.m1.common_dmg_)))
+    )
   ),
-  registerBuff('enemy_defRed_', enemyDebuff.common.defRed_.add(numConditional))
+  registerBuff(
+    'm2_physical_resRed_',
+    enemyDebuff.common.resRed_.physical.add(
+      cmpGE(
+        char.mindscape,
+        2,
+        prod(exSpecial_chain_ult_hits, percent(dm.m2.physical_resRed_))
+      )
+    )
+  )
 )
 export default sheet
