7cd09005668eeca683f3398244a289328941b758
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2025-07-06 17:05:35 +0300

    pulchra sheet

--- libs/zzz/formula-ui/src/char/sheets/Pulchra.tsx
+++ libs/zzz/formula-ui/src/char/sheets/Pulchra.tsx
@@ -1,25 +1,68 @@
 import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
 import { buffs, conditionals } from '@genshin-optimizer/zzz/formula'
 import { trans } from '../../util'
-import { createBaseSheet } from '../sheetUtil'
+import { createBaseSheet, fieldForBuff } from '../sheetUtil'
 
 const key: CharacterKey = 'Pulchra'
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const [, ch] = trans('char', key)
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const cond = conditionals[key]
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const buff = buffs[key]
 
-const sheet = createBaseSheet(key)
+const sheet = createBaseSheet(key, {
+  core: [
+    {
+      type: 'conditional',
+      conditional: {
+        label: ch('coreCond'),
+        metadata: cond.hunters_gait,
+        fields: [fieldForBuff(buff.core_dazeInc_)],
+      },
+    },
+  ],
+  ability: [
+    {
+      type: 'conditional',
+      conditional: {
+        label: ch('abilityCond'),
+        metadata: cond.binding_trap,
+        fields: [fieldForBuff(buff.ability_aftershock_dmg_)],
+      },
+    },
+  ],
+  m1: [
+    {
+      type: 'conditional',
+      conditional: {
+        label: ch('abilityCond'),
+        metadata: cond.binding_trap,
+        fields: [fieldForBuff(buff.m1_crit_)],
+      },
+    },
+  ],
+  m2: [
+    {
+      type: 'conditional',
+      conditional: {
+        label: ch('coreCond'),
+        metadata: cond.hunters_gait,
+        fields: [fieldForBuff(buff.m2_atk_)],
+      },
+    },
+  ],
+  m6: [
+    {
+      type: 'fields',
+      fields: [fieldForBuff(buff.m6_special_dmg_)],
+    },
+    {
+      type: 'conditional',
+      conditional: {
+        label: ch('abilityCond'),
+        metadata: cond.binding_trap,
+        fields: [fieldForBuff(buff.m6_common_dmg_)],
+      },
+    },
+  ],
+})
 
 export default sheet
