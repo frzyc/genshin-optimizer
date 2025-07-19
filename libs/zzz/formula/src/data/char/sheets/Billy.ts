39bf0e081e76f4270cde063bc7444efad120afdd
Adomas Volungevičius <81253282+priolette@users.noreply.github.com>
2025-07-20 00:49:50 +0300

    ZO: Character sheets for Nekomata, Soldier 11, Corin and Billy (#3004)
    * nekomata sheet
    
    * soldier 11 sheet
    
    * corin sheet
    
    * billy sheet

--- libs/zzz/formula/src/data/char/sheets/Billy.ts
+++ libs/zzz/formula/src/data/char/sheets/Billy.ts
@@ -1,18 +1,20 @@
-import { cmpGE } from '@genshin-optimizer/pando/engine'
+import { cmpGE, prod, subscript, sum } from '@genshin-optimizer/pando/engine'
 import { type CharacterKey } from '@genshin-optimizer/zzz/consts'
 import { allStats, mappedStats } from '@genshin-optimizer/zzz/stats'
 import {
-  allBoolConditionals,
-  allListConditionals,
   allNumConditionals,
-  enemyDebuff,
   own,
   ownBuff,
+  percent,
   register,
   registerBuff,
-  teamBuff,
+  team,
 } from '../../util'
-import { entriesForChar, registerAllDmgDazeAndAnom } from '../util'
+import {
+  dmgDazeAndAnomOverride,
+  entriesForChar,
+  registerAllDmgDazeAndAnom,
+} from '../util'
 
 const key: CharacterKey = 'Billy'
 const data_gen = allStats.char[key]
@@ -20,10 +22,18 @@ const dm = mappedStats.char[key]
 
 const { char } = own
 
-// TODO: Add conditionals
-const { boolConditional } = allBoolConditionals(key)
-const { listConditional } = allListConditionals(key, ['val1', 'val2'])
-const { numConditional } = allNumConditionals(key, true, 0, 2)
+const { ult_dmg_stacks } = allNumConditionals(key, true, 0, dm.ability.stacks)
+const { distance } = allNumConditionals(
+  key,
+  true,
+  0,
+  dm.m4.exSpecial_crit_ * 100
+)
+const { m6_stacks } = allNumConditionals(key, true, 0, dm.m6.stacks)
+
+const core_dmg_ = ownBuff.combat.common_dmg_.add(
+  percent(subscript(char.core, dm.core.common_dmg_))
+)
 
 const sheet = register(
   key,
@@ -31,19 +41,67 @@ const sheet = register(
   entriesForChar(data_gen),
 
   // Formulas
-  ...registerAllDmgDazeAndAnom(key, dm),
+  ...registerAllDmgDazeAndAnom(
+    key,
+    dm,
+    // Per-hit buffs
+    dmgDazeAndAnomOverride(
+      dm,
+      'basic',
+      'BasicAttackFullFirepower',
+      2,
+      { damageType1: 'basic' },
+      'atk',
+      undefined,
+      core_dmg_
+    ),
+    dmgDazeAndAnomOverride(
+      dm,
+      'basic',
+      'BasicAttackFullFirepower',
+      3,
+      { damageType1: 'basic' },
+      'atk',
+      undefined,
+      core_dmg_
+    )
+  ),
 
   // Buffs
+  registerBuff('core_common_dmg_', core_dmg_, undefined, undefined, false),
   registerBuff(
-    'm6_dmg_',
-    ownBuff.combat.common_dmg_.add(
-      cmpGE(char.mindscape, 6, boolConditional.ifOn(1))
+    'ability_ult_dmg_',
+    ownBuff.combat.dmg_.addWithDmgType(
+      'ult',
+      cmpGE(
+        sum(
+          team.common.count.physical,
+          team.common.count.withFaction('CunningHares')
+        ),
+        3,
+        prod(ult_dmg_stacks, percent(dm.ability.ult_dmg_))
+      )
     )
   ),
   registerBuff(
-    'team_dmg_',
-    teamBuff.combat.common_dmg_.add(listConditional.map({ val1: 1, val2: 2 }))
+    'm2_dodgeCounter_dmg_',
+    ownBuff.combat.dmg_.addWithDmgType(
+      'dodgeCounter',
+      cmpGE(char.mindscape, 2, percent(dm.m2.dodgeCounter_dmg_))
+    )
   ),
-  registerBuff('enemy_defRed_', enemyDebuff.common.defRed_.add(numConditional))
+  registerBuff(
+    'm4_exSpecial_crit_',
+    ownBuff.combat.crit_.addWithDmgType(
+      'exSpecial',
+      cmpGE(char.mindscape, 4, prod(distance, percent(0.01)))
+    )
+  ),
+  registerBuff(
+    'm6_common_dmg_',
+    ownBuff.combat.common_dmg_.add(
+      cmpGE(char.mindscape, 6, prod(m6_stacks, percent(dm.m6.dmg_)))
+    )
+  )
 )
 export default sheet
