ad7b80931f0436b97efe360b531fb99e5a52acf8
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-07-29 22:07:30 -0700

    Generate files

--- libs/gi/formula/src/data/weapon/index.ts
+++ libs/gi/formula/src/data/weapon/index.ts
@@ -60,12 +60,14 @@ import FerrousShadow from './FerrousShadow'
 import FesteringDesire from './FesteringDesire'
 import FilletBlade from './FilletBlade'
 import FinaleOfTheDeep from './FinaleOfTheDeep'
+import FlameForgedInsight from './FlameForgedInsight'
 import FleuveCendreFerryman from './FleuveCendreFerryman'
 import FlowerWreathedFeathers from './FlowerWreathedFeathers'
 import FlowingPurity from './FlowingPurity'
 import FluteOfEzpitzal from './FluteOfEzpitzal'
 import FootprintOfTheRainbow from './FootprintOfTheRainbow'
 import ForestRegalia from './ForestRegalia'
+import FracturedHalo from './FracturedHalo'
 import FreedomSworn from './FreedomSworn'
 import Frostbearer from './Frostbearer'
 import FruitOfFulfillment from './FruitOfFulfillment'
@@ -270,6 +272,7 @@ const data: TagMapNodeEntries[] = [
   FangOfTheMountainKing,
   FavoniusGreatsword,
   FerrousShadow,
+  FlameForgedInsight,
   ForestRegalia,
   FruitfulHook,
   KatsuragikiriNagamasa,
@@ -313,6 +316,7 @@ const data: TagMapNodeEntries[] = [
   EngulfingLightning,
   FavoniusLance,
   FootprintOfTheRainbow,
+  FracturedHalo,
   Halberd,
   IronPoint,
   KitainCrossSpear,
