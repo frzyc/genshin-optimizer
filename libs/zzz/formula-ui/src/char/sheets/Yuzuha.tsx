0da5d4acaefbff94c3f1961e6fb3d4c727fa8c98
Adomas Volungevičius <volungevicius.adomas@gmail.com>
2025-08-02 14:16:45 +0300

    initial yuzuha sheet

--- libs/zzz/formula-ui/src/char/sheets/Yuzuha.tsx
+++ libs/zzz/formula-ui/src/char/sheets/Yuzuha.tsx
@@ -1,25 +1,16 @@
 import type { CharacterKey } from '@genshin-optimizer/zzz/consts'
-import { buffs, conditionals } from '@genshin-optimizer/zzz/formula'
+import { buffs, conditionals, formulas } from '@genshin-optimizer/zzz/formula'
 import { trans } from '../../util'
 import { createBaseSheet } from '../sheetUtil'
 
 const key: CharacterKey = 'Yuzuha'
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const [, ch] = trans('char', key)
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const cond = conditionals[key]
-// TODO: Cleanup
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const buff = buffs[key]
+const formula = formulas[key]
 
-const sheet = createBaseSheet(key)
+const sheet = createBaseSheet(key, {
+  core: [],
+})
 
 export default sheet
