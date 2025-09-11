588156ab148247043e57b450c97b048149b9cdaf
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-09-11 09:49:39 -0700

    Add Lauma to index

--- libs/gi/sheets/src/Characters/index.ts
+++ libs/gi/sheets/src/Characters/index.ts
@@ -60,6 +60,7 @@ import Klee from './Klee'
 import KujouSara from './KujouSara'
 import KukiShinobu from './KukiShinobu'
 import LanYan from './LanYan'
+import Lauma from './Lauma'
 import Layla from './Layla'
 import Lisa from './Lisa'
 import Lynette from './Lynette'
@@ -176,6 +177,7 @@ const characters: Record<CharacterSheetKey, CharacterSheet> = {
   KujouSara,
   KukiShinobu,
   LanYan,
+  Lauma,
   Layla,
   Lisa,
   Lynette,
