8007bcd44a8be44b585a35bfe3262774d299bd4b
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2025-09-13 15:15:11 -0700

    Silently revise GOODv3 to be properly backcompat

--- libs/gi/good/src/IArtifact.ts
+++ libs/gi/good/src/IArtifact.ts
@@ -10,10 +10,13 @@ import type {
 // GOOD 3 and above
 export interface IArtifact extends IBaseArtifact {
   totalRolls?: number // 3-9 for valid 5* artifacts; includes starting rolls
-  startingRolls?: number // 3-4 for valid 5* artifacts; If a 4th substat is in substats array, and this value is 3, the 4th substat is a revealed stat, but should not be factored into calculation
   astralMark?: boolean // Favorite star in-game
-  discard?: boolean // Trash icon in-game
-  elixirCrafted?: boolean // Flag for if the artifact was created using Sanctifying Elixir. This guarantees the main stat + 2 additional rolls on the first 2 substats
+  elixirCrafted?: boolean // Flag for if the artifact was created using Sanctifying Elixir. This guarantees the main stat + at least 2 rolls on the first 2 substats
+  unactivatedSubstats?: ISubstat[] // Unactivated substat(s). Once a substat is activated, it should be moved to `substats` instead
+}
+
+export interface ISubstat extends IBaseSubstat {
+  initialValue?: number // Initial roll of the artifact, if it is known. This includes the first roll of this stat, even if it was not revealed initially e.g. from `unactivatedSubstats`
 }
 
 // GOOD 2 and below
@@ -28,7 +31,7 @@ export interface IBaseArtifact {
   substats: ISubstat[]
 }
 
-export interface ISubstat {
+export interface IBaseSubstat {
   key: SubstatKey | ''
   value: number
 }
