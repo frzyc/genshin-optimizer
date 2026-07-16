46e514eec77cb02b9a946709d92698603803eb73
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2026-07-15 20:12:36 -0700

    Fix and bump to nx 22.7.7 to try to fix flashing console windows

--- apps/frontend-e2e/cypress.config.ts
+++ apps/frontend-e2e/cypress.config.ts
@@ -2,20 +2,20 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset'
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
-    e2e: {
-        ...nxE2EPreset(__filename, {
-            cypressDir: 'src',
-            bundler: 'vite',
-            webServerCommands: {
-                default: 'yarn nx run frontend:dev',
-                production: 'yarn nx run frontend:preview',
-            },
-            ciWebServerCommand: 'yarn nx run frontend:preview',
-            ciBaseUrl: 'http://localhost:4300',
-        }),
-        baseUrl: 'http://localhost:4200',
-        // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
-        // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
-        injectDocumentDomain: true
-    }
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: {
+        default: 'yarn nx run frontend:dev',
+        production: 'yarn nx run frontend:preview',
+      },
+      ciWebServerCommand: 'yarn nx run frontend:preview',
+      ciBaseUrl: 'http://localhost:4300',
+    }),
+    baseUrl: 'http://localhost:4200',
+    // Please ensure you use `cy.origin()` when navigating between domains and remove this option.
+    // See https://docs.cypress.io/app/references/migration-guide#Changes-to-cyorigin
+    injectDocumentDomain: true,
+  },
 })
