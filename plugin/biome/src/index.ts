93a3e2b481041eedeb986465c43aa093a8a3ec3a
Van Nguyen <36019388+nguyentvan7@users.noreply.github.com>
2026-07-16 17:40:47 -0700

    Update scope of lint/format for all projects, exclude root project

--- plugin/biome/src/index.ts
+++ plugin/biome/src/index.ts
@@ -5,13 +5,12 @@ import {
 } from '@nx/devkit'
 import { dirname } from 'path'
 
-export type MyPluginOptions = {
-}
+export type MyPluginOptions = {}
 
 export const createNodesV2: CreateNodesV2<MyPluginOptions> = [
   // look for all package.json files in the workspace
   // (keep this as project.json if you're not using npm workspaces)
-  '**/package.json',
+  '**/project.json',
   async (configFiles, options, context) => {
     return await createNodesFromFiles(
       (configFile, options, context) =>
@@ -35,10 +34,9 @@ async function createNodesInternal(
     projects: {
       [root]: {
         targets: {
-          'lint': {
+          lint: {
             // Nx target syntax to execute a command. More on {projectRoot} below
-            command:
-              `yarn biome lint --diagnostic-level=warn {projectRoot}`,
+            command: 'yarn biome lint --diagnostic-level=warn {projectRoot}',
             cache: true,
             inputs: [
               'default',
@@ -49,9 +47,9 @@ async function createNodesInternal(
               },
             ],
           },
-          'format': {
+          format: {
             // Nx target syntax to execute a command. More on {projectRoot} below
-            command: `yarn biome format {projectRoot}`,
+            command: 'yarn biome format {projectRoot}',
             cache: true,
             inputs: [
               'default',
